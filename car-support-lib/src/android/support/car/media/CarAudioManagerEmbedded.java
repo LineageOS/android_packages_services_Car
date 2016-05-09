df5e37387b5bcbed7345b11c0e8393d2d8959306
Vitalii Tomkiv <vitalit@google.com>
2016-05-09 13:44:22 -0700

    Add CarAudioRecord to android.support.car.
    Bug: 27655492
    Change-Id: I98c84fbcfe6a12a78de266fe492cce6c528f0247

--- car-support-lib/src/android/support/car/media/CarAudioManagerEmbedded.java
+++ car-support-lib/src/android/support/car/media/CarAudioManagerEmbedded.java
@@ -16,12 +16,22 @@
 package android.support.car.media;
 
 import android.media.AudioAttributes;
+import android.media.AudioFormat;
 import android.media.AudioManager.OnAudioFocusChangeListener;
+import android.media.AudioRecord;
 
 /**
  * @hide
  */
 public class CarAudioManagerEmbedded extends CarAudioManager {
+
+    private static final int MAX_BUFFER_SIZE_BYTE = 512 * 1024;
+    private static final int SAMPLING_RATE = 16000;
+    private static final AudioFormat AUDIO_RECORD_FORMAT = new AudioFormat.Builder()
+            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+            .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+            .setSampleRate(SAMPLING_RATE)
+            .build();
 
     private final android.car.media.CarAudioManager mManager;
 
@@ -45,6 +55,31 @@ public class CarAudioManagerEmbedded extends CarAudioManager {
     @Override
     public int abandonAudioFocus(OnAudioFocusChangeListener l, AudioAttributes aa) {
         return mManager.abandonAudioFocus(l, aa);
+    }
+
+    @Override
+    public AudioFormat getAudioRecordAudioFormat() {
+        return AUDIO_RECORD_FORMAT;
+    }
+
+    @Override
+    public int getAudioRecordMinBufferSize() {
+        return AudioRecord.getMinBufferSize(SAMPLING_RATE, AUDIO_RECORD_FORMAT.getChannelMask(),
+                AUDIO_RECORD_FORMAT.getEncoding());
+    }
+
+    @Override
+    public int getAudioRecordMaxBufferSize() {
+        return Math.max(getAudioRecordMinBufferSize(), MAX_BUFFER_SIZE_BYTE);
+    }
+
+    @Override
+    public CarAudioRecord createCarAudioRecord(int bufferSize) throws SecurityException {
+        if (bufferSize < getAudioRecordMinBufferSize() ||
+            bufferSize > getAudioRecordMaxBufferSize()) {
+            throw new IllegalArgumentException("Bad bufferSize value");
+        }
+        return new CarAudioRecordEmbedded(AUDIO_RECORD_FORMAT, bufferSize);
     }
 
     @Override
---
+++ car-support-lib/src/android/support/car/media/CarAudioRecord.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.support.car.media;
+
+/**
+ * CarAudioRecord allows apps to use microphone.
+ */
+public interface CarAudioRecord {
+    /**
+     * Get buffer size which was specified in
+     * {@link CarAudioManager#createCarAudioRecord(int)}
+     */
+    int getBufferSize();
+
+    /**
+     * Start audio recording.
+     */
+    void startRecording();
+
+    /**
+     * Stop audio recording. Calling stop multiple times will be a safe operation.
+     */
+    void stop();
+
+    /**
+     * Release native resource allocated for this instance. {@link CarAudioRecord} can no longer
+     * be used once release is called.
+     */
+    void release();
+
+    /** Check {@link AudioRecord#getRecordingState() }. */
+    int getRecordingState();
+
+    /** Check {@link AudioRecord#getState() }. */
+    int getState();
+
+    /** Check {@link AudioRecord#getAudioSessionId() }. */
+    int getAudioSessionId();
+
+    /**
+     * Read recorded audio. Note that audio recording should be started with
+     * {@link #startRecording()} before this.
+     * @param audioData
+     * @param offsetInBytes
+     * @param sizeInBytes
+     * @return Number of bytes read. will return {@link android.media.AudioRecord#ERROR} on error.
+     * @throws IllegalStateException if audio recording was not started.
+     */
+    int read(byte[] audioData, int offsetInBytes, int sizeInBytes)
+            throws IllegalStateException;
+}
