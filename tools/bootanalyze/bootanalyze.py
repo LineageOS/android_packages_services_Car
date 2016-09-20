abc9233c5099cdbf5827fdb07d8024ee67877049
Vitalii Tomkiv <vitalit@google.com>
2016-09-20 11:55:44 -0700

    Add multiple runs to bootanalyze tool.
    This change adds new flag to bootanalyze tool to run mutliple
    measurements and providing AVG value.
    
    Test: manually tested
    Change-Id: I2e3232707f1d129eaa0c81cba84fab8fd974ddf8

--- tools/bootanalyze/bootanalyze.py
+++ tools/bootanalyze/bootanalyze.py
@@ -25,18 +25,25 @@ import re
 import os
 import subprocess
 import time
+import math
 import datetime
 import sys
 import operator
 
 TIME_DMESG = "\[\s*(\d+\.\d+)\]"
 TIME_LOGCAT = "^\d\d-\d\d\s+\d\d:\d\d:\d\d\.\d\d\d"
+VALUE_TOO_BIG = 600
+MAX_RETRIES = 5
 
 def main():
   args = init_arguments()
 
-  if args.reboot:
-    reboot()
+  if args.iterate < 1:
+    raise Exception('Number of iteration must be >=1');
+
+  if args.iterate > 1 and not args.reboot:
+    print "Forcing reboot flag"
+    args.reboot = True
 
   cfg = yaml.load(args.config)
 
@@ -46,13 +53,44 @@ def main():
   if 'stop_event' not in cfg:
     raise Exception('Logcat stop_event is missing in config');
 
+  data_points = {}
+  for it in range(0, args.iterate):
+    if args.iterate > 1:
+      print "Run: {0}".format(it + 1)
+    attempt = 1
+    processing_data = None
+    while attempt <= MAX_RETRIES and processing_data is None:
+      attempt += 1
+      processing_data = iterate(args, search_events, cfg)
+
+    if processing_data is None:
+      # Processing error
+      print "Failed to collect valid samples for run {0}".format(it + 1)
+      continue
+    for k, v in processing_data.iteritems():
+      if k not in data_points:
+        data_points[k] = []
+      data_points[k].append(v['value'])
+
+  if args.iterate > 1:
+    print "Avg values after {0} runs".format(args.iterate)
+    print '{0:30}: {1:<7} {2:<7}'.format("Event", "Mean", "stddev")
+
+    for item in sorted(data_points.items(), key=operator.itemgetter(1)):
+      print '{0:30}: {1:<7.5} {2:<7.5}'.format(
+        item[0], sum(item[1])/len(item[1]), stddev(item[1]))
+
+def iterate(args, search_events, cfg):
+  if args.reboot:
+    reboot()
+
   logcat_events = collect_events(
     search_events, 'adb logcat -b all', cfg['stop_event'])
 
   dmesg_events = collect_events(search_events, 'adb shell dmesg')
 
   logcat_event_time = extract_time(
-    logcat_events, TIME_LOGCAT, logcat_time_func(2016));
+    logcat_events, TIME_LOGCAT, logcat_time_func(datetime.date.today().year));
   logcat_original_time = extract_time(
     logcat_events, TIME_LOGCAT, str);
   dmesg_event_time = extract_time(
@@ -77,13 +115,26 @@ def main():
   for k in events_to_correct:
     if events[k] - diff_time > 0:
       events[k] = events[k] - diff_time
+      if events[k] > VALUE_TOO_BIG:
+        print "Event {0} value {1} too big , possible processing error".format(
+          k, events[k])
+        return None
+
+  data_points = {}
 
   for item in sorted(events.items(), key=operator.itemgetter(1)):
+    data_points[item[0]] = {
+      'value': item[1],
+      'from_dmesg': item[0] in replaced_from_dmesg,
+      'logcat_value': logcat_original_time[item[0]]
+    }
     print '{0:30}: {1:<7.5} {2:1} ({3})'.format(
       item[0], item[1], '*' if item[0] in replaced_from_dmesg else '',
       logcat_original_time[item[0]])
 
-  print '\n', '* - event time was obtained from dmesg log'
+  print '\n* - event time was obtained from dmesg log\n'
+
+  return data_points
 
 def init_arguments():
   parser = argparse.ArgumentParser(description='Measures boot time.')
@@ -93,6 +144,8 @@ def init_arguments():
   parser.add_argument('-c', '--config', dest='config',
                       default='config.yaml', type=argparse.FileType('r'),
                       help='config file for the tool', )
+  parser.add_argument('-n', '--iterate', dest='iterate', type=int, default=1,
+                      help='number of time to repeat the measurement', )
   return parser.parse_args()
 
 def collect_events(search_events, command, stop_event=None):
@@ -143,6 +196,13 @@ def logcat_time_func(offset_year):
 
 def datetime_to_unix_time(ndate):
   return time.mktime(ndate.timetuple()) + ndate.microsecond/1000000.0
+
+def stddev(data):
+  items_count = len(data)
+  avg = sum(data) / items_count
+  sq_diffs_sum = sum([(v - avg) ** 2 for v in data])
+  variance = sq_diffs_sum / items_count
+  return math.sqrt(variance)
 
 if __name__ == '__main__':
   main()
