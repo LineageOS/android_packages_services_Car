d9f6ae031964aa9e7f97668af499d0b92a17ef2e
Rakesh Iyer <rni@google.com>
2016-10-20 00:05:13 -0700

    Add car trust agent.
    Original sha1: 7014aa9053e3fb1d60a5e4069adb8e6f47cf5218
    
    Credits:
      victorchan@
    
    Bug: 32118797
    Test: Manual.
    Change-Id: Ia498d0019b2e88add1957b1ed863dbbf17305913

---
+++ TrustAgent/src/com/android/car/trust/BootupReceiver.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.car.trust;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootupReceiver extends BroadcastReceiver {
+    private static final String TAG = "CarBTTrustAgent";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            if (Log.isLoggable(TAG, Log.DEBUG)) {
+                Log.d(TAG, "ACTION_BOOT_COMPLETED");
+            }
+            context.startService(new Intent(context, CarBluetoothTrustAgent.class));
+        }
+    }
+}
