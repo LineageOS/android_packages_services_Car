a7d8ed1681d297bdda8345c1598c7b9a388599be
Antonio Cortes <antoniocortes@google.com>
2017-02-02 11:50:01 -0800

    VMS skeleton code: VmsSubscriberManager
    Create class that will be used by clients to interact with VMS
    subcription services.
    
    Bug: b/34976096
    Test: VmsSubscriberManagerTest
    Test: Flashed a Mojave board and verified there was no Car service
    crashes.
    
    Change-Id: Ic11806275465f185fb7670729b64542526cd9a7f

---
+++ car-lib/src/android/car/vms/VmsSubscriberManager.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.car.vms;
+
+import android.annotation.SystemApi;
+import android.car.Car;
+import android.car.CarManagerBase;
+import android.car.CarNotConnectedException;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.android.internal.annotations.GuardedBy;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * API for interfacing with the VmsSubscriberService. It supports a single listener that can
+ * (un)subscribe to different layers. After getting an instance of this manager, the first step
+ * must be to call #setListener. After that, #subscribe and #unsubscribe methods can be invoked.
+ *
+ * @hide
+ */
+@SystemApi
+public final class VmsSubscriberManager implements CarManagerBase {
+    private static final boolean DBG = true;
+    private static final String TAG = "VmsSubscriberManager";
+
+    private final Handler mHandler;
+    private final IVmsSubscriberService mVmsSubscriberService;
+    private final IOnVmsMessageReceivedListener mIListener;
+    private final Object mListenerLock = new Object();
+    @GuardedBy("mListenerLock")
+    private OnVmsMessageReceivedListener mListener;
+
+    /** Interface exposed to VMS subscribers: it is a wrapper of IOnVmsMessageReceivedListener. */
+    public interface OnVmsMessageReceivedListener {
+        /** Called when the property is updated */
+        void onVmsMessageReceived(VmsProperty message);
+    }
+
+    /**
+     * Allows to asynchronously dispatch onVmsMessageReceived events.
+     */
+    private final static class VmsEventHandler extends Handler {
+        /** Constants handled in the handler */
+        private static final int ON_RECEIVE_MESSAGE_EVENT = 0;
+
+        private final WeakReference<VmsSubscriberManager> mMgr;
+
+        VmsEventHandler(VmsSubscriberManager mgr, Looper looper) {
+            super(looper);
+            mMgr = new WeakReference<>(mgr);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case ON_RECEIVE_MESSAGE_EVENT:
+                    VmsSubscriberManager mgr = mMgr.get();
+                    if (mgr != null) {
+                        mgr.dispatchOnReceiveMessage((VmsProperty) msg.obj);
+                    }
+                    break;
+                default:
+                    Log.e(VmsSubscriberManager.TAG, "Event type not handled:  " + msg.what);
+                    break;
+            }
+        }
+    }
+
+    public VmsSubscriberManager(IBinder service, Handler handler) {
+        mVmsSubscriberService = IVmsSubscriberService.Stub.asInterface(service);
+        mHandler = new VmsEventHandler(this, handler.getLooper());
+        mIListener = new IOnVmsMessageReceivedListener.Stub() {
+            @Override
+            public void onVmsMessageReceived(VmsProperty message) throws RemoteException {
+                mHandler.sendMessage(
+                        mHandler.obtainMessage(VmsEventHandler.ON_RECEIVE_MESSAGE_EVENT, message));
+            }
+        };
+    }
+
+    /**
+     * Sets the listener ({@link #mListener}) this manager is linked to. Subscriptions to the
+     * {@link com.android.car.VmsSubscriberService} are done through the {@link #mIListener}.
+     * Therefore, notifications from the {@link com.android.car.VmsSubscriberService} are received
+     * by the {@link #mIListener} and then forwarded to the {@link #mListener}.
+     *
+     * @param listener subscriber listener that will handle onVmsMessageReceived events.
+     * @throws IllegalStateException if the listener was already set.
+     */
+    public void setListener(OnVmsMessageReceivedListener listener) {
+        if (DBG) {
+            Log.d(TAG, "Setting listener.");
+        }
+        synchronized (mListenerLock) {
+            if (mListener != null) {
+                throw new IllegalStateException("Listener is already configured.");
+            }
+            mListener = listener;
+        }
+    }
+
+    /**
+     * Removes the listener and unsubscribes from all the layer/version.
+     */
+    public void clearListener() {
+        synchronized (mListenerLock) {
+            mListener = null;
+        }
+        // TODO(antoniocortes): logic to unsubscribe from all the layer/version pairs.
+    }
+
+    /**
+     * Subscribes to listen to the layer/version specified.
+     *
+     * @param layer           the layer id to subscribe to.
+     * @param version         the layer version to subscribe to.
+     * @param silentSubscribe if true, the listener does not notify publishers of its existence,
+     *                        it only listens passively.
+     * @throws IllegalStateException if the listener was not set via {@link #setListener}.
+     */
+    public void subscribe(int layer, int version, boolean silentSubscribe)
+            throws CarNotConnectedException {
+        if (DBG) {
+            Log.d(TAG, "Subscribing to layer: " + layer + ", version: " + version);
+        }
+        OnVmsMessageReceivedListener listener;
+        synchronized (mListenerLock) {
+            listener = mListener;
+        }
+        if (listener == null) {
+            Log.w(TAG, "subscribe: listener was not set, " +
+                    "setListener must be called first.");
+            throw new IllegalStateException("Listener was not set.");
+        }
+        try {
+            mVmsSubscriberService.addOnVmsMessageReceivedListener(layer, version, mIListener,
+                    silentSubscribe);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Could not connect: ", e);
+            throw new CarNotConnectedException(e);
+        } catch (IllegalStateException ex) {
+            Car.checkCarNotConnectedExceptionFromCarService(ex);
+        }
+    }
+
+    /**
+     * Unsubscribes from the layer/version specified.
+     *
+     * @param layer   the layer id to unsubscribe from.
+     * @param version the layer version to unsubscribe from.
+     * @throws IllegalStateException if the listener was not set via {@link #setListener}.
+     */
+    public void unsubscribe(int layer, int version) {
+        if (DBG) {
+            Log.d(TAG, "Unsubscribing from layer: " + layer + ", version: " + version);
+        }
+        OnVmsMessageReceivedListener listener;
+        synchronized (mListenerLock) {
+            listener = mListener;
+        }
+        if (listener == null) {
+            Log.w(TAG, "unsubscribe: listener was not set, " +
+                    "setListener must be called first.");
+            throw new IllegalStateException("Listener was not set.");
+        }
+        try {
+            mVmsSubscriberService.removeOnVmsMessageReceivedListener(layer, version, mIListener);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Failed to unregister subscriber", e);
+            // ignore
+        } catch (IllegalStateException ex) {
+            Car.hideCarNotConnectedExceptionFromCarService(ex);
+        }
+    }
+
+    private void dispatchOnReceiveMessage(VmsProperty message) {
+        OnVmsMessageReceivedListener listener;
+        synchronized (mListenerLock) {
+            listener = mListener;
+        }
+        if (listener == null) {
+            Log.e(TAG, "Listener died, not dispatching event.");
+            return;
+        }
+        listener.onVmsMessageReceived(message);
+    }
+
+    /** @hide */
+    @Override
+    public void onCarDisconnected() {
+        clearListener();
+    }
+}
---
+++ tests/carservice_test/src/com/android/car/test/VmsSubscriberManagerTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.test;
+
+import android.car.Car;
+import android.car.VehicleAreaType;
+import android.car.vms.VmsSubscriberManager.OnVmsMessageReceivedListener;
+import android.car.vms.VmsSubscriberManager;
+import android.car.vms.VmsProperty;
+import android.hardware.automotive.vehicle.V2_0.VehiclePropValue;
+import android.hardware.automotive.vehicle.V2_0.VehicleProperty;
+import android.hardware.automotive.vehicle.V2_0.VehiclePropertyAccess;
+import android.hardware.automotive.vehicle.V2_0.VehiclePropertyChangeMode;
+import android.os.SystemClock;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.util.Log;
+
+import com.android.car.vehiclehal.VehiclePropValueBuilder;
+import com.android.car.vehiclehal.test.MockedVehicleHal.VehicleHalPropertyHandler;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+@MediumTest
+public class VmsSubscriberManagerTest extends MockedCarTestBase {
+    private static final String TAG = "VmsSubscriberManagerTest";
+
+    private HalHandler mHalHandler;
+    // Used to block until the HAL property is updated in HalHandler.onPropertySet.
+    private Semaphore mHalHandlerSemaphore;
+    // Used to block until a value is propagated to the TestListener.onVmsMessageReceived.
+    private Semaphore mSubscriberSemaphore;
+
+    @Override
+    protected synchronized void configureMockedHal() {
+        mHalHandler = new HalHandler();
+        addProperty(VehicleProperty.VEHICLE_MAP_SERVICE, mHalHandler)
+                .setChangeMode(VehiclePropertyChangeMode.ON_CHANGE)
+                .setAccess(VehiclePropertyAccess.READ_WRITE)
+                .setSupportedAreas(VehicleAreaType.VEHICLE_AREA_TYPE_NONE);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mSubscriberSemaphore = new Semaphore(0);
+        mHalHandlerSemaphore = new Semaphore(0);
+    }
+
+    // Test injecting a value in the HAL and verifying it propagates to a subscriber.
+    public void testSubscribe() throws Exception {
+        VmsSubscriberManager vmsSubscriberManager = (VmsSubscriberManager) getCar().getCarManager(
+                Car.VMS_SUBSCRIBER_SERVICE);
+        TestListener listener = new TestListener();
+        vmsSubscriberManager.setListener(listener);
+        vmsSubscriberManager.subscribe(0, 0, false);
+
+        // Inject a value and wait for its callback in TestListener.onVmsMessageReceived.
+        VehiclePropValue v = VehiclePropValueBuilder.newBuilder(VehicleProperty.VEHICLE_MAP_SERVICE)
+                .setAreaId(VehicleAreaType.VEHICLE_AREA_TYPE_NONE)
+                .setTimestamp(SystemClock.elapsedRealtimeNanos())
+                .build();
+        v.value.stringValue = "Hello, World!";
+        assertEquals(0, mSubscriberSemaphore.availablePermits());
+        getMockedVehicleHal().injectEvent(v);
+        assertTrue(mSubscriberSemaphore.tryAcquire(2L, TimeUnit.SECONDS));
+        assertEquals("Hello, World!", listener.getProperty().getValue());
+    }
+
+    private class HalHandler implements VehicleHalPropertyHandler {
+        private VehiclePropValue mValue;
+
+        @Override
+        public synchronized void onPropertySet(VehiclePropValue value) {
+            mValue = value;
+            mHalHandlerSemaphore.release();
+        }
+
+        @Override
+        public synchronized VehiclePropValue onPropertyGet(VehiclePropValue value) {
+            return mValue != null ? mValue : value;
+        }
+
+        @Override
+        public synchronized void onPropertySubscribe(int property, int zones, float sampleRate) {
+            Log.d(TAG, "onPropertySubscribe property " + property + " sampleRate " + sampleRate);
+        }
+
+        @Override
+        public synchronized void onPropertyUnsubscribe(int property) {
+            Log.d(TAG, "onPropertyUnSubscribe property " + property);
+        }
+
+        public VehiclePropValue getValue() {
+            return mValue;
+        }
+    }
+
+
+    private class TestListener implements OnVmsMessageReceivedListener {
+        private VmsProperty mProperty;
+
+        @Override
+        public void onVmsMessageReceived(VmsProperty property) {
+            Log.d(TAG, "onVmsMessageReceived: " + property);
+            mProperty = property;
+            mSubscriberSemaphore.release();
+        }
+
+        public VmsProperty getProperty() {
+            return mProperty;
+        }
+    }
+}
