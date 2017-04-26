02c0419f33bb63937c5d250342615104cab35920
Scott Randolph <randolphs@google.com>
2017-04-25 17:24:50 -0700

    Remove unnecessary dependencies for EVS Manager
    Clean up Android.mk for EVS manager to remove unnecessary dependencies
    Test:  Build locally
    
    Change-Id: Ica20481b84d6c1c164335494b99cc70525e6cf24

--- evs/manager/Android.mk
+++ evs/manager/Android.mk
@@ -9,26 +9,22 @@ LOCAL_SRC_FILES := \
     HalCamera.cpp \
     VirtualCamera.cpp \
 
-LOCAL_C_INCLUDES += \
-    frameworks/base/include \
-    packages/services/Car/evs/manager \
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     liblog \
     libutils \
     libui \
-    libhwbinder \
     libhidlbase \
     libhidltransport \
-    libtinyalsa \
     libhardware \
     android.hardware.automotive.evs@1.0 \
 
-LOCAL_STRIP_MODULE := keep_symbols
 
 LOCAL_MODULE := android.automotive.evs.manager@1.0
+
 LOCAL_MODULE_TAGS := optional
+LOCAL_STRIP_MODULE := keep_symbols
 
 LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
 LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
