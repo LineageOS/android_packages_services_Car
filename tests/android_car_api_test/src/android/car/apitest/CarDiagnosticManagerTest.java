f5d514a94cf91c2ac878bd4f48a68928e0c0b646
Enrico Granata <egranata@google.com>
2017-05-22 11:09:21 -0700

    Revert "Check whether the diagnostics API is enabled before running"
    This reverts commit 642dd5c83857e0640f6d2fc9f14ac899b10aa3f1.

--- tests/android_car_api_test/src/android/car/apitest/CarDiagnosticManagerTest.java
+++ tests/android_car_api_test/src/android/car/apitest/CarDiagnosticManagerTest.java
@@ -25,15 +25,11 @@ import android.os.IBinder;
 import android.os.Looper;
 import android.test.AndroidTestCase;
 import android.test.suitebuilder.annotation.MediumTest;
-import android.util.Log;
-import com.android.car.internal.FeatureConfiguration;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 @MediumTest
 public class CarDiagnosticManagerTest extends AndroidTestCase {
-    private static final String TAG = CarDiagnosticManagerTest.class.getSimpleName();
-
     private static final long DEFAULT_WAIT_TIMEOUT_MS = 5000;
 
     private final Semaphore mConnectionWait = new Semaphore(0);
@@ -64,25 +60,14 @@ public class CarDiagnosticManagerTest extends AndroidTestCase {
         mConnectionWait.tryAcquire(timeoutMs, TimeUnit.MILLISECONDS);
     }
 
-    private boolean isFeatureEnabled() {
-        return FeatureConfiguration.ENABLE_DIAGNOSTIC;
-    }
-
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         mCar = Car.createCar(getContext(), mConnectionListener);
         mCar.connect();
         waitForConnection(DEFAULT_WAIT_TIMEOUT_MS);
-
-        if (isFeatureEnabled()) {
-            Log.i(TAG, "attempting to get DIAGNOSTIC_SERVICE");
-            mCarDiagnosticManager =
-                    (CarDiagnosticManager) mCar.getCarManager(Car.DIAGNOSTIC_SERVICE);
-            assertNotNull(mCarDiagnosticManager);
-        } else {
-            Log.i(TAG, "skipping diagnostic tests as ENABLE_DIAGNOSTIC flag is false");
-        }
+        mCarDiagnosticManager = (CarDiagnosticManager) mCar.getCarManager(Car.DIAGNOSTIC_SERVICE);
+        assertNotNull(mCarDiagnosticManager);
     }
 
     @Override
@@ -97,11 +82,6 @@ public class CarDiagnosticManagerTest extends AndroidTestCase {
      * @throws Exception
      */
     public void testLiveFrame() throws Exception {
-        if (!isFeatureEnabled()) {
-            Log.i(TAG, "skipping testLiveFrame as diagnostics API is not enabled");
-            return;
-        }
-
         CarDiagnosticEvent liveFrame = mCarDiagnosticManager.getLatestLiveFrame();
         if (null != liveFrame) {
             assertTrue(liveFrame.isLiveFrame());
@@ -115,11 +95,6 @@ public class CarDiagnosticManagerTest extends AndroidTestCase {
      * @throws Exception
      */
     public void testFreezeFrames() throws Exception {
-        if (!isFeatureEnabled()) {
-            Log.i(TAG, "skipping testFreezeFrames as diagnostics API is not enabled");
-            return;
-        }
-
         long[] timestamps = mCarDiagnosticManager.getFreezeFrameTimestamps();
         if (null != timestamps) {
             for (long timestamp : timestamps) {
