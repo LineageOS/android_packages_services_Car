d8af2bfd33ca38fd6c60e1875948f722cad409f8
Enrico Granata <egranata@google.com>
2017-11-21 11:38:06 -0800

    Add a KitchenSink page to display supported Vehicle HAL properties
    This should make it easy to inspect an arbitrary vehicle and discover
    what HAL properties are supported on it
    
    Test: manual
    Change-Id: Ife18355fee75a573859a6f75675fd8cf1a058328

--- tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/KitchenSinkActivity.java
+++ tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/KitchenSinkActivity.java
@@ -49,6 +49,7 @@ import com.google.android.car.kitchensink.sensor.SensorsTestFragment;
 import com.google.android.car.kitchensink.setting.CarServiceSettingsActivity;
 import com.google.android.car.kitchensink.storagelifetime.StorageLifetimeFragment;
 import com.google.android.car.kitchensink.touch.TouchTestFragment;
+import com.google.android.car.kitchensink.vhal.VehicleHalFragment;
 import com.google.android.car.kitchensink.volume.VolumeTestFragment;
 
 import java.util.ArrayList;
@@ -150,6 +151,7 @@ public class KitchenSinkActivity extends CarDrawerActivity {
             add("bluetooth headset",BluetoothHeadsetFragment.class);
             add("bluetooth messaging test", MapMceTestFragment.class);
             add("storage lifetime", StorageLifetimeFragment.class);
+            add("vehicle hal", VehicleHalFragment.class);
             add("car service settings", () -> {
                 Intent intent = new Intent(KitchenSinkActivity.this,
                     CarServiceSettingsActivity.class);
