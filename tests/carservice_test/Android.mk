bc8ff3f40ef91c59bc9ca2fe94e72f618154e51e
Ying Zheng <yizheng@google.com>
2018-09-19 14:36:39 -0700

    Add car user lib to car service test.
    Test: build
    Bug: 113868873
    Change-Id: Id93752083051eeef46ed6f85048704fa6ee024f3
    (cherry picked from commit d75edf01ce71d04bf7ac20db041c5ae2078ed99a)

--- tests/carservice_test/Android.mk
+++ tests/carservice_test/Android.mk
@@ -38,16 +38,21 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 LOCAL_PROGUARD_ENABLED := disabled
 
 LOCAL_STATIC_JAVA_LIBRARIES := junit
-LOCAL_STATIC_JAVA_LIBRARIES += car-service-lib-for-test \
-                               vehicle-hal-support-lib \
-                               car-systemtest \
-                               mockito-target-inline \
-                               android-support-test \
-                               android.hardware.automotive.vehicle-V2.0-java \
-                               com.android.car.test.utils \
-                               truth-prebuilt
+LOCAL_STATIC_JAVA_LIBRARIES += \
+    car-service-lib-for-test \
+    vehicle-hal-support-lib \
+    car-systemtest \
+    mockito-target-inline \
+    android-support-test \
+    android.hardware.automotive.vehicle-V2.0-java \
+    com.android.car.test.utils \
+    truth-prebuilt
 
-LOCAL_JAVA_LIBRARIES := android.car android.test.runner android.test.base
+LOCAL_JAVA_LIBRARIES := \
+    android.car \
+    android.car.user \
+    android.test.runner \
+    android.test.base
 
 LOCAL_JNI_SHARED_LIBRARIES := libdexmakerjvmtiagent
 
