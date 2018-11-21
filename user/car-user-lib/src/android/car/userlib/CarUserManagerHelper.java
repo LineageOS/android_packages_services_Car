59748bdf878a2198f02cc2c654efb87530d8e3b1
Inseob Kim <inseob@google.com>
2018-11-21 14:20:58 +0900

    Schematize Car system properties
    Properties accessed across partitions are now schematized and will
    become APIs to make explicit interfaces among partitions.
    
    Bug: 117924132
    Test: mma -j
    Change-Id: I738be670644d80d7d738456304a6661eabbe2fc2
    (cherry picked from commit 80bf2c940ffd42b6201e24d86f8fc601fa4493ce)

--- user/car-user-lib/src/android/car/userlib/CarUserManagerHelper.java
+++ user/car-user-lib/src/android/car/userlib/CarUserManagerHelper.java
@@ -29,10 +29,10 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
+import android.sysprop.CarProperties;
 import android.util.Log;
 
 import com.android.internal.util.UserIcons;
@@ -56,7 +56,6 @@ import java.util.Set;
  */
 public class CarUserManagerHelper {
     private static final String TAG = "CarUserManagerHelper";
-    private static final String HEADLESS_SYSTEM_USER = "android.car.systemuser.headless";
 
     // Place holder for user name of the first user created.
     public static final String DEFAULT_FIRST_ADMIN_NAME = "Driver";
@@ -259,7 +258,7 @@ public class CarUserManagerHelper {
      * @return {@boolean true} if headless system user.
      */
     public boolean isHeadlessSystemUser() {
-        return SystemProperties.getBoolean(HEADLESS_SYSTEM_USER, false);
+        return CarProperties.headless_system_user().orElse(false);
     }
 
     /**
