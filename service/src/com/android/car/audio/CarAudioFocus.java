12edb60badcac7d2be034933918b13ab0e7d6878
Scott Randolph <randolphs@google.com>
2018-11-28 18:57:09 -0800

    Move CarAudioFocus into audio subfolder
    Aligns with refactoring that happened in master after this change was
    originally applied in pi-car-dev.
    
    Test: Build Mojave and play sound in KitchenSink
    Change-Id: I0670684faffd924f391b7e7527709e86abf31bd5

--- service/src/com/android/car/audio/CarAudioFocus.java
+++ service/src/com/android/car/audio/CarAudioFocus.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.car;
+package com.android.car.audio;
 
 import android.hardware.automotive.audiocontrol.V1_0.ContextNumber;
 import android.media.AudioAttributes;
