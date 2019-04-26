23a53599469663b82aca51531d5156d359871de1
Mark Tabry <mtab@google.com>
2019-04-26 13:18:58 -0700

    Create a mock client framework for VMS API tests.
    This allows for explicit control and monitoring of all parties involved
    in VMS transactions (publishers, subscribers, HAL clients), thus de-flaking
    the tests.
    
    Bug: 116333782
    Test: atest AndroidCarApiTest CarServiceTest CarServiceUnitTest
    Change-Id: I8254e975399040fea7542cf6f592c37a3c90f44d

--- tests/carservice_test/src/com/android/car/VmsHalServiceSubscriptionEventTest.java
+++ tests/carservice_test/src/com/android/car/VmsHalServiceSubscriptionEventTest.java
@@ -17,62 +17,24 @@
 package com.android.car;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import android.car.VehicleAreaType;
 import android.car.vms.VmsLayer;
-import android.hardware.automotive.vehicle.V2_0.VehiclePropValue;
-import android.hardware.automotive.vehicle.V2_0.VehicleProperty;
-import android.hardware.automotive.vehicle.V2_0.VehiclePropertyAccess;
-import android.hardware.automotive.vehicle.V2_0.VehiclePropertyChangeMode;
-import android.hardware.automotive.vehicle.V2_0.VmsAvailabilityStateIntegerValuesIndex;
 import android.hardware.automotive.vehicle.V2_0.VmsBaseMessageIntegerValuesIndex;
 import android.hardware.automotive.vehicle.V2_0.VmsMessageType;
 import android.hardware.automotive.vehicle.V2_0.VmsSubscriptionsStateIntegerValuesIndex;
 
 import androidx.test.filters.MediumTest;
-import androidx.test.runner.AndroidJUnit4;
-
-import com.android.car.vehiclehal.VehiclePropValueBuilder;
-import com.android.car.vehiclehal.test.MockedVehicleHal;
-import com.android.car.vehiclehal.test.MockedVehicleHal.VehicleHalPropertyHandler;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 
-@RunWith(AndroidJUnit4.class)
 @MediumTest
-public class VmsHalServiceSubscriptionEventTest extends MockedCarTestBase {
-    private static final String TAG = "VmsHalServiceTest";
-
-    private HalHandler mHalHandler;
-    private MockedVehicleHal mHal;
-    // Used to block until the HAL property is updated in HalHandler.onPropertySet.
-    private Semaphore mHalHandlerSemaphore;
-
-    @Override
-    protected synchronized void configureMockedHal() {
-        mHalHandler = new HalHandler();
-        addProperty(VehicleProperty.VEHICLE_MAP_SERVICE, mHalHandler)
-                .setChangeMode(VehiclePropertyChangeMode.ON_CHANGE)
-                .setAccess(VehiclePropertyAccess.READ_WRITE)
-                .addAreaConfig(VehicleAreaType.VEHICLE_AREA_TYPE_GLOBAL, 0, 0);
-    }
-
-    @Override
-    public void setUp() throws Exception {
-        mHalHandlerSemaphore = new Semaphore(0);
-        super.setUp();
-        mHal = getMockedVehicleHal();
-    }
-
+public class VmsHalServiceSubscriptionEventTest extends MockedVmsTestBase {
     @Test
     public void testEmptySubscriptions() throws Exception {
         List<VmsLayer> layers = new ArrayList<>();
@@ -81,7 +43,8 @@ public class VmsHalServiceSubscriptionEventTest extends MockedCarTestBase {
 
     @Test
     public void testOneSubscription() throws Exception {
-        List<VmsLayer> layers = Arrays.asList(new VmsLayer(8, 0, 3));
+        List<VmsLayer> layers =
+                Collections.singletonList(new VmsLayer(8, 0, 3));
         subscriptionTestLogic(layers);
     }
 
@@ -101,27 +64,16 @@ public class VmsHalServiceSubscriptionEventTest extends MockedCarTestBase {
      * responds with the same layers.
      */
     private void subscriptionTestLogic(List<VmsLayer> layers) throws Exception {
-        // Wait for availability change message that signals the service is ready.
-        assertTrue(mHalHandlerSemaphore.tryAcquire(2L, TimeUnit.SECONDS));
-        // Validate response.
-        ArrayList<Integer> v = mHalHandler.getValues();
-        assertEquals(VmsMessageType.AVAILABILITY_CHANGE,
-                (int) v.get(VmsBaseMessageIntegerValuesIndex.MESSAGE_TYPE));
-        assertEquals(0, (int) v.get(VmsAvailabilityStateIntegerValuesIndex.SEQUENCE_NUMBER));
-
         int sequenceNumber = 0;
         for (VmsLayer layer : layers) {
             sequenceNumber++;
             subscribeViaHal(sequenceNumber, layer);
         }
         // Send subscription request.
-        mHal.injectEvent(createHalSubscriptionRequest());
-
-        // Wait for subscription response
-        assertTrue(mHalHandlerSemaphore.tryAcquire(2L, TimeUnit.SECONDS));
+        getMockHalClient().sendMessage(VmsMessageType.SUBSCRIPTIONS_REQUEST);
 
         // Validate response.
-        v = mHalHandler.getValues();
+        List<Integer> v = getMockHalClient().receiveMessage().value.int32Values;
         assertEquals(VmsMessageType.SUBSCRIPTIONS_RESPONSE,
                 (int) v.get(VmsBaseMessageIntegerValuesIndex.MESSAGE_TYPE));
         assertEquals(sequenceNumber,
@@ -146,45 +98,19 @@ public class VmsHalServiceSubscriptionEventTest extends MockedCarTestBase {
      */
     private void subscribeViaHal(int sequenceNumber, VmsLayer layer) throws Exception {
         // Send subscribe request.
-        mHal.injectEvent(createHalSubscribeRequest(layer));
-        // Wait for response.
-        assertTrue(mHalHandlerSemaphore.tryAcquire(2L, TimeUnit.SECONDS));
+        getMockHalClient().sendMessage(
+                VmsMessageType.SUBSCRIBE,
+                layer.getType(),
+                layer.getSubtype(),
+                layer.getVersion());
+
         // Validate response.
-        ArrayList<Integer> v = mHalHandler.getValues();
+        List<Integer> v = getMockHalClient().receiveMessage().value.int32Values;
         assertEquals(VmsMessageType.SUBSCRIPTIONS_CHANGE,
                 (int) v.get(VmsBaseMessageIntegerValuesIndex.MESSAGE_TYPE));
         assertEquals(sequenceNumber,
                 (int) v.get(VmsSubscriptionsStateIntegerValuesIndex.SEQUENCE_NUMBER));
         assertEquals(sequenceNumber,
                 (int) v.get(VmsSubscriptionsStateIntegerValuesIndex.NUMBER_OF_LAYERS));
-    }
-
-    private VehiclePropValue createHalSubscribeRequest(VmsLayer layer) {
-        return VehiclePropValueBuilder.newBuilder(VehicleProperty.VEHICLE_MAP_SERVICE)
-                .addIntValue(VmsMessageType.SUBSCRIBE)
-                .addIntValue(layer.getType())
-                .addIntValue(layer.getSubtype())
-                .addIntValue(layer.getVersion())
-                .build();
-    }
-
-    private VehiclePropValue createHalSubscriptionRequest() {
-        return VehiclePropValueBuilder.newBuilder(VehicleProperty.VEHICLE_MAP_SERVICE)
-                .addIntValue(VmsMessageType.SUBSCRIPTIONS_REQUEST)
-                .build();
-    }
-
-    private class HalHandler implements VehicleHalPropertyHandler {
-        private ArrayList<Integer> mValues;
-
-        @Override
-        public synchronized void onPropertySet(VehiclePropValue value) {
-            mValues = value.value.int32Values;
-            mHalHandlerSemaphore.release();
-        }
-
-        public ArrayList<Integer> getValues() {
-            return mValues;
-        }
     }
 }
