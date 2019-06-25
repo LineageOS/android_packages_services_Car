f6541b99e5ad5ea51d592b5b1b9f39fa919e2a46
Wenting Zhai <wentingzhai@google.com>
2019-06-24 17:40:01 -0700

    Add unit test for CarTrustAgentBleManager
    Bug: 135953137
    Test: atest CarServiceUnitTest
    Change-Id: I136679efeb7b23be0ad1f5f935db347d2b65343a

---
+++ tests/carservice_unit_test/src/com/android/car/trust/CarTrustAgentBleManagerTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.trust;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.android.car.BLEStreamProtos.BLEOperationProto.OperationType;
+import com.android.car.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.UUID;
+
+
+/**
+ * Unit test for {@link CarTrustAgentBleManager}.
+ */
+@RunWith(AndroidJUnit4.class)
+public class CarTrustAgentBleManagerTest {
+
+    private static final String ADDRESS = "00:11:22:33:AA:BB";
+    private static final byte[] TEST_DATA = "testtest".getBytes();
+    private static final int TEST_SINGLE_MESSAGE_SIZE =
+            TEST_DATA.length + BLEMessageV1Factory.getProtoHeaderSize(OperationType.CLIENT_MESSAGE,
+                    true);
+
+    private CarTrustAgentBleManager mCarTrustAgentBleManager;
+    private BluetoothDevice mBluetoothDevice;
+    private Context mContext;
+
+    @Before
+    public void setUp() {
+        mContext = InstrumentationRegistry.getTargetContext();
+        mCarTrustAgentBleManager = new CarTrustAgentBleManager(mContext);
+        mCarTrustAgentBleManager.setupEnrollmentBleServer();
+        mCarTrustAgentBleManager.setBleMessageRetryLimit(3);
+        mBluetoothDevice = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(ADDRESS);
+    }
+
+    @Test
+    public void testRetrySendingMessage_noACK_reachRetryLimit() throws InterruptedException {
+        // Make sure the length of the message queue is greater than 1.
+        mCarTrustAgentBleManager.onMtuSizeChanged(TEST_SINGLE_MESSAGE_SIZE - 1);
+        mCarTrustAgentBleManager.sendEnrollmentMessage(mBluetoothDevice, TEST_DATA,
+                OperationType.CLIENT_MESSAGE, true);
+        for (int i = 0; i < mCarTrustAgentBleManager.mBleMessageRetryLimit; i++) {
+            Thread.sleep(mCarTrustAgentBleManager.BLE_MESSAGE_RETRY_DELAY_MS);
+            assertThat(mCarTrustAgentBleManager.mBleMessageRetryStartCount).isEqualTo(i + 1);
+        }
+    }
+
+    @Test
+    public void testRetrySendingMessage_receivedACK_stopRetry() throws InterruptedException {
+        mCarTrustAgentBleManager.onMtuSizeChanged(TEST_SINGLE_MESSAGE_SIZE - 1);
+        mCarTrustAgentBleManager.sendEnrollmentMessage(mBluetoothDevice, TEST_DATA,
+                OperationType.CLIENT_MESSAGE, true);
+        Thread.sleep(mCarTrustAgentBleManager.BLE_MESSAGE_RETRY_DELAY_MS);
+        // Retried once.
+        assertThat(mCarTrustAgentBleManager.mBleMessageRetryStartCount).isEqualTo(1);
+
+        // Receive acknowledge back.
+        mCarTrustAgentBleManager.handleClientAckMessage(mBluetoothDevice,
+                UUID.fromString(mContext.getString(R.string.enrollment_client_write_uuid)));
+        assertThat(mCarTrustAgentBleManager.mBleMessageRetryStartCount).isEqualTo(0);
+    }
+}
