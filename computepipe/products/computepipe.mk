79fe3004bd87ea41f99c9f8f541a1d1f85ff62af
Hanumant Singh <hanumantsingh@google.com>
2019-11-18 17:04:44 -0800

    Add selinux policies for computepipe services
    Enable computepipe for products.
    
    Bug: 144312729
    Change-Id: I9ac806d5244b19e50c9f8268ce1a8abca349ef11

---
+++ computepipe/products/computepipe.mk
@@ -0,0 +1,25 @@
+# Copyright (C) 2019 The Android Open Source Project
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Enable computepipe interface
+PRODUCT_PACKAGES += android.automotive.computepipe.router@1.0-impl
+
+# Enable computepipe router
+PRODUCT_PACKAGES += android.automotive.computepipe.router@1.0
+
+# Selinux public policies for computepipe services
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += packages/services/Car/computepipe/sepolicy/public
+
+# Selinux private policies for computepipe services
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += packages/services/Car/computepipe/sepolicy/private
