4792f64e6481f5f0b406fce2955fca976663b9bf
Yuncheol Heo <ycheo@google.com>
2020-01-10 12:13:02 -0800

    Add log messages to track the lifecycle into CarCtsDummyLauncher.
    The CL applies the cherry-pick of (ag/9853726) into CarCtsDummyLauncher.
    
    Bug: 147293343
    Test: Check the log messages manually.
    Change-Id: I58d77e8b13a9c89db74cb6b9e51c016a9cf32a7c

--- tests/CarCtsDummyLauncher/Android.mk
+++ tests/CarCtsDummyLauncher/Android.mk
@@ -17,17 +17,15 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := tests
+LOCAL_MODULE_TAGS := optional
 
 # Only compile source java files in this apk.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CarCtsDummyLauncher
 
-LOCAL_SDK_VERSION := current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
-LOCAL_DEX_PREOPT := false
-
-LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 include $(BUILD_PACKAGE)
