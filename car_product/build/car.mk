0a0a7847beb0f05fdbdcc2bd8cc3d478bd47e379
Ulya Trafimovich <skvadrik@google.com>
2020-01-29 12:16:16 +0000

    Drop useless check for PRODUCT_BOOT_JARS redefinition.
    It is not clear why this check exists; it seems to be unneeded, and
    removing it does not break any tests.
    
    PRODUCT_BOOT_JARS is a product variable, and therefore handled in a
    special way (unlike ordinary Make variables). It is substituted with
    a macro that inherits makefile-specific instances of this variable
    and merges them into one. This can be seen by adding debug output
    to the device-specific makefile.
    
    For example, add the following to device/generic/car/aosp_car_arm.mk:
    
      $(warning $(PRODUCT_BOOT_JARS))
    
    The build will generate a warning similar to this:
    
      device/generic/car/aosp_car_arm.mk:21: warning: @inherit:build/make/target/product/aosp_arm.mk @inherit:device/generic/car/emulator/aosp_car_emulator.mk
    
    Test: lunch aosp_car_arm && m
    
    Change-Id: Ib5e09c6624dbd4db29105c67543a53caa490b818
    Merged-In: I061b70b751ca00efa8f215dba8419d2b9e920403

--- car_product/build/car.mk
+++ car_product/build/car.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
     libcar-framework-service-jni \
 
 # System Server components
+# Order is important: if X depends on Y, then Y should precede X on the list.
 PRODUCT_SYSTEM_SERVER_JARS += car-frameworks-service
 
 # Boot animation
@@ -110,8 +111,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_LOCALES := en_US af_ZA am_ET ar_EG bg_BG bn_BD ca_ES cs_CZ da_DK de_DE el_GR en_AU en_GB en_IN es_ES es_US et_EE eu_ES fa_IR fi_FI fr_CA fr_FR gl_ES hi_IN hr_HR hu_HU hy_AM in_ID is_IS it_IT iw_IL ja_JP ka_GE km_KH ko_KR ky_KG lo_LA lt_LT lv_LV km_MH kn_IN mn_MN ml_IN mk_MK mr_IN ms_MY my_MM ne_NP nb_NO nl_NL pl_PL pt_BR pt_PT ro_RO ru_RU si_LK sk_SK sl_SI sr_RS sv_SE sw_TZ ta_IN te_IN th_TH tl_PH tr_TR uk_UA vi_VN zh_CN zh_HK zh_TW zu_ZA en_XA ar_XB
 
-# should add to BOOT_JARS only once
-ifeq (,$(INCLUDED_ANDROID_CAR_TO_PRODUCT_BOOT_JARS))
 PRODUCT_BOOT_JARS += \
     android.car
 
@@ -123,6 +122,3 @@ PRODUCT_HIDDENAPI_STUBS_SYSTEM := \
 
 PRODUCT_HIDDENAPI_STUBS_TEST := \
     android.car-test-stubs
-
-INCLUDED_ANDROID_CAR_TO_PRODUCT_BOOT_JARS := yes
-endif
