131e309a014c714064a25a2f66688e3177905c82
Linus Tufvesson <lus@google.com>
2020-04-09 15:51:40 +0100

    Explicitly request a public VD CarActivityView
    Bug: 150310377
    Bug: 151639049
    Test: atest CtsWindowManagerDeviceTestCases:ActivityViewTest (on gcar_emu_x86-userdebug)
    Change-Id: I391836d2f7754a8e34c2f5040c1db8a690f7f9e6

--- car-lib/src/android/car/app/CarActivityView.java
+++ car-lib/src/android/car/app/CarActivityView.java
@@ -57,7 +57,7 @@ public final class CarActivityView extends ActivityView {
 
     public CarActivityView(
             Context context, AttributeSet attrs, int defStyle, boolean singleTaskInstance) {
-        super(context, attrs, defStyle, singleTaskInstance);
+        super(context, attrs, defStyle, singleTaskInstance, /*usePublicVirtualDisplay=*/ true);
         super.setCallback(new CarActivityViewCallback());
         Car.createCar(mContext, /*handler=*/ null,
                 Car.CAR_WAIT_TIMEOUT_DO_NOT_WAIT,
