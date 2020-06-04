572023abfebdee782087cec1ed18d6f0cb9c8ab3
Keun young Park <keunyoung@google.com>
2020-06-03 17:29:36 -0700

    Add missing permission check to CarPackageManager.restartTask
    - As it can affect other apps, it should be protected with right
      permission: Add REAL_GET_TASKS permission check as client should
      access task id with APIs requiring the permission.
    - Added permission check tests
    - Also renamed other permission tests to have *PermissionTest name
    
    Bug: 154964781
    Test: atest CarSecurityPermissionTest
    Change-Id: I16dad3754253aa1a98424e7ea0c3b70fc4c9772f
    (cherry picked from commit 22dc65b399862383f74dd7ad169c08083679c2c4)

--- tests/CarSecurityPermissionTest/src/com/android/car/CarPermisisonTest.java
+++ tests/CarSecurityPermissionTest/src/com/android/car/CarPermisisonTest.java
@@ -13,14 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.car.input;
-
-import static com.google.common.truth.Truth.assertThat;
+package com.android.car;
 
 import static org.testng.Assert.assertThrows;
 
 import android.car.Car;
-import android.car.input.CarInputManager;
+import android.os.Handler;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -29,27 +27,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 
 /**
- * This class contains security permission tests for the {@link CarInputManager}'s system APIs.
+ * This class contains security permission tests for the {@link Car}'s system APIs.
  */
 @RunWith(AndroidJUnit4.class)
-public class CarInputManagerTest {
-    private Car mCar;
-
-    private CarInputManager mCarInputManager;
-
-    @Mock
-    private CarInputManager.CarInputCaptureCallback mMockedCallback;
+public class CarPermisisonTest {
+    private Car mCar = null;
 
     @Before
     public void setUp() throws Exception {
         mCar = Car.createCar(
-                InstrumentationRegistry.getInstrumentation().getTargetContext());
-        assertThat(mCar).isNotNull();
-        mCarInputManager = (CarInputManager) mCar.getCarManager(Car.CAR_INPUT_SERVICE);
-        assertThat(mCarInputManager).isNotNull();
+                InstrumentationRegistry.getInstrumentation().getTargetContext(), (Handler) null);
     }
 
     @After
@@ -59,10 +48,26 @@ public class CarInputManagerTest {
 
     @Test
     public void testEnableFeaturePermission() throws Exception {
-        assertThrows(SecurityException.class, () -> mCarInputManager.requestInputEventCapture(
-                mMockedCallback,
-                CarInputManager.TARGET_DISPLAY_TYPE_MAIN,
-                new int[]{CarInputManager.INPUT_TYPE_ROTARY_NAVIGATION}, 0));
+        assertThrows(SecurityException.class, () -> mCar.enableFeature("some feature"));
+    }
+
+    @Test
+    public void testDisableFeaturePermission() throws Exception {
+        assertThrows(SecurityException.class, () -> mCar.disableFeature("some feature"));
+    }
+
+    @Test
+    public void testGetAllEnabledFeaturesPermission() throws Exception {
+        assertThrows(SecurityException.class, () -> mCar.getAllEnabledFeatures());
+    }
+
+    @Test
+    public void testGetAllPendingDisabledFeaturesPermission() throws Exception {
+        assertThrows(SecurityException.class, () -> mCar.getAllPendingDisabledFeatures());
+    }
+
+    @Test
+    public void testGetAllPendingEnabledFeaturesPermission() throws Exception {
+        assertThrows(SecurityException.class, () -> mCar.getAllPendingEnabledFeatures());
     }
 }
-
