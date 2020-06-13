ab9ca58fc89dd7934e400bbbbb6b077ea62d7dc5
Jordan Jozwiak <jjoz@google.com>
2020-06-12 18:01:40 -0700

    Fix map of virtual displays to physical ports
    mPortLookup is map of display ids to physical ports. For virtual
    displays we had incorrectly saved a map of the virtual display's
    id to its physical display id.
    
    This wasn't caught earlier because most of our test devices seem
    to use the same id for the display and physical port.
    
    Bug: 154263657
    Test: atest CarUxRestrictionsManagerServiceTest
    Change-Id: I1ba01c3277fcb2406c96dcef76ae7ec0d80b8059
    Merged-In: I1ba01c3277fcb2406c96dcef76ae7ec0d80b8059

--- service/src/com/android/car/CarUxRestrictionsManagerService.java
+++ service/src/com/android/car/CarUxRestrictionsManagerService.java
@@ -1152,7 +1152,13 @@ public class CarUxRestrictionsManagerService extends ICarUxRestrictionsManager.S
             mRemoteCallbackList.register(callback);
             mActivityViewDisplayInfoMap.put(virtualDisplayId,
                     new DisplayInfo(callback, physicalDisplayId));
-            mPortLookup.put(virtualDisplayId, (byte) physicalDisplayId);
+            Byte physicalPort = getPhysicalPortLocked(physicalDisplayId);
+            if (physicalPort == null) {
+                // This should not happen.
+                Log.wtf(TAG, "No known physicalPort for displayId:" + physicalDisplayId);
+                physicalPort = mDefaultDisplayPhysicalPort;
+            }
+            mPortLookup.put(virtualDisplayId, physicalPort);
         }
     }
 
