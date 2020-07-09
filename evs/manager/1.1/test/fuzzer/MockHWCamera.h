292c7dd99fc3ecee7f307cbc7ed0f9c71c2b57d5
Zhaoming Yin <zhaomingyin@google.com>
2020-07-09 14:40:41 -0700

    Add VirtualCameraFuzzer
    Bug: 160801430
    Test: Follow go/android-fuzzing to build and test
    
    Change-Id: Ide2e7f31d08859e1c655c32470891aa7d67935d0

--- evs/manager/1.1/test/fuzzer/MockHWCamera.h
+++ evs/manager/1.1/test/fuzzer/MockHWCamera.h
@@ -29,7 +29,7 @@ public:
     MockHWCamera() = default;
 
     // v1.0 methods
-    MOCK_METHOD(Return<void>, getCameraInfo, (getCameraInfo_cb _hidl_cb), (override));
+    Return<void> getCameraInfo(getCameraInfo_cb _hidl_cb) override { return {}; }
     Return<EvsResult> setMaxFramesInFlight(uint32_t bufferCount) override {
         if (bufferCount > 1024) {
             return EvsResult::INVALID_ARG;
@@ -39,14 +39,21 @@ public:
     Return<EvsResult> startVideoStream(const ::android::sp<IEvsCameraStream_1_0>& stream) override {
         return EvsResult::OK;
     }
-    Return<void> doneWithFrame(const BufferDesc_1_0& buffer) override { return Void(); }
-    Return<void> stopVideoStream() override { return Void(); }
-    MOCK_METHOD(Return<int32_t>, getExtendedInfo, (uint32_t opaqueIdentifier), (override));
-    MOCK_METHOD(Return<EvsResult>, setExtendedInfo,
-                (uint32_t opaqueIdentifier, int32_t opaqueValue), (override));
+    Return<void> doneWithFrame(const BufferDesc_1_0& buffer) override { return {}; }
+    Return<void> stopVideoStream() override { return {}; }
+    Return<int32_t> getExtendedInfo(uint32_t opaqueIdentifier) override {
+        if (mExtendedInfo.find(opaqueIdentifier) != mExtendedInfo.end()) {
+            return mExtendedInfo[opaqueIdentifier];
+        }
+        return 0;
+    }
+    Return<EvsResult> setExtendedInfo(uint32_t opaqueIdentifier, int32_t opaqueValue) override {
+        mExtendedInfo[opaqueIdentifier] = opaqueValue;
+        return EvsResult::OK;
+    }
 
     // v1.1 methods
-    MOCK_METHOD(Return<void>, getCameraInfo_1_1, (getCameraInfo_1_1_cb _hidl_cb), (override));
+    Return<void> getCameraInfo_1_1(getCameraInfo_1_1_cb _hidl_cb) override { return {}; }
     MOCK_METHOD(Return<void>, getPhysicalCameraInfo,
                 (const hidl_string& deviceId, getPhysicalCameraInfo_cb _hidl_cb), (override));
     MOCK_METHOD(Return<EvsResult>, pauseVideoStream, (), (override));
@@ -57,9 +64,10 @@ public:
     MOCK_METHOD(Return<EvsResult>, setMaster, (), (override));
     MOCK_METHOD(Return<EvsResult>, forceMaster, (const sp<IEvsDisplay_1_0>& display), (override));
     MOCK_METHOD(Return<EvsResult>, unsetMaster, (), (override));
-    MOCK_METHOD(Return<void>, getParameterList, (getParameterList_cb _hidl_cb), (override));
-    MOCK_METHOD(Return<void>, getIntParameterRange,
-                (CameraParam id, getIntParameterRange_cb _hidl_cb), (override));
+    Return<void> getParameterList(getParameterList_cb _hidl_cb) override { return {}; }
+    Return<void> getIntParameterRange(CameraParam id, getIntParameterRange_cb _hidl_cb) override {
+        return {};
+    }
     Return<void> setIntParameter(CameraParam id, int32_t value,
                                  setIntParameter_cb _hidl_cb) override {
         // FIXME in default implementation, it's _hidl_cb(EvsResult::INVALID_ARG, 0); which will
@@ -67,21 +75,30 @@ public:
         std::vector<int32_t> vec;
         vec.push_back(0);
         _hidl_cb(EvsResult::INVALID_ARG, vec);
-        return Void();
+        return {};
     }
 
     Return<void> getIntParameter(CameraParam id, getIntParameter_cb _hidl_cb) {
         _hidl_cb(EvsResult::INVALID_ARG, 0);
-        return Void();
+        return {};
     }
 
-    MOCK_METHOD(Return<void>, getExtendedInfo_1_1,
-                (uint32_t opaqueIdentifier, getExtendedInfo_1_1_cb _hidl_cb), (override));
-    MOCK_METHOD(Return<EvsResult>, setExtendedInfo_1_1,
-                (uint32_t opaqueIdentifier, const hidl_vec<uint8_t>& opaqueValue), (override));
-    MOCK_METHOD(Return<void>, importExternalBuffers,
-                (const hidl_vec<BufferDesc_1_1>& buffers, importExternalBuffers_cb _hidl_cb),
-                (override));
+    Return<void> getExtendedInfo_1_1(uint32_t opaqueIdentifier,
+                                     getExtendedInfo_1_1_cb _hidl_cb) override {
+        return {};
+    }
+    Return<EvsResult> setExtendedInfo_1_1(uint32_t opaqueIdentifier,
+                                          const hidl_vec<uint8_t>& opaqueValue) override {
+        return EvsResult::OK;
+    }
+    Return<void> importExternalBuffers(const hidl_vec<BufferDesc_1_1>& buffers,
+                                       importExternalBuffers_cb _hidl_cb) override {
+        return {};
+    }
+
+private:
+    std::map<uint32_t, int32_t> mExtendedInfo;
+    std::map<uint32_t, int32_t> mExtendedInfo_1_1;
 };
 
 }  // namespace implementation
