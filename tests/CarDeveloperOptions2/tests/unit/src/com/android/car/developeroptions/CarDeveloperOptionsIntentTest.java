59575eb2b0eb6e934392df34b756774f620d8260
Alex Stetson <alexstetson@google.com>
2020-08-07 12:45:59 -0700

    Create direct fork of developer options
    Rather than having a static clone of phone developer options, this
    implementation compiles the current version of phone settings with all
    non-developer options intents removed.
    
    CarDeveloperOptions2 will replace CarDeveloperOptions in a future CL.
    
    Bug: 150385483
    Test: manual, atest CarDeveloperOptionsUnitTests
    Change-Id: Ie9539d2e5553d89ca49d94f45af8eef3da56b20c

---
+++ tests/CarDeveloperOptions2/tests/unit/src/com/android/car/developeroptions/CarDeveloperOptionsIntentTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.developeroptions;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(AndroidJUnit4.class)
+public class CarDeveloperOptionsIntentTest {
+    private static final List<String> ACTIVITY_ALLOWLIST = Arrays.asList(
+            "com.android.car.developeroptions.CarDevelopmentSettingsDashboardActivity",
+            "com.android.settings.development.DevelopmentSettingsDisabledActivity");
+
+    private Context mContext = ApplicationProvider.getApplicationContext();
+    private PackageManager mPm;
+
+    @Before
+    public void setup() {
+        mPm = mContext.getPackageManager();
+    }
+
+    @Test
+    public void testAvailableIntentActivities_onlyAllowlisted() {
+        List<ResolveInfo> results = mPm.queryIntentActivities(createSettingsPackageIntent(),
+                PackageManager.MATCH_ALL);
+        for (ResolveInfo resolved : results) {
+            assertThat(ACTIVITY_ALLOWLIST).contains(resolved.getComponentInfo().name);
+        }
+    }
+
+    @Test
+    public void testAvailableIntentServices_returnsZero() {
+        List<ResolveInfo> results = mPm.queryIntentServices(createSettingsPackageIntent(),
+                PackageManager.MATCH_ALL);
+        assertThat(results.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testAvailableBroadcastReceivers_returnsZero() {
+        List<ResolveInfo> results = mPm.queryBroadcastReceivers(createSettingsPackageIntent(),
+                PackageManager.MATCH_ALL);
+        assertThat(results.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testAvailableContentProviders_returnsZero() {
+        List<ResolveInfo> results = mPm.queryIntentContentProviders(createSettingsPackageIntent(),
+                PackageManager.MATCH_ALL);
+        assertThat(results.size()).isEqualTo(0);
+    }
+
+    private Intent createSettingsPackageIntent() {
+        Intent intent = new Intent();
+        intent.setPackage("com.android.settings");
+        return intent;
+    }
+}
