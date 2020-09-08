4b180f7a2866227ddc2d1923612d1bcf5992ee21
Changyeon Jo <changyeon@google.com>
2020-09-08 13:20:09 -0700

    Build EVS manager as a part of the car product
    This change modifies car_product/build/car_base.mk to build EVS manager
    v1.1 by default.  To do this, this reorganizes EVS selinux policy files
    and places them in each component's directories.
    
    And, this change introduces a new build variable, ENABLE_EVS_SAMPLE.
    When this variable is set as true, the reference EVS application and EVS
    HAL implementation will be built.
    
    Also, this change replaces a few selinux rules with their equivalent
    macros.
    
    Bug: 168017200
    Test: Build, flash the image, and manually check audit messages on the
          device running in the permissive mode while below executables are
          running:
          - /system/bin/evs_app
          - /system/bin/android.automotive.evs.manager@1.1
          - /vendor/bin/android.hardware.automotive.evs@1.1-sample
    Change-Id: Ieffabcba6e05e7b20f0282d1346d77e87553fb6a

--- cpp/evs/sepolicy/evs.mk
+++
@@ -1,3 +0,0 @@
-# Extended View System Selinux Policies
-PRODUCT_PUBLIC_SEPOLICY_DIRS += packages/services/Car/cpp/evs/sepolicy/public
-PRODUCT_PRIVATE_SEPOLICY_DIRS += packages/services/Car/cpp/evs/sepolicy/private
