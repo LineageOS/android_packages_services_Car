aa48cf14be201c0f5ee0bf72ab6008f20c5ece40
Lakshman Annadorai <lakshmana@google.com>
2020-09-16 16:29:59 -0700

    Remove unnessary intermediate UidIoStats structs.
    This clean up work makes upcoming changes much cleaner.
    
    Test: Tested with existing unit tests.
    Bug: 167240592, 158125958
    
    Change-Id: I9cec201c029964b4a20add00cc9bbf9ec65f2dc6

--- cpp/watchdog/server/src/UidIoStats.cpp
+++ cpp/watchdog/server/src/UidIoStats.cpp
@@ -44,26 +44,42 @@ using android::base::StringPrintf;
 
 namespace {
 
-bool parseUidIoStats(const std::string& data, UidIoStat* uidIoStat) {
+bool parseUidIoStats(const std::string& data, UidIoUsage* usage) {
     std::vector<std::string> fields = Split(data, " ");
-    if (fields.size() < 11 || !ParseUint(fields[0], &uidIoStat->uid) ||
-        !ParseUint(fields[1], &uidIoStat->io[FOREGROUND].rchar) ||
-        !ParseUint(fields[2], &uidIoStat->io[FOREGROUND].wchar) ||
-        !ParseUint(fields[3], &uidIoStat->io[FOREGROUND].readBytes) ||
-        !ParseUint(fields[4], &uidIoStat->io[FOREGROUND].writeBytes) ||
-        !ParseUint(fields[5], &uidIoStat->io[BACKGROUND].rchar) ||
-        !ParseUint(fields[6], &uidIoStat->io[BACKGROUND].wchar) ||
-        !ParseUint(fields[7], &uidIoStat->io[BACKGROUND].readBytes) ||
-        !ParseUint(fields[8], &uidIoStat->io[BACKGROUND].writeBytes) ||
-        !ParseUint(fields[9], &uidIoStat->io[FOREGROUND].fsync) ||
-        !ParseUint(fields[10], &uidIoStat->io[BACKGROUND].fsync)) {
+    if (fields.size() < 11 || !ParseUint(fields[0], &usage->uid) ||
+        !ParseUint(fields[3], &usage->ios.metrics[READ_BYTES][FOREGROUND]) ||
+        !ParseUint(fields[4], &usage->ios.metrics[WRITE_BYTES][FOREGROUND]) ||
+        !ParseUint(fields[7], &usage->ios.metrics[READ_BYTES][BACKGROUND]) ||
+        !ParseUint(fields[8], &usage->ios.metrics[WRITE_BYTES][BACKGROUND]) ||
+        !ParseUint(fields[9], &usage->ios.metrics[FSYNC_COUNT][FOREGROUND]) ||
+        !ParseUint(fields[10], &usage->ios.metrics[FSYNC_COUNT][BACKGROUND])) {
         ALOGW("Invalid uid I/O stats: \"%s\"", data.c_str());
         return false;
     }
     return true;
 }
 
+uint64_t maybeDiff(uint64_t lhs, uint64_t rhs) {
+    return lhs > rhs ? lhs - rhs : 0;
+}
+
 }  // namespace
+
+IoUsage& IoUsage::operator-=(const IoUsage& rhs) {
+    metrics[READ_BYTES][FOREGROUND] =
+            maybeDiff(metrics[READ_BYTES][FOREGROUND], rhs.metrics[READ_BYTES][FOREGROUND]);
+    metrics[READ_BYTES][BACKGROUND] =
+            maybeDiff(metrics[READ_BYTES][BACKGROUND], rhs.metrics[READ_BYTES][BACKGROUND]);
+    metrics[WRITE_BYTES][FOREGROUND] =
+            maybeDiff(metrics[WRITE_BYTES][FOREGROUND], rhs.metrics[WRITE_BYTES][FOREGROUND]);
+    metrics[WRITE_BYTES][BACKGROUND] =
+            maybeDiff(metrics[WRITE_BYTES][BACKGROUND], rhs.metrics[WRITE_BYTES][BACKGROUND]);
+    metrics[FSYNC_COUNT][FOREGROUND] =
+            maybeDiff(metrics[FSYNC_COUNT][FOREGROUND], rhs.metrics[FSYNC_COUNT][FOREGROUND]);
+    metrics[FSYNC_COUNT][BACKGROUND] =
+            maybeDiff(metrics[FSYNC_COUNT][BACKGROUND], rhs.metrics[FSYNC_COUNT][BACKGROUND]);
+    return *this;
+}
 
 bool IoUsage::isZero() const {
     for (int i = 0; i < METRIC_TYPES; i++) {
@@ -90,63 +106,44 @@ Result<std::unordered_map<uid_t, UidIoUsage>> UidIoStats::collect() {
     }
 
     Mutex::Autolock lock(mMutex);
-    const auto& uidIoStats = getUidIoStatsLocked();
-    if (!uidIoStats.ok() || uidIoStats->empty()) {
-        return Error() << "Failed to get UID IO stats: " << uidIoStats.error();
+    const auto& uidIoUsages = getUidIoUsagesLocked();
+    if (!uidIoUsages.ok() || uidIoUsages->empty()) {
+        return Error() << "Failed to get UID IO stats: " << uidIoUsages.error();
     }
 
     std::unordered_map<uid_t, UidIoUsage> usage;
-    for (const auto& it : *uidIoStats) {
-        const UidIoStat& uidIoStat = it.second;
-        usage[uidIoStat.uid] = {};
-        struct UidIoUsage& uidUsage = usage[uidIoStat.uid];
-        uidUsage.uid = uidIoStat.uid;
-
-        int64_t fgRdDelta = uidIoStat.io[FOREGROUND].readBytes -
-                            mLastUidIoStats[uidIoStat.uid].io[FOREGROUND].readBytes;
-        int64_t bgRdDelta = uidIoStat.io[BACKGROUND].readBytes -
-                            mLastUidIoStats[uidIoStat.uid].io[BACKGROUND].readBytes;
-        int64_t fgWrDelta = uidIoStat.io[FOREGROUND].writeBytes -
-                            mLastUidIoStats[uidIoStat.uid].io[FOREGROUND].writeBytes;
-        int64_t bgWrDelta = uidIoStat.io[BACKGROUND].writeBytes -
-                            mLastUidIoStats[uidIoStat.uid].io[BACKGROUND].writeBytes;
-        int64_t fgFsDelta =
-            uidIoStat.io[FOREGROUND].fsync - mLastUidIoStats[uidIoStat.uid].io[FOREGROUND].fsync;
-        int64_t bgFsDelta =
-            uidIoStat.io[BACKGROUND].fsync - mLastUidIoStats[uidIoStat.uid].io[BACKGROUND].fsync;
-
-        uidUsage.ios.metrics[READ_BYTES][FOREGROUND] += (fgRdDelta < 0) ? 0 : fgRdDelta;
-        uidUsage.ios.metrics[READ_BYTES][BACKGROUND] += (bgRdDelta < 0) ? 0 : bgRdDelta;
-        uidUsage.ios.metrics[WRITE_BYTES][FOREGROUND] += (fgWrDelta < 0) ? 0 : fgWrDelta;
-        uidUsage.ios.metrics[WRITE_BYTES][BACKGROUND] += (bgWrDelta < 0) ? 0 : bgWrDelta;
-        uidUsage.ios.metrics[FSYNC_COUNT][FOREGROUND] += (fgFsDelta < 0) ? 0 : fgFsDelta;
-        uidUsage.ios.metrics[FSYNC_COUNT][BACKGROUND] += (bgFsDelta < 0) ? 0 : bgFsDelta;
+    for (const auto& it : *uidIoUsages) {
+        const UidIoUsage& curUsage = it.second;
+        usage[it.first] = curUsage;
+        if (mLastUidIoUsages.find(it.first) != mLastUidIoUsages.end()) {
+            usage[it.first] -= mLastUidIoUsages[it.first];
+        }
     }
-    mLastUidIoStats = *uidIoStats;
+    mLastUidIoUsages = *uidIoUsages;
     return usage;
 }
 
-Result<std::unordered_map<uid_t, UidIoStat>> UidIoStats::getUidIoStatsLocked() const {
+Result<std::unordered_map<uid_t, UidIoUsage>> UidIoStats::getUidIoUsagesLocked() const {
     std::string buffer;
     if (!ReadFileToString(kPath, &buffer)) {
         return Error() << "ReadFileToString failed for " << kPath;
     }
 
     std::vector<std::string> ioStats = Split(std::move(buffer), "\n");
-    std::unordered_map<uid_t, UidIoStat> uidIoStats;
-    UidIoStat uidIoStat;
+    std::unordered_map<uid_t, UidIoUsage> uidIoUsages;
+    UidIoUsage usage;
     for (size_t i = 0; i < ioStats.size(); i++) {
         if (ioStats[i].empty() || !ioStats[i].compare(0, 4, "task")) {
             // Skip per-task stats as CONFIG_UID_SYS_STATS_DEBUG is not set in the kernel and
             // the collected data is aggregated only per-UID.
             continue;
         }
-        if (!parseUidIoStats(std::move(ioStats[i]), &uidIoStat)) {
+        if (!parseUidIoStats(std::move(ioStats[i]), &usage)) {
             return Error() << "Failed to parse the contents of " << kPath;
         }
-        uidIoStats[uidIoStat.uid] = uidIoStat;
+        uidIoUsages[usage.uid] = usage;
     }
-    return uidIoStats;
+    return uidIoUsages;
 }
 
 }  // namespace watchdog
