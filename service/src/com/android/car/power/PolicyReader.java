0d393abb1e211d2b485ac9a74f68ffb2185c0cf5
Eric Jeong <ericjeong@google.com>
2020-11-20 15:18:07 -0800

    Add location/microphone component to PowerComponent
    - Controlling location is decided to be done in GNSS HAL.
    - So, location power change needs to be notified.
    - Besides audio input, microphone is separately added.
    
    Bug: 173733058
    Test: manual test
    Change-Id: Ic022b0a9752a7c7c9eaa8b84bf303b014ad417c1

--- service/src/com/android/car/power/PolicyReader.java
+++ service/src/com/android/car/power/PolicyReader.java
@@ -16,6 +16,12 @@
 
 package com.android.car.power;
 
+import static com.android.car.power.PowerComponentUtil.FIRST_POWER_COMPONENT;
+import static com.android.car.power.PowerComponentUtil.INVALID_POWER_COMPONENT;
+import static com.android.car.power.PowerComponentUtil.LAST_POWER_COMPONENT;
+import static com.android.car.power.PowerComponentUtil.powerComponentToString;
+import static com.android.car.power.PowerComponentUtil.toPowerComponent;
+
 import static org.xmlpull.v1.XmlPullParser.END_DOCUMENT;
 import static org.xmlpull.v1.XmlPullParser.END_TAG;
 import static org.xmlpull.v1.XmlPullParser.START_TAG;
@@ -55,8 +61,6 @@ import java.util.stream.Collectors;
  * class is not thread-safe, and must be used in the main thread or with additional serialization.
  */
 final class PolicyReader {
-    static final int INVALID_POWER_COMPONENT = -1;
-
     private static final String TAG = PolicyReader.class.getSimpleName();
     private static final String VENDOR_POLICY_PATH = "/vendor/etc/power_policy.xml";
 
@@ -80,27 +84,7 @@ final class PolicyReader {
     private static final String POWER_ONOFF_OFF = "off";
     private static final String POWER_ONOFF_UNTOUCHED = "untouched";
 
-    private static final String POWER_COMPONENT_PREFIX = "POWER_COMPONENT_";
     private static final int INVALID_POWER_STATE = -1;
-
-    private static final String POWER_COMPONENT_AUDIO = "AUDIO";
-    private static final String POWER_COMPONENT_MEDIA = "MEDIA";
-    private static final String POWER_COMPONENT_DISPLAY_MAIN = "DISPLAY_MAIN";
-    private static final String POWER_COMPONENT_DISPLAY_CLUSTER = "DISPLAY_CLUSTER";
-    private static final String POWER_COMPONENT_DISPLAY_FRONT_PASSENGER = "DISPLAY_FRONT_PASSENGER";
-    private static final String POWER_COMPONENT_DISPLAY_REAR_PASSENGER = "DISPLAY_REAR_PASSENGER";
-    private static final String POWER_COMPONENT_BLUETOOTH = "BLUETOOTH";
-    private static final String POWER_COMPONENT_WIFI = "WIFI";
-    private static final String POWER_COMPONENT_CELLULAR = "CELLULAR";
-    private static final String POWER_COMPONENT_ETHERNET = "ETHERNET";
-    private static final String POWER_COMPONENT_PROJECTION = "PROJECTION";
-    private static final String POWER_COMPONENT_NFC = "NFC";
-    private static final String POWER_COMPONENT_INPUT = "INPUT";
-    private static final String POWER_COMPONENT_VOICE_INTERACTION = "VOICE_INTERACTION";
-    private static final String POWER_COMPONENT_VISUAL_INTERACTION = "VISUAL_INTERACTION";
-    private static final String POWER_COMPONENT_TRUSTED_DEVICE_DETECTION =
-            "TRUSTED_DEVICE_DETECTION";
-
     private static final String POWER_STATE_WAIT_FOR_VHAL = "WaitForVHAL";
     private static final String POWER_STATE_ON = "On";
     private static final String POWER_STATE_DEEP_SLEEP_ENTRY = "DeepSleepEntry";
@@ -117,7 +101,8 @@ final class PolicyReader {
             PowerComponent.DISPLAY_CLUSTER, PowerComponent.DISPLAY_FRONT_PASSENGER,
             PowerComponent.DISPLAY_REAR_PASSENGER, PowerComponent.BLUETOOTH,
             PowerComponent.PROJECTION, PowerComponent.NFC, PowerComponent.INPUT,
-            PowerComponent.VOICE_INTERACTION, PowerComponent.VISUAL_INTERACTION
+            PowerComponent.VOICE_INTERACTION, PowerComponent.VISUAL_INTERACTION,
+            PowerComponent.LOCATION, PowerComponent.MICROPHONE
     };
     private static final Set<Integer> SYSTEM_POLICY_CONFIGURABLE_COMPONENTS =
             new ArraySet<>(Arrays.asList(PowerComponent.BLUETOOTH, PowerComponent.NFC,
@@ -411,8 +396,8 @@ final class PolicyReader {
             untouched = true;
         }
         if (!untouched) {
-            for (int component = PowerComponent.AUDIO;
-                    component <= PowerComponent.TRUSTED_DEVICE_DETECTION; component++) {
+            for (int component = FIRST_POWER_COMPONENT;
+                    component <= LAST_POWER_COMPONENT; component++) {
                 if (components.indexOfKey(component) >= 0) continue;
                 components.put(component, enabled);
             }
@@ -636,98 +621,6 @@ final class PolicyReader {
             components.put(component, enabled);
         }
         return null;
-    }
-
-    static boolean isValidPowerComponent(int component) {
-        return component >= PowerComponent.AUDIO
-                && component <= PowerComponent.TRUSTED_DEVICE_DETECTION;
-    }
-
-    static int toPowerComponent(String component, boolean prefix) {
-        if (component == null) {
-            return INVALID_POWER_COMPONENT;
-        }
-        if (prefix) {
-            if (!component.startsWith(POWER_COMPONENT_PREFIX)) {
-                return INVALID_POWER_COMPONENT;
-            }
-            component = component.substring(POWER_COMPONENT_PREFIX.length());
-        }
-        switch (component) {
-            case POWER_COMPONENT_AUDIO:
-                return PowerComponent.AUDIO;
-            case POWER_COMPONENT_MEDIA:
-                return PowerComponent.MEDIA;
-            case POWER_COMPONENT_DISPLAY_MAIN:
-                return PowerComponent.DISPLAY_MAIN;
-            case POWER_COMPONENT_DISPLAY_CLUSTER:
-                return PowerComponent.DISPLAY_CLUSTER;
-            case POWER_COMPONENT_DISPLAY_FRONT_PASSENGER:
-                return PowerComponent.DISPLAY_FRONT_PASSENGER;
-            case POWER_COMPONENT_DISPLAY_REAR_PASSENGER:
-                return PowerComponent.DISPLAY_REAR_PASSENGER;
-            case POWER_COMPONENT_BLUETOOTH:
-                return PowerComponent.BLUETOOTH;
-            case POWER_COMPONENT_WIFI:
-                return PowerComponent.WIFI;
-            case POWER_COMPONENT_CELLULAR:
-                return PowerComponent.CELLULAR;
-            case POWER_COMPONENT_ETHERNET:
-                return PowerComponent.ETHERNET;
-            case POWER_COMPONENT_PROJECTION:
-                return PowerComponent.PROJECTION;
-            case POWER_COMPONENT_NFC:
-                return PowerComponent.NFC;
-            case POWER_COMPONENT_INPUT:
-                return PowerComponent.INPUT;
-            case POWER_COMPONENT_VOICE_INTERACTION:
-                return PowerComponent.VOICE_INTERACTION;
-            case POWER_COMPONENT_VISUAL_INTERACTION:
-                return PowerComponent.VISUAL_INTERACTION;
-            case POWER_COMPONENT_TRUSTED_DEVICE_DETECTION:
-                return PowerComponent.TRUSTED_DEVICE_DETECTION;
-            default:
-                return INVALID_POWER_COMPONENT;
-        }
-    }
-
-    static String powerComponentToString(int component) {
-        switch (component) {
-            case PowerComponent.AUDIO:
-                return POWER_COMPONENT_AUDIO;
-            case PowerComponent.MEDIA:
-                return POWER_COMPONENT_MEDIA;
-            case PowerComponent.DISPLAY_MAIN:
-                return POWER_COMPONENT_DISPLAY_MAIN;
-            case PowerComponent.DISPLAY_CLUSTER:
-                return POWER_COMPONENT_DISPLAY_CLUSTER;
-            case PowerComponent.DISPLAY_FRONT_PASSENGER:
-                return POWER_COMPONENT_DISPLAY_FRONT_PASSENGER;
-            case PowerComponent.DISPLAY_REAR_PASSENGER:
-                return POWER_COMPONENT_DISPLAY_REAR_PASSENGER;
-            case PowerComponent.BLUETOOTH:
-                return POWER_COMPONENT_BLUETOOTH;
-            case PowerComponent.WIFI:
-                return POWER_COMPONENT_WIFI;
-            case PowerComponent.CELLULAR:
-                return POWER_COMPONENT_CELLULAR;
-            case PowerComponent.ETHERNET:
-                return POWER_COMPONENT_ETHERNET;
-            case PowerComponent.PROJECTION:
-                return POWER_COMPONENT_PROJECTION;
-            case PowerComponent.NFC:
-                return POWER_COMPONENT_NFC;
-            case PowerComponent.INPUT:
-                return POWER_COMPONENT_INPUT;
-            case PowerComponent.VOICE_INTERACTION:
-                return POWER_COMPONENT_VOICE_INTERACTION;
-            case PowerComponent.VISUAL_INTERACTION:
-                return POWER_COMPONENT_VISUAL_INTERACTION;
-            case PowerComponent.TRUSTED_DEVICE_DETECTION:
-                return POWER_COMPONENT_TRUSTED_DEVICE_DETECTION;
-            default:
-                return "unknown component";
-        }
     }
 
     private static int[] toIntArray(SparseBooleanArray array, boolean value) {
