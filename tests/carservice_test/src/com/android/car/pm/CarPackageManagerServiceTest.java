05323c9f467782a9fc6c8dd5155abeb3d56fc67f
Mayank Garg <gargmayank@google.com>
2021-01-19 22:21:10 -0800

    Activity allowlist/denylist creation on demand for CarPackageManager
    BYPASS_INCLUSIVE_LANGUAGE_REASON = "back-porting CL"
    
    Bug: 177228254
    Bug: 176856335
    Test: atest CarServiceTest
    
    Change-Id: Ia47942b8e0c8353764097800dd26604c01287d4a
    Merged-In: Ia47942b8e0c8353764097800dd26604c01287d4a
    (cherry picked from commit fde5e661382f3fb87beeec90dbe08dbdf28d077f)

--- tests/carservice_test/src/com/android/car/pm/CarPackageManagerServiceTest.java
+++ tests/carservice_test/src/com/android/car/pm/CarPackageManagerServiceTest.java
@@ -26,7 +26,6 @@ import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.android.car.CarUxRestrictionsManagerService;
 import com.android.car.SystemActivityMonitoringService;
-import com.android.car.user.CarUserService;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -55,15 +54,12 @@ public class CarPackageManagerServiceTest {
     private CarUxRestrictionsManagerService mMockUxrService;
     @Mock
     private SystemActivityMonitoringService mMockSamService;
-    @Mock
-    private CarUserService mMockUserService;
 
     @Before
     public void setUp() {
         mContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        mService = new CarPackageManagerService(mContext, mMockUxrService, mMockSamService,
-                mMockUserService);
+        mService = new CarPackageManagerService(mContext, mMockUxrService, mMockSamService);
     }
 
     @Test
