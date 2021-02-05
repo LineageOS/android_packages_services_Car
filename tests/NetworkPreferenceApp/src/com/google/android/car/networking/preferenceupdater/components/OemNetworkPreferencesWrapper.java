b41bae98e28234778d6a6b252d12cd6d8b9c3210
Serik Beketayev <serikb@google.com>
2021-02-04 19:58:37 -0800

    [NetworkPreferencesApp] Step 2: Create config
    This corresponds to Step 2 from go/aae-pans-refapp
    "Create config overlay to reading initial values from"
    
    Bug: 178245725
    Test: mma & manual
    Change-Id: Ie35d12f079fe72f78790e84fd806db9353c7767a
    Merged-In: Ie35d12f079fe72f78790e84fd806db9353c7767a

--- tests/NetworkPreferenceApp/src/com/google/android/car/networking/preferenceupdater/components/OemNetworkPreferencesWrapper.java
+++ tests/NetworkPreferenceApp/src/com/google/android/car/networking/preferenceupdater/components/OemNetworkPreferencesWrapper.java
@@ -15,8 +15,10 @@
  */
 package com.google.android.car.networking.preferenceupdater.components;
 
+import android.annotation.IntDef;
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.util.SparseArray;
 // import android.net.OemNetworkPreferences;
 
 import java.util.Set;
@@ -25,11 +27,33 @@ import java.util.Set;
 public final class OemNetworkPreferencesWrapper {
     private ConnectivityManager mConnectivityManager;
 
+    // Convert constants from OemNetworkPreferences into enum
+    @IntDef(prefix = { "OEM_NETWORK_PREFERENCE_" }, value = {
+        OEM_NETWORK_PREFERENCE_DEFAULT,
+        OEM_NETWORK_PREFERENCE_OEM_PAID,
+        OEM_NETWORK_PREFERENCE_OEM_PAID_NO_FALLBACK,
+        OEM_NETWORK_PREFERENCE_OEM_PAID_ONLY,
+        OEM_NETWORK_PREFERENCE_OEM_PRIVATE_ONLY,
+    })
+    public @interface Type {}
+
+    // Uncomment origins once OemNetworkPreferences class is exposed.
+    public static final int OEM_NETWORK_PREFERENCE_DEFAULT = 0;
+    //        android.net.OemNetworkPreferences.OEM_NETWORK_PREFERENCE_DEFAULT;
+    public static final int OEM_NETWORK_PREFERENCE_OEM_PAID = 1;
+    //        android.net.OemNetworkPreferences.OEM_NETWORK_PREFERENCE_OEM_PAID;
+    public static final int OEM_NETWORK_PREFERENCE_OEM_PAID_NO_FALLBACK = 2;
+    //        android.net.OemNetworkPreferences.OEM_NETWORK_PREFERENCE_OEM_PAID_NO_FALLBACK;
+    public static final int OEM_NETWORK_PREFERENCE_OEM_PAID_ONLY = 3;
+    //        android.net.OemNetworkPreferences.OEM_NETWORK_PREFERENCE_OEM_PAID_ONLY;
+    public static final int OEM_NETWORK_PREFERENCE_OEM_PRIVATE_ONLY = 4;
+    //        android.net.OemNetworkPreferences.OEM_NETWORK_PREFERENCE_OEM_PRIVATE_ONLY;
+
     public OemNetworkPreferencesWrapper(Context context) {
         mConnectivityManager = context.getSystemService(ConnectivityManager.class);
     }
 
-    public void applyPolicy(Set<String> oemPaidApps, Set<String> oemPrivateApps) {
+    public void applyPreference(SparseArray<Set<String>> preference) {
         /*
         OemNetworkPreferences.Builder prefsBuilder = new OemNetworkPreferences.Builder();
         prefsBuilder.addNetworkPreference(
