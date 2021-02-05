42f610ee7c5b39ac7087871a74b26ffac70dce83
Serik Beketayev <serikb@google.com>
2021-02-04 19:58:37 -0800

    [NetworkPreferencesApp] Step 2: Create config
    This corresponds to Step 2 from go/aae-pans-refapp
    "Create config overlay to reading initial values from"
    
    Bug: 178245725
    Test: mma & manual
    Change-Id: Ie35d12f079fe72f78790e84fd806db9353c7767a

--- tests/NetworkPreferenceApp/src/com/google/android/car/networking/preferenceupdater/components/PersonalStorage.java
+++ tests/NetworkPreferenceApp/src/com/google/android/car/networking/preferenceupdater/components/PersonalStorage.java
@@ -15,41 +15,96 @@
  */
 package com.google.android.car.networking.preferenceupdater.components;
 
+import static com.google.android.car.networking.preferenceupdater.components.OemNetworkPreferencesWrapper.OEM_NETWORK_PREFERENCE_DEFAULT;
+import static com.google.android.car.networking.preferenceupdater.components.OemNetworkPreferencesWrapper.OEM_NETWORK_PREFERENCE_OEM_PAID;
+import static com.google.android.car.networking.preferenceupdater.components.OemNetworkPreferencesWrapper.OEM_NETWORK_PREFERENCE_OEM_PAID_NO_FALLBACK;
+import static com.google.android.car.networking.preferenceupdater.components.OemNetworkPreferencesWrapper.OEM_NETWORK_PREFERENCE_OEM_PAID_ONLY;
+import static com.google.android.car.networking.preferenceupdater.components.OemNetworkPreferencesWrapper.OEM_NETWORK_PREFERENCE_OEM_PRIVATE_ONLY;
+
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.ArraySet;
+import android.util.SparseArray;
 
-import java.util.HashSet;
+import com.google.android.car.networking.preferenceupdater.R;
+
+import java.util.Arrays;
 import java.util.Set;
 
 /** This class will abstragate storage where OEM network policies are persisted */
 public final class PersonalStorage {
     private static final String KEY_PREFERENCE_APP = "key_preference_app";
-    private static final String KEY_OEM_PAID_APPS_LIST = "key_oem_paid_apps_list";
-    private static final String KEY_OEM_PRIVATE_APPS_LIST = "key_oem_private_apps_list";
+
+    private static final String KEY_OEM_DEFAULT_APPS = "key_oem_default_apps";
+    private static final String KEY_OEM_PAID_APPS = "key_oem_paid_apps";
+    private static final String KEY_OEM_PAID_NO_FALLBACK_APPS = "key_oem_paid_no_fallback_apps";
+    private static final String KEY_OEM_PAID_ONLY_APPS = "key_oem_paid_only_apps";
+    private static final String KEY_OEM_PRIVATE_ONLY_APPS = "key_oem_private_only_apps";
+
+    private final ArraySet<String> mDefaultOemDefaultApps;
+    private final ArraySet<String> mDefaultOemPaidApps;
+    private final ArraySet<String> mDefaultOemPaidNoFallbackApps;
+    private final ArraySet<String> mDefaultOemPaidOnlyApps;
+    private final ArraySet<String> mDefaultOemPrivateOnlyApps;
 
     private SharedPreferences mSharedPrefs;
+    private Context mContext;
 
     public PersonalStorage(Context ctx) {
+        mContext = ctx;
         mSharedPrefs = ctx.getSharedPreferences(KEY_PREFERENCE_APP, Context.MODE_PRIVATE);
+
+        // Loading default values, will be used in case there is nothing in SharedPreferences
+        mDefaultOemDefaultApps = getRes(R.array.config_network_preference_oem_default_apps);
+        mDefaultOemPaidApps = getRes(R.array.config_network_preference_oem_paid_apps);
+        mDefaultOemPaidNoFallbackApps = getRes(
+                R.array.config_network_preference_oem_paid_no_fallback_apps);
+        mDefaultOemPaidOnlyApps = getRes(R.array.config_network_preference_oem_paid_only);
+        mDefaultOemPrivateOnlyApps = getRes(R.array.config_network_preference_oem_private_only);
     }
 
-    public Set<String> getOEMPaidAppSet() {
-        return mSharedPrefs.getStringSet(KEY_OEM_PAID_APPS_LIST, new HashSet<String>());
+    public Set<String> get(@OemNetworkPreferencesWrapper.Type int type) {
+        switch (type) {
+            case OEM_NETWORK_PREFERENCE_DEFAULT:
+                return mSharedPrefs.getStringSet(KEY_OEM_DEFAULT_APPS, mDefaultOemDefaultApps);
+            case OEM_NETWORK_PREFERENCE_OEM_PAID:
+                return mSharedPrefs.getStringSet(KEY_OEM_PAID_APPS, mDefaultOemPaidApps);
+            case OEM_NETWORK_PREFERENCE_OEM_PAID_NO_FALLBACK:
+                return mSharedPrefs.getStringSet(
+                        KEY_OEM_PAID_NO_FALLBACK_APPS, mDefaultOemPaidNoFallbackApps);
+            case OEM_NETWORK_PREFERENCE_OEM_PAID_ONLY:
+                return mSharedPrefs.getStringSet(KEY_OEM_PAID_ONLY_APPS, mDefaultOemPaidOnlyApps);
+            case OEM_NETWORK_PREFERENCE_OEM_PRIVATE_ONLY:
+                return mSharedPrefs.getStringSet(
+                        KEY_OEM_PRIVATE_ONLY_APPS, mDefaultOemPrivateOnlyApps);
+            default:
+                return null;
+        }
     }
 
-    public Set<String> getOEMPrivateAppSet() {
-        return mSharedPrefs.getStringSet(KEY_OEM_PRIVATE_APPS_LIST, new HashSet<String>());
-    }
-
-    public void saveOEMPaidAppSet(Set<String> st) {
+    public void store(SparseArray<Set<String>> preference) {
         SharedPreferences.Editor editor = mSharedPrefs.edit();
-        editor.putStringSet(KEY_OEM_PAID_APPS_LIST, st);
+        Set<String> st = null;
+
+        st = preference.get(OEM_NETWORK_PREFERENCE_DEFAULT);
+        if (st != null) editor.putStringSet(KEY_OEM_DEFAULT_APPS, st);
+
+        st = preference.get(OEM_NETWORK_PREFERENCE_OEM_PAID);
+        if (st != null) editor.putStringSet(KEY_OEM_PAID_APPS, st);
+
+        st = preference.get(OEM_NETWORK_PREFERENCE_OEM_PAID_NO_FALLBACK);
+        if (st != null) editor.putStringSet(KEY_OEM_PAID_NO_FALLBACK_APPS, st);
+
+        st = preference.get(OEM_NETWORK_PREFERENCE_OEM_PAID_ONLY);
+        if (st != null) editor.putStringSet(KEY_OEM_PAID_ONLY_APPS, st);
+
+        st = preference.get(OEM_NETWORK_PREFERENCE_OEM_PRIVATE_ONLY);
+        if (st != null) editor.putStringSet(KEY_OEM_PRIVATE_ONLY_APPS, st);
+
         editor.apply();
     }
 
-    public void saveOEMPrivateAppSet(Set<String> st) {
-        SharedPreferences.Editor editor = mSharedPrefs.edit();
-        editor.putStringSet(KEY_OEM_PRIVATE_APPS_LIST, st);
-        editor.apply();
+    private ArraySet<String> getRes(int resId) {
+        return new ArraySet<String>(Arrays.asList(mContext.getResources().getStringArray(resId)));
     }
 }
