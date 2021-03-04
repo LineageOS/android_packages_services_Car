ec1944bd689ab83cf0eb7fc37cb288fddbb502b2
Lakshman Annadorai <lakshmana@google.com>
2021-03-03 16:18:34 -0800

    Implement CarWatchdogManager I/O overuse APIs.
    - Add basic checks for ResourceOveruseConfiguration on setting.
    - Support multiple listeners for each package.
    
    Test: `atest CarWatchdogManagerUnitTest`
    Bug: 177429052
    Bug: 170741935
    
    Change-Id: I5128f35c95d64ff10d531c7d1b01831b9cebb925

--- tests/carservice_unit_test/src/com/android/car/watchdog/CarWatchdogManagerUnitTest.java
+++ tests/carservice_unit_test/src/com/android/car/watchdog/CarWatchdogManagerUnitTest.java
@@ -15,23 +15,43 @@
  */
 package com.android.car.watchdog;
 
+import static android.car.watchdog.CarWatchdogManager.FLAG_MINIMUM_STATS_IO_1_MB;
+import static android.car.watchdog.CarWatchdogManager.FLAG_RESOURCE_OVERUSE_IO;
+import static android.car.watchdog.CarWatchdogManager.STATS_PERIOD_CURRENT_DAY;
 import static android.car.watchdog.CarWatchdogManager.TIMEOUT_CRITICAL;
+import static android.car.watchdog.ResourceOveruseConfiguration.COMPONENT_TYPE_SYSTEM;
 
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertThrows;
 
 import android.car.Car;
 import android.car.watchdog.CarWatchdogManager;
 import android.car.watchdog.ICarWatchdogServiceCallback;
+import android.car.watchdog.IResourceOveruseListener;
+import android.car.watchdog.IoOveruseConfiguration;
+import android.car.watchdog.IoOveruseStats;
+import android.car.watchdog.PackageKillableState;
+import android.car.watchdog.PerStateBytes;
+import android.car.watchdog.ResourceOveruseConfiguration;
+import android.car.watchdog.ResourceOveruseStats;
 import android.content.Context;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
+import android.os.Process;
+import android.os.UserHandle;
+import android.util.ArrayMap;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -40,16 +60,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Executor;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CarWatchdogManagerUnitTest {
-
+    private static final String TAG = CarWatchdogManager.class.getSimpleName();
     private static final int MAX_WAIT_TIME_MS = 3000;
 
     private final Handler mMainHandler = new Handler(Looper.getMainLooper());
+    @Spy
     private final Context mContext =
             InstrumentationRegistry.getInstrumentation().getTargetContext();
     private final Executor mExecutor = mContext.getMainExecutor();
@@ -111,6 +136,288 @@ public class CarWatchdogManagerUnitTest {
         testClientResponse(new ReturnAndPongClient());
     }
 
+    @Test
+    public void testGetResourceOveruseStats() throws Exception {
+        UserHandle userHandle = Process.myUserHandle();
+        ResourceOveruseStats expectedStats =
+                new ResourceOveruseStats.Builder("random.package", userHandle).build();
+        when(mService.getResourceOveruseStats(FLAG_RESOURCE_OVERUSE_IO, STATS_PERIOD_CURRENT_DAY))
+                .thenReturn(expectedStats);
+
+        ResourceOveruseStats actualStats = mCarWatchdogManager.getResourceOveruseStats(
+                FLAG_RESOURCE_OVERUSE_IO, STATS_PERIOD_CURRENT_DAY);
+
+        assertThat(actualStats).isEqualTo(expectedStats);
+    }
+
+    @Test
+    public void testGetAllResourceOveruseStats() throws Exception {
+        UserHandle userHandle = Process.myUserHandle();
+        List<ResourceOveruseStats> expectedStats = new ArrayList<>();
+        expectedStats.add(new ResourceOveruseStats.Builder("random.package.1", userHandle).build());
+        expectedStats.add(new ResourceOveruseStats.Builder("random.package.2", userHandle).build());
+        expectedStats.add(new ResourceOveruseStats.Builder("random.package.3", userHandle).build());
+        when(mService.getAllResourceOveruseStats(FLAG_RESOURCE_OVERUSE_IO,
+                FLAG_MINIMUM_STATS_IO_1_MB, STATS_PERIOD_CURRENT_DAY)).thenReturn(expectedStats);
+
+        List<ResourceOveruseStats> actualStats = mCarWatchdogManager.getAllResourceOveruseStats(
+                FLAG_RESOURCE_OVERUSE_IO, FLAG_MINIMUM_STATS_IO_1_MB, STATS_PERIOD_CURRENT_DAY);
+
+        assertThat(actualStats).isEqualTo(expectedStats);
+    }
+
+    @Test
+    public void testGetResourceOveruseStatsForUserPackage() throws Exception {
+        UserHandle userHandle = Process.myUserHandle();
+        String packageName = "random.package";
+        ResourceOveruseStats expectedStats =
+                new ResourceOveruseStats.Builder(packageName, userHandle).build();
+        when(mService.getResourceOveruseStatsForUserPackage(packageName, userHandle,
+                FLAG_RESOURCE_OVERUSE_IO, STATS_PERIOD_CURRENT_DAY)).thenReturn(expectedStats);
+
+        ResourceOveruseStats actualStats =
+                mCarWatchdogManager.getResourceOveruseStatsForUserPackage(packageName, userHandle,
+                        FLAG_RESOURCE_OVERUSE_IO, STATS_PERIOD_CURRENT_DAY);
+
+        assertThat(actualStats).isEqualTo(expectedStats);
+    }
+
+    @Test
+    public void testAddResourceOveruseListener() throws Exception {
+        CarWatchdogManager.ResourceOveruseListener listener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+
+        mCarWatchdogManager.addResourceOveruseListener(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, listener);
+
+        IResourceOveruseListener listenerImpl =
+                captureListenerImplOnAdd(FLAG_RESOURCE_OVERUSE_IO);
+
+        ResourceOveruseStats stats = setIoOveruseStats(sampleResourceOveruseStats()).build();
+        listenerImpl.onOveruse(stats);
+
+        verify(listener, timeout(MAX_WAIT_TIME_MS).times(1)).onOveruse(eq(stats));
+
+        // Shouldn't call the underlying listener as the I/O overuse stats is null.
+        listenerImpl.onOveruse(sampleResourceOveruseStats().build());
+
+        mCarWatchdogManager.removeResourceOveruseListener(listener);
+        verify(mService).removeResourceOveruseListener(listenerImpl);
+
+        // Shouldn't call the listener as it is removed.
+        listenerImpl.onOveruse(stats);
+
+        verifyNoMoreInteractions(listener);
+    }
+
+    @Test
+    public void testMultipleAddResourceOveruseListener() throws Exception {
+        CarWatchdogManager.ResourceOveruseListener firstListener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+        CarWatchdogManager.ResourceOveruseListener secondListener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+
+        mCarWatchdogManager.addResourceOveruseListener(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, firstListener);
+
+        IResourceOveruseListener listenerImpl =
+                captureListenerImplOnAdd(FLAG_RESOURCE_OVERUSE_IO);
+
+        mCarWatchdogManager.addResourceOveruseListener(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, secondListener);
+
+        /*
+         * Shouldn't add listener implementation to service again as the resource overuse flag
+         * didn't change.
+         */
+        verifyNoMoreInteractions(mService);
+
+        ResourceOveruseStats stats = setIoOveruseStats(sampleResourceOveruseStats()).build();
+        listenerImpl.onOveruse(stats);
+
+        verify(firstListener, timeout(MAX_WAIT_TIME_MS).times(1)).onOveruse(eq(stats));
+
+        mCarWatchdogManager.removeResourceOveruseListener(firstListener);
+
+        listenerImpl.onOveruse(stats);
+
+        verifyNoMoreInteractions(firstListener);
+        verify(secondListener, timeout(MAX_WAIT_TIME_MS).times(2)).onOveruse(eq(stats));
+
+        mCarWatchdogManager.removeResourceOveruseListener(secondListener);
+
+        // Should be called only once when removing last listener.
+        verify(mService, times(1)).removeResourceOveruseListener(listenerImpl);
+    }
+
+    @Test
+    public void testReAddResourceOveruseListener() throws Exception {
+        CarWatchdogManager.ResourceOveruseListener firstListener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+        CarWatchdogManager.ResourceOveruseListener secondListener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+
+        mCarWatchdogManager.addResourceOveruseListener(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, firstListener);
+        mCarWatchdogManager.addResourceOveruseListener(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, secondListener);
+        mCarWatchdogManager.removeResourceOveruseListener(firstListener);
+        mCarWatchdogManager.removeResourceOveruseListener(secondListener);
+
+        mCarWatchdogManager.addResourceOveruseListener(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, firstListener);
+        mCarWatchdogManager.removeResourceOveruseListener(firstListener);
+
+        verify(mService, times(2)).addResourceOveruseListener(eq(FLAG_RESOURCE_OVERUSE_IO),
+                any(IResourceOveruseListener.class));
+        verify(mService, times(2))
+                .removeResourceOveruseListener(any(IResourceOveruseListener.class));
+    }
+
+    @Test
+    public void testRemoveRemovedResourceOveruseListener() throws Exception {
+        CarWatchdogManager.ResourceOveruseListener listener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+        mCarWatchdogManager.addResourceOveruseListener(mExecutor, FLAG_RESOURCE_OVERUSE_IO,
+                listener);
+        mCarWatchdogManager.removeResourceOveruseListener(listener);
+        // The following call should not throw an exception.
+        mCarWatchdogManager.removeResourceOveruseListener(listener);
+    }
+
+    @Test
+    public void testAddResourceOveruseListenerForSystem() throws Exception {
+        CarWatchdogManager.ResourceOveruseListener listener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+
+        mCarWatchdogManager.addResourceOveruseListenerForSystem(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, listener);
+
+        IResourceOveruseListener listenerImpl =
+                captureListenerForSystemImplOnAdd(FLAG_RESOURCE_OVERUSE_IO);
+
+        ResourceOveruseStats stats = setIoOveruseStats(sampleResourceOveruseStats()).build();
+        listenerImpl.onOveruse(stats);
+
+        verify(listener, timeout(MAX_WAIT_TIME_MS).times(1)).onOveruse(eq(stats));
+
+        // Shouldn't call the underlying listener as the I/O overuse stats is null.
+        listenerImpl.onOveruse(sampleResourceOveruseStats().build());
+
+        mCarWatchdogManager.removeResourceOveruseListenerForSystem(listener);
+        verify(mService).removeResourceOveruseListenerForSystem(listenerImpl);
+
+        // Shouldn't call the listener as it is removed.
+        listenerImpl.onOveruse(stats);
+
+        verifyNoMoreInteractions(listener);
+    }
+
+    @Test
+    public void testMultipleAddResourceOveruseListenerForSystem() throws Exception {
+        CarWatchdogManager.ResourceOveruseListener firstListener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+        CarWatchdogManager.ResourceOveruseListener secondListener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+
+        mCarWatchdogManager.addResourceOveruseListenerForSystem(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, firstListener);
+
+        IResourceOveruseListener listenerImpl =
+                captureListenerForSystemImplOnAdd(FLAG_RESOURCE_OVERUSE_IO);
+
+        mCarWatchdogManager.addResourceOveruseListenerForSystem(
+                mExecutor, FLAG_RESOURCE_OVERUSE_IO, secondListener);
+
+        /*
+         * Shouldn't add listener implementation to service again as the resource overuse flag
+         * didn't change.
+         */
+        verifyNoMoreInteractions(mService);
+
+        ResourceOveruseStats stats = setIoOveruseStats(sampleResourceOveruseStats()).build();
+        listenerImpl.onOveruse(stats);
+
+        verify(firstListener, timeout(MAX_WAIT_TIME_MS).times(1)).onOveruse(eq(stats));
+
+        mCarWatchdogManager.removeResourceOveruseListenerForSystem(firstListener);
+
+        listenerImpl.onOveruse(stats);
+
+        verifyNoMoreInteractions(firstListener);
+        verify(secondListener, timeout(MAX_WAIT_TIME_MS).times(2)).onOveruse(eq(stats));
+
+        mCarWatchdogManager.removeResourceOveruseListenerForSystem(secondListener);
+
+        // Should be called only once when removing last listener.
+        verify(mService, times(1)).removeResourceOveruseListenerForSystem(listenerImpl);
+    }
+
+    @Test
+    public void testRemoveRemovedResourceOveruseListenerForSystem() throws Exception {
+        CarWatchdogManager.ResourceOveruseListener listener =
+                mock(CarWatchdogManager.ResourceOveruseListener.class);
+
+        mCarWatchdogManager.addResourceOveruseListenerForSystem(mExecutor, FLAG_RESOURCE_OVERUSE_IO,
+                listener);
+
+        mCarWatchdogManager.removeResourceOveruseListenerForSystem(listener);
+
+        // The following call should not throw an exception.
+        mCarWatchdogManager.removeResourceOveruseListenerForSystem(listener);
+    }
+
+    @Test
+    public void testSetKillablePackageAsUser() throws Exception {
+        UserHandle userHandle = Process.myUserHandle();
+
+        mCarWatchdogManager.setKillablePackageAsUser("random.package", userHandle,
+                /*isKillable=*/true);
+
+        verify(mService, times(1)).setKillablePackageAsUser("random.package", userHandle,
+                /*isKillable=*/true);
+    }
+
+    @Test
+    public void testGetPackageKillableStatesAsUser() throws Exception {
+        UserHandle userHandle = Process.myUserHandle();
+        List<PackageKillableState> expectedPackages = new ArrayList<>(Arrays.asList(
+                new PackageKillableState("random.package.A", /*userId=*/10,
+                        PackageKillableState.KILLABLE_STATE_YES),
+                new PackageKillableState("random.package.B", /*userId=*/11,
+                        PackageKillableState.KILLABLE_STATE_NEVER)));
+        when(mService.getPackageKillableStatesAsUser(userHandle)).thenReturn(expectedPackages);
+
+        List<PackageKillableState> actualPackages =
+                mCarWatchdogManager.getPackageKillableStatesAsUser(userHandle);
+
+        assertThat(actualPackages).isEqualTo(expectedPackages);
+    }
+
+    @Test
+    public void testSetResourceOveruseConfigurations() throws Exception {
+        ResourceOveruseConfiguration config = sampleResourceOveruseConfigBuilder().build();
+        List<ResourceOveruseConfiguration> configs = new ArrayList<>(Arrays.asList(config));
+
+        mCarWatchdogManager.setResourceOveruseConfigurations(configs, FLAG_RESOURCE_OVERUSE_IO);
+
+        verify(mService, times(1)).setResourceOveruseConfigurations(configs,
+                FLAG_RESOURCE_OVERUSE_IO);
+    }
+
+    @Test
+    public void testGetResourceOveruseConfigurations() throws Exception {
+        ResourceOveruseConfiguration config = sampleResourceOveruseConfigBuilder().build();
+        List<ResourceOveruseConfiguration> expectedConfigs = new ArrayList<>(Arrays.asList(config));
+        when(mService.getResourceOveruseConfigurations(FLAG_RESOURCE_OVERUSE_IO))
+                .thenReturn(expectedConfigs);
+
+        List<ResourceOveruseConfiguration> actualConfigs =
+                mCarWatchdogManager.getResourceOveruseConfigurations(FLAG_RESOURCE_OVERUSE_IO);
+
+        assertThat(actualConfigs).isEqualTo(expectedConfigs);
+    }
+
     private ICarWatchdogServiceCallback registerClient(
             CarWatchdogManager.CarWatchdogClientCallback client) {
         mCarWatchdogManager.registerClient(mExecutor, client, TIMEOUT_CRITICAL);
@@ -121,11 +428,58 @@ public class CarWatchdogManagerUnitTest {
         return clientImplCaptor.getValue();
     }
 
+    private ResourceOveruseConfiguration.Builder sampleResourceOveruseConfigBuilder() {
+        IoOveruseConfiguration ioOveruseConfig = new IoOveruseConfiguration.Builder(
+                /* componentLevelThresholds= */new PerStateBytes(200, 300, 400),
+                /* packageSpecificThresholds= */new ArrayMap<>(),
+                /* appCategorySpecificThresholds= */new ArrayMap<>(),
+                /* systemWideThresholds= */new ArrayList<>()).build();
+        ResourceOveruseConfiguration.Builder configBuilder =
+                new ResourceOveruseConfiguration.Builder(
+                        COMPONENT_TYPE_SYSTEM, /* safeTiKillPackages= */new ArrayList<>(),
+                        /* vendorPackagePrefixes= */new ArrayList<>(),
+                        /* packagesToAppCategoryTypes= */ new ArrayMap<>());
+        configBuilder.setIoOveruseConfiguration(ioOveruseConfig);
+        return configBuilder;
+    }
+
     private void testClientResponse(CarWatchdogManager.CarWatchdogClientCallback client)
             throws Exception {
         ICarWatchdogServiceCallback clientImpl = registerClient(client);
         clientImpl.onCheckHealthStatus(123456, TIMEOUT_CRITICAL);
         verify(mService, timeout(MAX_WAIT_TIME_MS)).tellClientAlive(clientImpl, 123456);
+    }
+
+    private ResourceOveruseStats.Builder sampleResourceOveruseStats() {
+        return new ResourceOveruseStats.Builder("", Process.myUserHandle());
+    }
+
+    private ResourceOveruseStats.Builder setIoOveruseStats(
+            ResourceOveruseStats.Builder resourceOveruseStatsBuilder) {
+        IoOveruseStats.Builder ioOveruseStatsBuilder = new IoOveruseStats.Builder(0, 120)
+                .setTotalOveruses(10).setTotalTimesKilled(2)
+                .setTotalBytesWritten(10 * 1024 * 1024).setKillableOnOveruse(true)
+                .setRemainingWriteBytes(new PerStateBytes(1024, 1024 * 1024, 0));
+        resourceOveruseStatsBuilder.setIoOveruseStats(ioOveruseStatsBuilder.build());
+        return resourceOveruseStatsBuilder;
+    }
+
+    private IResourceOveruseListener captureListenerImplOnAdd(int resourceOveruseFlag) {
+        ArgumentCaptor<IResourceOveruseListener> listenerArgumentCaptor =
+                ArgumentCaptor.forClass(IResourceOveruseListener.class);
+
+        verify(mService, times(1)).addResourceOveruseListener(eq(resourceOveruseFlag),
+                listenerArgumentCaptor.capture());
+        return listenerArgumentCaptor.getValue();
+    }
+
+    private IResourceOveruseListener captureListenerForSystemImplOnAdd(int resourceOveruseFlag) {
+        ArgumentCaptor<IResourceOveruseListener> listenerArgumentCaptor =
+                ArgumentCaptor.forClass(IResourceOveruseListener.class);
+
+        verify(mService).addResourceOveruseListenerForSystem(eq(resourceOveruseFlag),
+                listenerArgumentCaptor.capture());
+        return listenerArgumentCaptor.getValue();
     }
 
     private final class TestClient extends CarWatchdogManager.CarWatchdogClientCallback {
