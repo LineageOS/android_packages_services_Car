ef6efc3ee7d49e5b5af7f86856cb77c0983ecaf9
Keun young Park <keunyoung@google.com>
2021-03-05 17:29:30 -0800

    Add LargeParcelable
    - This is a hidden API which allows passing bigger Parcelable through
      shared memory.
    - LargeParcelable can pass any Parcelable under it.
    - LargeParcelableBase is for child class to implement a Parcelable directly.
    - Also provided two methods to make it work with stable AIDL code.
      serializeStableAIDLParcelable: for sending
      reconstructStableAIDLParcelable: for receiving
      Sender needs to replace sending code. Test code shows how binder call should
      be overwridden to make more efficient calls.
    
    Bug: 148703287
    Test: run added test
           atest com.android.car.internal.test.LargeParcelableTest
                 com.android.car.internal.test.LargeParcelableJavaStableAIDLTest
    Change-Id: I84a1fb19c1f75b632faec62648fa0d775e27783c

---
+++ car-lib/src/com/android/car/internal/LargeParcelable.java
@@ -0,0 +1,291 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.internal;
+
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.os.Parcel;
+import android.os.ParcelFileDescriptor;
+import android.os.Parcelable;
+import android.os.SharedMemory;
+import android.util.Log;
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * Utility to pass any {@code Parcelable} through binder with automatic conversion into shared
+ * memory when data size is too big.
+ *
+ * <p> This class can work by itself but child class can be useful to use a custom class to
+ * interface with C++ world. For such usage, child class will only add its own {@code CREATOR} impl
+ * and a constructor taking {@code Parcel in}.
+ * <p>For stable AIDL, this class also provides two methods for serialization {@link
+ * #serializeStableAIDLParcelable(Parcel, Parcelable, int, boolean)} and deserialization
+ * {@link #reconstructStableAIDLParcelable(Parcelable, boolean)}. Plz check included test for the
+ * usage.
+ *
+ * @hide
+ */
+public class LargeParcelable extends LargeParcelableBase {
+    /**
+     * Stable AIDL Parcelable should have this member with {@code ParcelFileDescriptor} to support
+     * bigger payload passing over shared memory.
+     */
+    public static final String STABLE_AIDL_SHARED_MEMORY_MEMBER = "sharedMemoryFd";
+    /**
+     * Stable AIDL Parcelable has {@code readFromParcel(Parcel)} public method.
+     */
+    public static final String STABLE_AIDL_PARCELABLE_READ_FROM_PARCEL = "readFromParcel";
+
+    private static final String TAG = LargeParcelable.class.getSimpleName();
+    private static final boolean DBG_PAYLOAD = false;
+    private static final boolean DBG_STABLE_AIDL_CLASS = false;
+
+    // cannot set this final even if it is set only once in constructor as set is done in
+    // deserialize call.
+    private @Nullable Parcelable mParcelable;
+
+    // This is shared across thread. As this is per class, use volatile to avoid adding
+    // separate lock. If additional static volatile is added, a lock should be added.
+    private static volatile WeakReference<ClassLoader> sClassLoader = null;
+
+    /**
+     * Sets {@code ClassLoader} for loading the {@Code Parcelable}. This should be done before
+     * getting binder call. Default classloader may not recognize the Parcelable passed and relevant
+     * classloader like package classloader should be set before getting any binder data.
+     */
+    public static void setClassLoader(ClassLoader loader) {
+        sClassLoader = new WeakReference<>(loader);
+    }
+
+    public LargeParcelable(Parcel in) {
+        super(in);
+    }
+
+    public LargeParcelable(Parcelable parcelable) {
+        mParcelable = parcelable;
+    }
+
+    /**
+     * Returns {@code Parcelable} carried by this instance.
+     */
+    public Parcelable getParcelable() {
+        return mParcelable;
+    }
+
+    @Override
+    protected void serialize(@NonNull Parcel dest, int flags) {
+        int startPosition;
+        if (DBG_PAYLOAD) {
+            startPosition = dest.dataPosition();
+        }
+        dest.writeParcelable(mParcelable, flags);
+        if (DBG_PAYLOAD) {
+            Log.d(TAG, "serialize-payload, start:" + startPosition
+                    + " size:" + (dest.dataPosition() - startPosition));
+        }
+    }
+
+    @Override
+    protected void serializeNullPayload(@NonNull Parcel dest) {
+        int startPosition;
+        if (DBG_PAYLOAD) {
+            startPosition = dest.dataPosition();
+        }
+        dest.writeParcelable(null, 0);
+        if (DBG_PAYLOAD) {
+            Log.d(TAG, "serializeNullPayload-payload, start:" + startPosition
+                    + " size:" + (dest.dataPosition() - startPosition));
+        }
+    }
+
+    @Override
+    protected void deserialize(@NonNull Parcel src) {
+        // default class loader does not work as it may not be in boot class path.
+        ClassLoader loader = (sClassLoader == null) ? null : sClassLoader.get();
+        int startPosition;
+        if (DBG_PAYLOAD) {
+            startPosition = src.dataPosition();
+        }
+        mParcelable = src.readParcelable(loader);
+        if (DBG_PAYLOAD) {
+            Log.d(TAG, "deserialize-payload, start:" + startPosition
+                    + " size:" + (src.dataPosition() - startPosition)
+                    + " mParcelable:" + mParcelable);
+        }
+    }
+
+    public static final @NonNull Parcelable.Creator<LargeParcelable> CREATOR =
+            new Parcelable.Creator<LargeParcelable>() {
+                @Override
+                public LargeParcelable[] newArray(int size) {
+                    return new LargeParcelable[size];
+                }
+
+                @Override
+                public LargeParcelable createFromParcel(@NonNull Parcel in) {
+                    return new LargeParcelable(in);
+                }
+            };
+
+    /**
+     * Serializes a {@code Parcelable} defined from Stable AIDL into {@code Parcel}.
+     *
+     * <p>>If payload size is big, it will be passed over the shared memory. The {@code Parcelable}
+     * should have a public member having name of {@link #STABLE_AIDL_SHARED_MEMORY_MEMBER} with
+     * {@code ParcelFileDescriptor} type. It also should have default constructor.
+     *
+     * <p>If {@code Parcelable} already contains shared memory fd, the shared memory would be
+     * serialized to {@code Parcel} and other payload would be ignored.
+     *
+     * @param p                {@code Parcelable} to serialize.
+     * @param flags            flags same with flags in {@link Parcel#writeParcelable(Parcelable,
+     *                         int)}.
+     * @param keepSharedMemory Whether to keep created shared memory in the original {@code
+     *                         Parcelable}. Set to {@code true} if this {@code Parcelable} is sent
+     *                         across binder repeatedly.
+     * @param dest Target {@code Parcel} to serialize.
+     */
+    public static void serializeStableAIDLParcelable(@NonNull Parcel dest, @Nullable Parcelable p,
+            int flags, boolean keepSharedMemory) {
+        if (p == null) {
+            // do  not write anything for null. Null marking should be handled before this.
+            return;
+        }
+        Class parcelableClass = p.getClass();
+        if (DBG_STABLE_AIDL_CLASS) {
+            Log.d(TAG, "serializeStableAIDLParcelable stable AIDL Parcelable:"
+                    + parcelableClass.getSimpleName());
+        }
+        Field field;
+        ParcelFileDescriptor sharedMemoryFd;
+        try {
+            field = parcelableClass.getField(STABLE_AIDL_SHARED_MEMORY_MEMBER);
+            sharedMemoryFd = (ParcelFileDescriptor) field.get(p);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Cannot access " + STABLE_AIDL_SHARED_MEMORY_MEMBER,
+                    e);
+        }
+        int startPosition = dest.dataPosition();
+        if (sharedMemoryFd == null) {
+            p.writeToParcel(dest, flags);
+            int payloadSize = dest.dataPosition() - startPosition;
+            if (payloadSize <= LargeParcelableBase.MAX_DIRECT_PAYLOAD_SIZE) {
+                // direct path, no re-write to shared memory.
+                if (DBG_PAYLOAD) {
+                    Log.d(TAG, "serializeStableAIDLParcelable send directly, payload size:"
+                            + payloadSize);
+                }
+                return;
+            }
+            SharedMemory memory = LargeParcelableBase.serializeParcelToSharedMemory(dest,
+                    startPosition, dest.dataPosition() - startPosition);
+            sharedMemoryFd = new ParcelFileDescriptor(memory.getFileDescriptor());
+            if (keepSharedMemory) {
+                try {
+                    field.set(p, sharedMemoryFd);
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("Cannot access "
+                            + STABLE_AIDL_SHARED_MEMORY_MEMBER, e);
+                }
+            }
+        }
+        // now write empty payload with only fd set, create with default constructor for that.
+        Parcelable emptyPayload;
+        ParcelFileDescriptor sentFd = null;
+        try {
+            emptyPayload = (Parcelable) parcelableClass.newInstance();
+            field.set(emptyPayload, sharedMemoryFd);
+            if (DBG_PAYLOAD) {
+                sentFd = (ParcelFileDescriptor) field.get(emptyPayload);
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Cannot access Parcelable constructor/member", e);
+        }
+        dest.setDataPosition(startPosition);
+        emptyPayload.writeToParcel(dest, flags);
+        if (DBG_PAYLOAD) {
+            Log.d(TAG, "serializeStableAIDLParcelable added shared memory, start position:"
+                    + startPosition + " last position:" + dest.dataPosition()
+                    + " sharedMemoryFd:" + sentFd);
+        }
+    }
+
+    /**
+     * Reconstructs {@code Parcelable} defined from Stable AIDL. It should have a {@link
+     * ParcelFileDescriptor} member named {@link #STABLE_AIDL_SHARED_MEMORY_MEMBER} and will create
+     * a new {@code Parcelable} if the shared memory portion is not null. If there is no shared
+     * memory, it will return the original {@code Parcelable p} as it is.
+     *
+     * @param p                Original {@code Parcelable} containing the payload.
+     * @param keepSharedMemory Whether to keep created shared memory in the returned {@code
+     *                         Parcelable}. Set to {@code true} if this {@code Parcelable} is sent
+     *                         across binder repeatedly.
+     * @return a new {@code Parcelable} if payload read from shared memory or old one if payload
+     * is small enough.
+     */
+    public static @Nullable Parcelable reconstructStableAIDLParcelable(@Nullable Parcelable p,
+            boolean keepSharedMemory) {
+        if (p == null) {
+            return null;
+        }
+        Class parcelableClass = p.getClass();
+        if (DBG_STABLE_AIDL_CLASS) {
+            Log.d(TAG, "reconstructStableAIDLParcelable stable AIDL Parcelable:"
+                    + parcelableClass.getSimpleName());
+        }
+        ParcelFileDescriptor sharedMemoryFd = null;
+        Field fieldSharedMemory;
+        try {
+            fieldSharedMemory = parcelableClass.getField(STABLE_AIDL_SHARED_MEMORY_MEMBER);
+            sharedMemoryFd = (ParcelFileDescriptor) fieldSharedMemory.get(p);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Cannot access " + STABLE_AIDL_SHARED_MEMORY_MEMBER,
+                    e);
+        }
+        if (sharedMemoryFd == null) {
+            if (DBG_PAYLOAD) {
+                Log.d(TAG, "reconstructStableAIDLParcelable null shared memory");
+            }
+            return p;
+        }
+        SharedMemory memory = SharedMemory.create(sharedMemoryFd);
+        Parcel in = LargeParcelableBase.copyFromSharedMemory(memory);
+        Parcelable retParcelable;
+        try {
+            retParcelable = (Parcelable) parcelableClass.newInstance();
+            // runs retParcelable.readFromParcel(in)
+            Method readMethod = parcelableClass.getMethod(STABLE_AIDL_PARCELABLE_READ_FROM_PARCEL,
+                    new Class[]{Parcel.class});
+            readMethod.invoke(retParcelable, in);
+            if (keepSharedMemory) {
+                fieldSharedMemory.set(retParcelable, sharedMemoryFd);
+            }
+            if (DBG_PAYLOAD) {
+                Log.d(TAG, "reconstructStableAIDLParcelable read shared memory, data size:"
+                        + in.dataPosition());
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Cannot access Parcelable constructor/method", e);
+        } finally {
+            in.recycle();
+        }
+        return retParcelable;
+    }
+}
---
+++ tests/android_car_api_test/src/com/android/car/internal/test/LargeParcelableJavaStableAIDLTest.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.internal.test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.car.apitest.IStableAIDLTestBinder;
+import android.car.apitest.StableAIDLTestLargeParcelable;
+import android.car.test.mocks.JavaMockitoHelper;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.android.car.internal.LargeParcelable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+@SmallTest
+public final class LargeParcelableJavaStableAIDLTest {
+    private static final long DEFAULT_TIMEOUT_MS = 3000;
+    private static final int ARRAY_LENGTH_SMALL = 2048;
+    // The current threshold is 4096.
+    private static final int ARRAY_LENGTH_BIG = 4099;
+
+    private final Context mContext = InstrumentationRegistry.getInstrumentation()
+            .getTargetContext();
+
+    private final TestServiceConnection mServiceConnection = new TestServiceConnection();
+
+    private IStableAIDLTestBinderWrapper mBinder;
+
+    @Before
+    public void setUp() throws Exception {
+        LargeParcelable.setClassLoader(mContext.getClassLoader());
+        Intent intent = new Intent();
+        intent.setClassName(mContext, IStableAIDLBinderTestService.class.getName());
+        mContext.bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+        JavaMockitoHelper.await(mServiceConnection.latch, DEFAULT_TIMEOUT_MS);
+    }
+
+    @After
+    public void tearDown() {
+        mContext.unbindService(mServiceConnection);
+    }
+
+    @Test
+    public void testEchoSmallPayload() throws Exception {
+        doTestLEcho(ARRAY_LENGTH_SMALL);
+    }
+
+    @Test
+    public void testEchoBigPayload() throws Exception {
+        doTestLEcho(ARRAY_LENGTH_BIG);
+    }
+
+    @Test
+    public void testEchoMultipleArgsSmallPayload() throws Exception {
+        doTestMultipleArgs(ARRAY_LENGTH_SMALL);
+    }
+
+    @Test
+    public void testEchoMultipleArgsBigPayload() throws Exception {
+        doTestMultipleArgs(ARRAY_LENGTH_BIG);
+    }
+
+    @Test
+    public void testNullParcelable() throws Exception {
+        StableAIDLTestLargeParcelable r = mBinder.echo(null);
+
+        assertThat(r).isNull();
+
+        long argValue = 0x12345678;
+
+        long rValue = mBinder.echoWithLong(null, argValue);
+
+        assertThat(argValue).isEqualTo(rValue);
+    }
+
+    private void doTestLEcho(int payloadSize) throws Exception {
+        StableAIDLTestLargeParcelable orig = new StableAIDLTestLargeParcelable();
+        orig.payload = LargeParcelableTest.createByteArray(payloadSize);
+
+        StableAIDLTestLargeParcelable r = mBinder.echo(orig);
+
+        assertThat(r).isNotNull();
+        assertThat(r.payload).isNotNull();
+        assertThat(r.payload).isEqualTo(orig.payload);
+        if (payloadSize > LargeParcelable.MAX_DIRECT_PAYLOAD_SIZE) {
+            assertThat(orig.sharedMemoryFd).isNotNull();
+            assertThat(r.sharedMemoryFd).isNotNull();
+        } else {
+            assertThat(orig.sharedMemoryFd).isNull();
+            assertThat(r.sharedMemoryFd).isNull();
+        }
+    }
+
+    private void doTestMultipleArgs(int payloadSize) throws Exception {
+        StableAIDLTestLargeParcelable orig = new StableAIDLTestLargeParcelable();
+        orig.payload = LargeParcelableTest.createByteArray(payloadSize);
+        long argValue = 0x12345678;
+        long expectedRet = argValue + IStableAIDLBinderTestService.calcByteSum(orig);
+
+        long r = mBinder.echoWithLong(orig, argValue);
+
+        assertThat(r).isEqualTo(expectedRet);
+        if (payloadSize > LargeParcelable.MAX_DIRECT_PAYLOAD_SIZE) {
+            assertThat(orig.sharedMemoryFd).isNotNull();
+        } else {
+            assertThat(orig.sharedMemoryFd).isNull();
+        }
+    }
+
+    // This class shows how binder call is wrapped to make it more efficient with shared memory.
+    // Most code is copied from auto-generated code with only small changes.
+    private static final class IStableAIDLTestBinderWrapper {
+        static final int TRANSACTION_echo = (IBinder.FIRST_CALL_TRANSACTION + 0);
+        static final int TRANSACTION_echoWithLong = (IBinder.FIRST_CALL_TRANSACTION + 1);
+
+        private final IStableAIDLTestBinder mBinder;
+
+        IStableAIDLTestBinderWrapper(IStableAIDLTestBinder binder) {
+            mBinder = binder;
+        }
+
+        StableAIDLTestLargeParcelable echo(StableAIDLTestLargeParcelable p) throws
+                RemoteException {
+            Parcel data = Parcel.obtain();
+            Parcel reply = Parcel.obtain();
+            StableAIDLTestLargeParcelable result;
+            try {
+                data.writeInterfaceToken(IStableAIDLTestBinder.DESCRIPTOR);
+                if (p != null) {
+                    data.writeInt(1);
+                    /// changed from auto-generated code
+                    LargeParcelable.serializeStableAIDLParcelable(data, p, 0, true);
+                } else {
+                    data.writeInt(0);
+                }
+                boolean status = mBinder.asBinder().transact(TRANSACTION_echo, data, reply, 0);
+                if (!status) {
+                    throw new IllegalArgumentException();
+                }
+                reply.readException();
+                if (0 != reply.readInt()) {
+                    // changed from auto-generated code
+                    result = StableAIDLTestLargeParcelable.CREATOR.createFromParcel(reply);
+                    result =
+                            (StableAIDLTestLargeParcelable)
+                                    LargeParcelable.reconstructStableAIDLParcelable(
+                                    result, true);
+                } else {
+                    result = null;
+                }
+            } finally {
+                reply.recycle();
+                data.recycle();
+            }
+            return result;
+        }
+
+        public long echoWithLong(StableAIDLTestLargeParcelable p, long v) throws RemoteException {
+            Parcel data = Parcel.obtain();
+            Parcel reply = Parcel.obtain();
+            long result;
+            try {
+                data.writeInterfaceToken(IStableAIDLTestBinder.DESCRIPTOR);
+                if (p != null) {
+                    data.writeInt(1);
+                    // changed from auto-generated code
+                    LargeParcelable.serializeStableAIDLParcelable(data, p, 0, true);
+                } else {
+                    data.writeInt(0);
+                }
+                data.writeLong(v);
+                boolean status = mBinder.asBinder().transact(TRANSACTION_echoWithLong, data, reply,
+                        0);
+                if (!status) {
+                    throw new IllegalArgumentException();
+                }
+                reply.readException();
+                result = reply.readLong();
+            } finally {
+                reply.recycle();
+                data.recycle();
+            }
+            return result;
+        }
+    }
+
+    private final class TestServiceConnection implements ServiceConnection {
+        public final CountDownLatch latch = new CountDownLatch(1);
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBinder = new IStableAIDLTestBinderWrapper(IStableAIDLTestBinder.Stub.asInterface(
+                    service));
+            latch.countDown();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    }
+}
