b6952b772e834aeccd5c11eb9d566255ae1fda70
Chiachang Wang <chiachangwang@google.com>
2021-04-09 09:29:47 +0800

    Remove the hidden OemNetworkPreferences references
    Due to connectivity modulization work, the hidden connectivity
    methods or members are not accessible outside the module,
    including tests. Thus, the dependency should be removed or
    replaced. Update test code to resolve such dependency.
    
    Bug: 182859030
    Test: atest
    Change-Id: I99b4fe7b1b7e8221f0de7e998c25bf1bebed0407

--- tests/NetworkPreferenceApp/src/com/google/android/car/networking/preferenceupdater/components/OemAppsManager.java
+++ tests/NetworkPreferenceApp/src/com/google/android/car/networking/preferenceupdater/components/OemAppsManager.java
@@ -22,7 +22,6 @@ import static android.net.OemNetworkPreferences.OEM_NETWORK_PREFERENCE_OEM_PRIVA
 
 import android.annotation.NonNull;
 import android.content.Context;
-import android.net.OemNetworkPreferences.OemNetworkPreference;
 import android.util.ArraySet;
 
 import com.google.android.car.networking.preferenceupdater.R;
@@ -32,8 +31,7 @@ import java.util.Set;
 
 /** Maintains list of applications per OEM network preference. Singleton class. */
 public final class OemAppsManager {
-    public static Set<String> getDefaultAppsFor(
-            @NonNull Context ctx, @OemNetworkPreference int type) {
+    public static Set<String> getDefaultAppsFor(@NonNull Context ctx, int type) {
         switch (type) {
             case OEM_NETWORK_PREFERENCE_OEM_PAID:
                 return getRes(ctx, R.array.config_network_preference_oem_paid_apps);
