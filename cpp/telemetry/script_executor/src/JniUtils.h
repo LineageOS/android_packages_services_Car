e864dfcc54b6c5123d7f0afde7358edc394090c9
Max Dashouk <mdashouk@google.com>
2021-05-20 14:19:32 -0700

    Add utility function that takes Java Bundle, converts it and pushes it to Lua table.
    Adds relevant unit tests.
    
    Bug: 187517413
    Test: atest JniUtilsTest
    Change-Id: I44a3f83efc5733c83506aad8897161e6f862f996

--- cpp/telemetry/script_executor/src/JniUtils.h
+++ cpp/telemetry/script_executor/src/JniUtils.h
@@ -13,40 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifndef CPP_TELEMETRY_SCRIPT_EXECUTOR_SRC_JNIUTILS_H_
+#define CPP_TELEMETRY_SCRIPT_EXECUTOR_SRC_JNIUTILS_H_
 
-#ifndef CPP_TELEMETRY_SCRIPT_EXECUTOR_SRC_LUAENGINE_H_
-#define CPP_TELEMETRY_SCRIPT_EXECUTOR_SRC_LUAENGINE_H_
-
-#include "ScriptExecutorListener.h"
-
-#include <memory>
-
-extern "C" {
-#include "lua.h"
-}
+#include "LuaEngine.h"
+#include "jni.h"
 
 namespace android {
 namespace automotive {
 namespace telemetry {
 namespace script_executor {
 
-// Encapsulates Lua script execution environment.
-class LuaEngine {
-public:
-    LuaEngine();
-
-    virtual ~LuaEngine();
-
-    // Returns pointer to Lua state object.
-    lua_State* GetLuaState();
-
-private:
-    lua_State* mLuaState;  // owned
-};
+// Helper function which takes android.os.Bundle object in "bundle" argument
+// and converts it to Lua table on top of Lua stack. All key-value pairs are
+// converted to the corresponding key-value pairs of the Lua table as long as
+// the Bundle value types are supported. At this point, we support boolean,
+// integer, double and String types in Java.
+void PushBundleToLuaTable(JNIEnv* env, LuaEngine* luaEngine, jobject bundle);
 
 }  // namespace script_executor
 }  // namespace telemetry
 }  // namespace automotive
 }  // namespace android
 
-#endif  // CPP_TELEMETRY_SCRIPT_EXECUTOR_SRC_LUAENGINE_H_
+#endif  // CPP_TELEMETRY_SCRIPT_EXECUTOR_SRC_JNIUTILS_H_
