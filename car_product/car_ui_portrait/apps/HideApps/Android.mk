8196e4ac1b235646d254d6ac0ee7bb5243685fad
Heemin Seog <hseog@google.com>
2021-06-03 19:09:40 +0000

    Remove reference ui RROs from exclusion list
    Test: manual
    Bug: 193919000
    Change-Id: I9ad998a70d564d8e166d821838da1661df9c894e
    Merged-In: I9ad998a70d564d8e166d821838da1661df9c894e

--- car_product/car_ui_portrait/apps/HideApps/Android.mk
+++ car_product/car_ui_portrait/apps/HideApps/Android.mk
@@ -28,30 +28,5 @@ LOCAL_OVERRIDES_PACKAGES := \
     RotaryPlayground \
     RotaryIME \
     CarRotaryImeRRO \
-    googlecarui-com-android-car-ui-paintbooth \
-    googlecarui-com-android-car-rotaryplayground \
-    googlecarui-com-android-car-themeplayground \
-    googlecarui-com-android-car-carlauncher \
-    googlecarui-com-android-car-home \
-    googlecarui-com-android-car-media \
-    googlecarui-com-android-car-radio \
-    googlecarui-com-android-car-calendar \
-    googlecarui-com-android-car-companiondevicesupport \
-    googlecarui-com-android-car-systemupdater \
-    googlecarui-com-android-car-dialer \
-    googlecarui-com-android-car-linkviewer \
-    googlecarui-com-android-car-settings \
-    googlecarui-com-android-car-voicecontrol \
-    googlecarui-com-android-car-faceenroll \
-    googlecarui-com-android-permissioncontroller \
-    googlecarui-com-android-settings-intelligence \
-    googlecarui-com-google-android-apps-automotive-inputmethod \
-    googlecarui-com-google-android-apps-automotive-inputmethod-dev \
-    googlecarui-com-google-android-embedded-projection \
-    googlecarui-com-google-android-gms \
-    googlecarui-com-google-android-packageinstaller \
-    googlecarui-com-google-android-carassistant \
-    googlecarui-com-google-android-tts \
-    googlecarui-com-android-vending \
 
 include $(BUILD_PACKAGE)
