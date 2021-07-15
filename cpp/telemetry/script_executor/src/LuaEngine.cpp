f3857ea78ccf7dc77fa285974b8d499225450bf0
Max Dashouk <mdashouk@google.com>
2021-07-14 18:46:52 -0700

    Introduce ability for the script to send the results back.
    Implements OnSuccess API method of ScriptExecutorListener.
    Adds routines to convert Lua table to Java Bundle.
    Adds test cases to verify that the script can process inputs and return
    the expected results back.
    Additionally, changes naming of methods in native code for
    ScriptExecutor to be lower-case.
    
    Bug: 187517415
    Test: atest CarServiceUnitTest:ScriptExecutorTest
    Change-Id: I23efe323f520e9ab17e27c8f4117068839d4a10e

--- cpp/telemetry/script_executor/src/LuaEngine.cpp
+++ cpp/telemetry/script_executor/src/LuaEngine.cpp
@@ -16,10 +16,15 @@
 
 #include "LuaEngine.h"
 
+#include "BundleWrapper.h"
+
+#include <android-base/logging.h>
+
 #include <utility>
 
 extern "C" {
 #include "lauxlib.h"
+#include "lua.h"
 #include "lualib.h"
 }
 
@@ -27,6 +32,16 @@ namespace android {
 namespace automotive {
 namespace telemetry {
 namespace script_executor {
+
+namespace {
+
+enum LuaNumReturnedResults {
+    ZERO_RETURNED_RESULTS = 0,
+};
+
+}  // namespace
+
+ScriptExecutorListener* LuaEngine::sListener = nullptr;
 
 LuaEngine::LuaEngine() {
     // Instantiate Lua environment
@@ -38,15 +53,18 @@ LuaEngine::~LuaEngine() {
     lua_close(mLuaState);
 }
 
-lua_State* LuaEngine::GetLuaState() {
+lua_State* LuaEngine::getLuaState() {
     return mLuaState;
 }
 
-void LuaEngine::ResetListener(ScriptExecutorListener* listener) {
-    mListener.reset(listener);
+void LuaEngine::resetListener(ScriptExecutorListener* listener) {
+    if (sListener != nullptr) {
+        delete sListener;
+    }
+    sListener = listener;
 }
 
-int LuaEngine::LoadScript(const char* scriptBody) {
+int LuaEngine::loadScript(const char* scriptBody) {
     // As the first step in Lua script execution we want to load
     // the body of the script into Lua stack and have it processed by Lua
     // to catch any errors.
@@ -61,11 +79,15 @@ int LuaEngine::LoadScript(const char* scriptBody) {
         // Starting read about Lua stack: https://www.lua.org/pil/24.2.html
         // TODO(b/192284232): add test case to trigger this.
         lua_pop(mLuaState, 1);
+        return status;
     }
+
+    // Register limited set of reserved methods for Lua to call native side.
+    lua_register(mLuaState, "on_success", LuaEngine::onSuccess);
     return status;
 }
 
-bool LuaEngine::PushFunction(const char* functionName) {
+bool LuaEngine::pushFunction(const char* functionName) {
     // Interaction between native code and Lua happens via Lua stack.
     // In such model, a caller first pushes the name of the function
     // that needs to be called, followed by the function's input
@@ -78,7 +100,7 @@ bool LuaEngine::PushFunction(const char* functionName) {
     return status;
 }
 
-int LuaEngine::Run() {
+int LuaEngine::run() {
     // Performs blocking call of the provided Lua function. Assumes all
     // input arguments are in the Lua stack as well in proper order.
     // On how to call Lua functions: https://www.lua.org/pil/25.2.html
@@ -87,6 +109,54 @@ int LuaEngine::Run() {
     // change from 1 to 2.
     // TODO(b/192284612): add test case for failed call.
     return lua_pcall(mLuaState, /* nargs= */ 1, /* nresults= */ 0, /*errfunc= */ 0);
+}
+
+int LuaEngine::onSuccess(lua_State* lua) {
+    if (sListener == nullptr) {
+        LOG(FATAL) << "sListener object must not be null";
+    }
+    // Any script we run can call on_success only with a single argument of Lua table type.
+    if (lua_gettop(lua) != 1 || !lua_istable(lua, /* index =*/-1)) {
+        // TODO(b/193565932): Return programming error through binder callback interface.
+        LOG(ERROR) << "Only a single input argument, a Lua table object, expected here";
+    }
+
+    // Helper object to create and populate Java Bundle object.
+    BundleWrapper bundleWrapper(sListener->getCurrentJNIEnv());
+    // Iterate over Lua table which is expected to be at the top of Lua stack.
+    // lua_next call pops the key from the top of the stack and finds the next
+    // key-value pair for the popped key. It returns 0 if the next pair was not found.
+    // More on lua_next in: https://www.lua.org/manual/5.3/manual.html#lua_next
+    lua_pushnil(lua);  // First key is a null value.
+    while (lua_next(lua, /* index =*/-2) != 0) {
+        //  'key' is at index -2 and 'value' is at index -1
+        // -1 index is the top of the stack.
+        // remove 'value' and keep 'key' for next iteration
+        // Process each key-value depending on a type and push it to Java Bundle.
+        const char* key = lua_tostring(lua, /* index =*/-2);
+        if (lua_isboolean(lua, /* index =*/-1)) {
+            bundleWrapper.putBoolean(key, static_cast<bool>(lua_toboolean(lua, /* index =*/-1)));
+        } else if (lua_isinteger(lua, /* index =*/-1)) {
+            bundleWrapper.putInteger(key, static_cast<int>(lua_tointeger(lua, /* index =*/-1)));
+        } else if (lua_isnumber(lua, /* index =*/-1)) {
+            bundleWrapper.putDouble(key, static_cast<double>(lua_tonumber(lua, /* index =*/-1)));
+        } else if (lua_isstring(lua, /* index =*/-1)) {
+            bundleWrapper.putString(key, lua_tostring(lua, /* index =*/-1));
+        } else {
+            // not supported yet...
+            LOG(WARNING) << "key=" << key << " has a Lua type which is not supported yet. "
+                         << "The bundle object will not have this key-value pair.";
+        }
+        // Pop 1 element from the stack.
+        lua_pop(lua, 1);
+        // The key is at index -1, the table is at index -2 now.
+    }
+
+    // Forward the populated Bundle object to Java callback.
+    sListener->onSuccess(bundleWrapper.getBundle());
+    // We explicitly must tell Lua how many results we return, which is 0 in this case.
+    // More on the topic: https://www.lua.org/manual/5.3/manual.html#lua_CFunction
+    return ZERO_RETURNED_RESULTS;
 }
 
 }  // namespace script_executor
--- tests/carservice_unit_test/src/com/android/car/telemetry/ScriptExecutorTest.java
+++ tests/carservice_unit_test/src/com/android/car/telemetry/ScriptExecutorTest.java
@@ -16,6 +16,8 @@
 
 package com.android.car.telemetry;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import static org.junit.Assert.fail;
 
 import android.car.telemetry.IScriptExecutor;
@@ -47,12 +49,17 @@ public final class ScriptExecutorTest {
 
 
     private static final class ScriptExecutorListener extends IScriptExecutorListener.Stub {
+        public Bundle mSavedBundle;
+        public final CountDownLatch mSuccessLatch = new CountDownLatch(1);
+
         @Override
         public void onScriptFinished(byte[] result) {
         }
 
         @Override
         public void onSuccess(Bundle stateToPersist) {
+            mSavedBundle = stateToPersist;
+            mSuccessLatch.countDown();
         }
 
         @Override
@@ -60,7 +67,7 @@ public final class ScriptExecutorTest {
         }
     }
 
-    private final IScriptExecutorListener mFakeScriptExecutorListener =
+    private final ScriptExecutorListener mFakeScriptExecutorListener =
             new ScriptExecutorListener();
 
     // TODO(b/189241508). Parsing of publishedData is not implemented yet.
@@ -69,15 +76,16 @@ public final class ScriptExecutorTest {
     private final Bundle mSavedState = new Bundle();
 
     private static final String LUA_SCRIPT =
-            "function hello(data, state)\n"
-            + "    print(\"Hello World\")\n"
-            + "end\n";
+            "function hello(state)\n"
+                    + "    print(\"Hello World\")\n"
+                    + "end\n";
 
     private static final String LUA_METHOD = "hello";
 
     private final CountDownLatch mBindLatch = new CountDownLatch(1);
 
     private static final int BIND_SERVICE_TIMEOUT_SEC = 5;
+    private static final int SCRIPT_SUCCESS_TIMEOUT_SEC = 10;
 
     private final ServiceConnection mScriptExecutorConnection =
             new ServiceConnection() {
@@ -92,6 +100,22 @@ public final class ScriptExecutorTest {
                     fail("Service unexpectedly disconnected");
                 }
             };
+
+    // Helper method to invoke the script and wait for it to complete and return the result.
+    public void runScriptAndWaitForResult(String script, String function, Bundle previousState)
+            throws RemoteException {
+        mScriptExecutor.invokeScript(script, function, mPublishedData, previousState,
+                mFakeScriptExecutorListener);
+        try {
+            if (!mFakeScriptExecutorListener.mSuccessLatch.await(SCRIPT_SUCCESS_TIMEOUT_SEC,
+                    TimeUnit.SECONDS)) {
+                fail("Failed to get on_success called by the script on time");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
 
     @Before
     public void setUp() throws InterruptedException {
@@ -118,6 +142,130 @@ public final class ScriptExecutorTest {
             e.printStackTrace();
             fail(e.getMessage());
         }
+    }
+
+    @Test
+    public void invokeScript_returnsResult() throws RemoteException {
+        String returnResultScript =
+                "function hello(state)\n"
+                        + "    result = {hello=\"world\"}\n"
+                        + "    on_success(result)\n"
+                        + "end\n";
+
+
+        runScriptAndWaitForResult(returnResultScript, "hello", mSavedState);
+
+        // Expect to get back a bundle with a single string key: string value pair:
+        // {"hello": "world"}
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.size()).isEqualTo(1);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getString("hello")).isEqualTo("world");
+    }
+
+    @Test
+    public void invokeScript_allSupportedTypes() throws RemoteException {
+        String script =
+                "function knows(state)\n"
+                        + "    result = {string=\"hello\", boolean=true, integer=1, number=1.1}\n"
+                        + "    on_success(result)\n"
+                        + "end\n";
+
+
+        runScriptAndWaitForResult(script, "knows", mSavedState);
+
+        // Expect to get back a bundle with 4 keys, each corresponding to a distinct supported type.
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.size()).isEqualTo(4);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getString("string")).isEqualTo("hello");
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getBoolean("boolean")).isEqualTo(true);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getInt("integer")).isEqualTo(1);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getDouble("number")).isEqualTo(1.1);
+    }
+
+    @Test
+    public void invokeScript_skipsUnsupportedTypes() throws RemoteException {
+        String script =
+                "function nested(state)\n"
+                        + "    result = {string=\"hello\", boolean=true, integer=1, number=1.1}\n"
+                        + "    result.nested_table = {x=0, y=0}\n"
+                        + "    on_success(result)\n"
+                        + "end\n";
+
+
+        runScriptAndWaitForResult(script, "nested", mSavedState);
+
+        // Bundle does not contain any value under "nested_table" key, because nested tables are
+        // not supported yet.
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.size()).isEqualTo(4);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getString("nested_table")).isNull();
+    }
+
+    @Test
+    public void invokeScript_emptyBundle() throws RemoteException {
+        String script =
+                "function empty(state)\n"
+                        + "    result = {}\n"
+                        + "    on_success(result)\n"
+                        + "end\n";
+
+
+        runScriptAndWaitForResult(script, "empty", mSavedState);
+
+        // If a script returns empty table as the result, we get an empty bundle.
+        assertThat(mFakeScriptExecutorListener.mSavedBundle).isNotNull();
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void invokeScript_processPreviousStateAndReturnResult() throws RemoteException {
+        // Here we verify that the script actually processes provided state from a previous run
+        // and makes calculation based on that and returns the result.
+        // TODO(b/189241508): update function signatures.
+        String script =
+                "function update(state)\n"
+                        + "    result = {y = state.x+1}\n"
+                        + "    on_success(result)\n"
+                        + "end\n";
+        Bundle previousState = new Bundle();
+        previousState.putInt("x", 1);
+
+
+        runScriptAndWaitForResult(script, "update", previousState);
+
+        // Verify that y = 2, because y = x + 1 and x = 1.
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.size()).isEqualTo(1);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getInt("y")).isEqualTo(2);
+    }
+
+    @Test
+    public void invokeScript_allSupportedTypesWorkRoundTripWithKeyNamesPreserved()
+            throws RemoteException {
+        // Here we verify that all supported types in supplied previous state Bundle are interpreted
+        // by the script as expected.
+        // TODO(b/189241508): update function signatures.
+        String script =
+                "function update_all(state)\n"
+                        + "    result = {}\n"
+                        + "    result.integer = state.integer + 1\n"
+                        + "    result.number = state.number + 0.1\n"
+                        + "    result.boolean = not state.boolean\n"
+                        + "    result.string = state.string .. \"CADABRA\"\n"
+                        + "    on_success(result)\n"
+                        + "end\n";
+        Bundle previousState = new Bundle();
+        previousState.putInt("integer", 1);
+        previousState.putDouble("number", 0.1);
+        previousState.putBoolean("boolean", false);
+        previousState.putString("string", "ABRA");
+
+
+        runScriptAndWaitForResult(script, "update_all", previousState);
+
+        // Verify that keys are preserved but the values are modified as expected.
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.size()).isEqualTo(4);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getInt("integer")).isEqualTo(2);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getDouble("number")).isEqualTo(0.2);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getBoolean("boolean")).isEqualTo(true);
+        assertThat(mFakeScriptExecutorListener.mSavedBundle.getString("string")).isEqualTo(
+                "ABRACADABRA");
     }
 }
 
