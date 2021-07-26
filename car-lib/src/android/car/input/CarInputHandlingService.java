3143bbbcf63bfbb3fbe299443122ef6a7d1d4992
Antonio Kantek <kanant@google.com>
2021-07-26 13:25:19 -0700

    Deleting already deprecated CarInputHandlingService
    Fixes: 188103727
    Test: atest CarServiceUnitTest
    Test: atest CarServiceTest
    Change-Id: I862f5087f258df7c05e275703870e1e3c9b0e787

--- car-lib/src/android/car/input/CarInputHandlingService.java
+++ car-lib/src/android/car/input/CarInputHandlingService.java
@@ -55,7 +55,7 @@ import java.lang.ref.WeakReference;
  * @deprecated No longer needed after the new Car Input API was introduced (see
  *         {@link CarInputManager} for more details).
  *
- * @remove to be removed in T
+ * @removed
  *
  * @hide
  */
