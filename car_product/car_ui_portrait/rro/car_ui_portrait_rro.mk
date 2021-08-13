6e449ab599b7918105d51cffc2564f6b00fd9801
priyank <priyanksingh@google.com>
2021-08-12 23:58:54 -0700

    Update Dialer to specs
    Bug: 196394827
    Test: Manual
    Change-Id: I6e7291d83d32ae8c2a03d3787ff2201593bcdce0

--- car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk
+++ car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk
@@ -18,6 +18,7 @@ $(call inherit-product, packages/services/Car/car_product/car_ui_portrait/rro/ca
 
 # All RROs to be included in car_ui_portrait builds.
 PRODUCT_PACKAGES += \
+    CarUiPortraitDialerRRO \
     CarUiPortraitMediaRRO \
     CarUiPortraitLauncherRRO \
     CarUiPortraitNotificationRRO \
