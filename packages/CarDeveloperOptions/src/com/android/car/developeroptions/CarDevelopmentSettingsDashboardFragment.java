5dc2295fd660c2c333d53e3b0eab69a96946cefe
Saeid Farivar Asanjan <farivar@google.com>
2021-08-19 22:08:49 +0000

    Fix crash when accessing developer options.
    Apparently CarDeveloperOptions is extending Theme.CarUi.WithToolbar
    but it's not extending car-ui-lib's PreferenceFramgnet.
    
    This causes a crash at runtime, because the fragment can't access the
    undelying androidx Recyclerview as it used to be.
    
    Bug: 197096138
    Test: manually
    Change-Id: Ied086e52dc87d7a61d9180d712185fed6a35c1ed

--- packages/CarDeveloperOptions/src/com/android/car/developeroptions/CarDevelopmentSettingsDashboardFragment.java
+++ packages/CarDeveloperOptions/src/com/android/car/developeroptions/CarDevelopmentSettingsDashboardFragment.java
@@ -23,7 +23,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.car.ui.recyclerview.CarUiRecyclerView;
 import com.android.car.ui.toolbar.MenuItem;
 import com.android.car.ui.toolbar.Toolbar;
 import com.android.car.ui.toolbar.ToolbarController;
@@ -74,6 +79,20 @@ public class CarDevelopmentSettingsDashboardFragment extends DevelopmentSettings
                     PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                     PackageManager.DONT_KILL_APP);
             finish();
+        }
+    }
+
+    @Override
+    public RecyclerView onCreateRecyclerView(LayoutInflater inflater, ViewGroup parent,
+            Bundle savedInstanceState) {
+        Object carUiRecyclerView = parent.findViewById(R.id.recycler_view);
+        RecyclerView recyclerView = (carUiRecyclerView instanceof CarUiRecyclerView)
+                ? ((CarUiRecyclerView) carUiRecyclerView).getRecyclerView()
+                : (RecyclerView) carUiRecyclerView;
+        if (recyclerView != null) {
+            return recyclerView;
+        } else {
+            return super.onCreateRecyclerView(inflater, parent, savedInstanceState);
         }
     }
 
