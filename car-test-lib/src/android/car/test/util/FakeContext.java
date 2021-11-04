f305fc76b890d4f7bb3f4026c1fdd73f89eefb78
Ashutosh Agarwal <radsaggi@google.com>
2021-11-04 17:34:04 +0000

    Add support for EXTERNAL_CAR_TIME in TimeHalService in CarServices.
    When config_enableExternalCarTimeToExternalTimeSuggestion is enabled
    and the VHAL supports EXTERNAL_CAR_TIME, the TimeHalService sends
    ExternalTimeSuggestions to TimeManager system service for any updates
    received on the EXTERNAL_CAR_TIME VHAL property.
    
    Added support in TimeHalService for the new AIDL properties instead of
    the HIDL properties.
    
    Bug: 202377635
    Bug: 202377490
    Test: atest CarServiceUnitTest:VehicleHalTest CarServiceUnitTest:TimeHalServiceTest
    Change-Id: Iea5bb66a63fd494e884ba2a48861a46a86e03d5a

--- car-test-lib/src/android/car/test/util/FakeContext.java
+++ car-test-lib/src/android/car/test/util/FakeContext.java
@@ -22,24 +22,66 @@ import static com.google.common.truth.Truth.assertWithMessage;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.os.Handler;
 import android.os.MessageQueue;
 import android.test.mock.MockContext;
+import android.util.ArrayMap;
 
 import com.google.common.collect.Lists;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * A fake implementation for {@link android.content.Context}, that helps broadcast {@link Intent}s
- * to registered {@link BroadcastReceiver} instances.
+ * A fake implementation for {@link android.content.Context}, that provides the following
+ * functions:
+ * <ul>
+ *     <li> Fake implementations for {@link #registerReceiver} and {@link #unregisterReceiver}.
+ *     The helper methods {@link #verifyReceiverRegistered} and
+ *     {@link #verifyReceiverNotRegistered} can be used to validate the state.
+ *     <li> Fake implementation for {@link #sendBroadcast} that sends the intent to a registered
+ *     {@link BroadcastReceiver}.
+ *     <li> Fake implementations for {@link #getSystemService} and {@link #getSystemServiceName}.
+ *     Helper method {@link #setSystemService} can be used to provide values returned by these
+ *     methods.
+ *     <li> Fake implementation for {@link #getResources}. Helper method {@link #setResources}
+ *     can be used to provide a value for this.
+ * </ul>
  */
 // TODO(b/202420937): Add unit tests for this class.
-public final class BroadcastingFakeContext extends MockContext {
+public final class FakeContext extends MockContext {
+
+    private final Map<String, Object> mSystemServices = new ArrayMap<>();
+
     private BroadcastReceiver mReceiver;
     private IntentFilter mIntentFilter;
     private Handler mHandler;
+    private Resources mResources;
+
+    public <T> void setSystemService(Class<T> serviceClass, T serviceInstance) {
+        mSystemServices.put(serviceClass.getName(), serviceInstance);
+    }
+
+    @Override
+    public Object getSystemService(String name) {
+        return mSystemServices.get(name);
+    }
+
+    @Override
+    public String getSystemServiceName(Class<?> serviceClass) {
+        return serviceClass.getName();
+    }
+
+    @Override
+    public Resources getResources() {
+        return mResources;
+    }
+
+    public void setResources(Resources resources) {
+        mResources = resources;
+    }
 
     @Override
     public Intent registerReceiver(BroadcastReceiver receiver, IntentFilter filter) {
--- service/src/com/android/car/hal/TimeHalService.java
+++ service/src/com/android/car/hal/TimeHalService.java
@@ -16,13 +16,17 @@
 
 package com.android.car.hal;
 
-import static android.hardware.automotive.vehicle.V2_0.VehicleProperty.EPOCH_TIME;
+import static android.hardware.automotive.vehicle.VehicleProperty.ANDROID_EPOCH_TIME;
+import static android.hardware.automotive.vehicle.VehicleProperty.EXTERNAL_CAR_TIME;
 
 import static com.android.car.internal.ExcludeFromCodeCoverageGeneratedReport.DUMP_INFO;
 
 import static java.util.Objects.requireNonNull;
 
 import android.annotation.Nullable;
+import android.app.time.ExternalTimeSuggestion;
+import android.app.time.TimeManager;
+import android.car.builtin.util.Slogf;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -32,8 +36,11 @@ import android.hardware.automotive.vehicle.V2_0.VehiclePropConfig;
 import android.hardware.automotive.vehicle.V2_0.VehiclePropValue;
 import android.hardware.automotive.vehicle.V2_0.VehiclePropertyStatus;
 
+import com.android.car.CarLog;
+import com.android.car.R;
 import com.android.car.internal.ExcludeFromCodeCoverageGeneratedReport;
 import com.android.car.internal.util.IndentingPrintWriter;
+import com.android.internal.annotations.GuardedBy;
 
 import java.io.PrintWriter;
 import java.time.Instant;
@@ -43,55 +50,91 @@ import java.util.List;
 /** Writes the Android System time to EPOCH_TIME in the VHAL, if supported. */
 public final class TimeHalService extends HalServiceBase {
 
-    private static final int[] SUPPORTED_PROPERTIES = new int[]{EPOCH_TIME};
+    private static final int[] SUPPORTED_PROPERTIES =
+            new int[]{ANDROID_EPOCH_TIME, EXTERNAL_CAR_TIME};
 
     private final Context mContext;
-
     private final VehicleHal mHal;
+    private final TimeManager mTimeManager;
+
+    private final boolean mEnableExternalCarTimeSuggestions;
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_TIME_CHANGED)) {
-                updateProperty(System.currentTimeMillis());
+            if (!intent.getAction().equals(Intent.ACTION_TIME_CHANGED)) {
+                return;
+            }
+
+            synchronized (mLock) {
+                if (mAndroidTimeSupported) {
+                    updateAndroidEpochTimePropertyLocked(System.currentTimeMillis());
+                }
             }
         }
     };
 
+    private final Object mLock = new Object();
+
+    @GuardedBy("mLock")
     private boolean mReceiverRegistered;
 
+    @GuardedBy("mLock")
     @Nullable
     private Instant mLastAndroidTimeReported;
 
+    @GuardedBy("mLock")
+    @Nullable
+    private ExternalTimeSuggestion mLastExternalTimeSuggestion;
+
+    @GuardedBy("mLock")
     private boolean mAndroidTimeSupported;
+
+    @GuardedBy("mLock")
+    private boolean mExternalCarTimeSupported;
 
     TimeHalService(Context context, VehicleHal hal) {
         mContext = requireNonNull(context);
         mHal = requireNonNull(hal);
+        mTimeManager = requireNonNull(context.getSystemService(TimeManager.class));
+        mEnableExternalCarTimeSuggestions = mContext.getResources().getBoolean(
+                R.bool.config_enableExternalCarTimeToExternalTimeSuggestion);
     }
 
     @Override
     public void init() {
-        if (!mAndroidTimeSupported) {
-            return;
+        synchronized (mLock) {
+            if (mAndroidTimeSupported) {
+                updateAndroidEpochTimePropertyLocked(System.currentTimeMillis());
+
+                IntentFilter filter = new IntentFilter(Intent.ACTION_TIME_CHANGED);
+                mContext.registerReceiver(mReceiver, filter);
+                mReceiverRegistered = true;
+                Slogf.d(CarLog.TAG_TIME,
+                        "Registered BroadcastReceiver for Intent.ACTION_TIME_CHANGED");
+            }
+
+            if (mExternalCarTimeSupported) {
+                VehiclePropValue propValue = mHal.get(EXTERNAL_CAR_TIME);
+                suggestExternalTimeLocked(propValue);
+            }
         }
-
-        updateProperty(System.currentTimeMillis());
-
-        IntentFilter filter = new IntentFilter(Intent.ACTION_TIME_CHANGED);
-        mContext.registerReceiver(mReceiver, filter);
-        mReceiverRegistered = true;
     }
 
     @Override
     public void release() {
-        if (mReceiverRegistered) {
-            mContext.unregisterReceiver(mReceiver);
-            mReceiverRegistered = false;
-        }
+        synchronized (mLock) {
+            if (mReceiverRegistered) {
+                mContext.unregisterReceiver(mReceiver);
+                mReceiverRegistered = false;
+            }
 
-        mAndroidTimeSupported = false;
-        mLastAndroidTimeReported = null;
+            mAndroidTimeSupported = false;
+            mLastAndroidTimeReported = null;
+
+            mExternalCarTimeSupported = false;
+            mLastExternalTimeSuggestion = null;
+        }
     }
 
     @Override
@@ -103,31 +146,86 @@ public final class TimeHalService extends HalServiceBase {
     public void takeProperties(Collection<VehiclePropConfig> properties) {
         for (VehiclePropConfig property : properties) {
             switch (property.prop) {
-                case EPOCH_TIME:
-                    mAndroidTimeSupported = true;
-                    return;
+                case ANDROID_EPOCH_TIME:
+                    synchronized (mLock) {
+                        mAndroidTimeSupported = true;
+                    }
+                    break;
+                case EXTERNAL_CAR_TIME:
+                    if (mEnableExternalCarTimeSuggestions) {
+                        synchronized (mLock) {
+                            mExternalCarTimeSupported = true;
+                        }
+                    }
+                    break;
             }
         }
     }
 
     @Override
     public void onHalEvents(List<VehiclePropValue> values) {
+        synchronized (mLock) {
+            if (!mExternalCarTimeSupported) {
+                return;
+            }
+
+            for (VehiclePropValue value : values) {
+                if (value.prop == EXTERNAL_CAR_TIME) {
+                    suggestExternalTimeLocked(value);
+                    break;
+                }
+            }
+        }
     }
 
+    /** Returns whether the service has detected support for ANDROID_EPOCH_TIME VHAL property. */
     public boolean isAndroidTimeSupported() {
-        return mAndroidTimeSupported;
+        synchronized (mLock) {
+            return mAndroidTimeSupported;
+        }
     }
 
-    private void updateProperty(long timeMillis) {
+    /** Returns whether the service has detected support for EXTERNAL_CAR_TIME VHAL property. */
+    public boolean isExternalCarTimeSupported() {
+        synchronized (mLock) {
+            return mExternalCarTimeSupported;
+        }
+    }
+
+    @GuardedBy("mLock")
+    private void updateAndroidEpochTimePropertyLocked(long timeMillis) {
         VehiclePropValue propValue = new VehiclePropValue();
-        propValue.prop = EPOCH_TIME;
+        propValue.prop = ANDROID_EPOCH_TIME;
         propValue.areaId = VehicleArea.GLOBAL;
         propValue.status = VehiclePropertyStatus.AVAILABLE;
         propValue.timestamp = timeMillis;
         propValue.value.int64Values.add(timeMillis);
 
+        Slogf.d(CarLog.TAG_TIME, "Writing value %d to property ANDROID_EPOCH_TIME", timeMillis);
         mHal.set(propValue);
         mLastAndroidTimeReported = Instant.ofEpochMilli(timeMillis);
+    }
+
+    @GuardedBy("mLock")
+    private void suggestExternalTimeLocked(VehiclePropValue value) {
+        if (value.prop != EXTERNAL_CAR_TIME || value.status != VehiclePropertyStatus.AVAILABLE) {
+            return;
+        }
+
+        if (value.value.int64Values.size() != 1) {
+            Slogf.e(CarLog.TAG_TIME, "Invalid value received for EXTERNAL_CAR_TIME.\n"
+                    + "  Expected a single element in int64values.\n"
+                    + "  Received: %s", value.value);
+            return;
+        }
+        long epochTime = value.value.int64Values.get(0);
+        // timestamp is stored in nanoseconds but the suggest API uses milliseconds.
+        long elapsedRealtime = value.timestamp / 1_000_000;
+
+        mLastExternalTimeSuggestion = new ExternalTimeSuggestion(elapsedRealtime, epochTime);
+
+        Slogf.d(CarLog.TAG_TIME, "Sending Time Suggestion: " + mLastExternalTimeSuggestion);
+        mTimeManager.suggestExternalTime(mLastExternalTimeSuggestion);
     }
 
     @Override
@@ -135,13 +233,17 @@ public final class TimeHalService extends HalServiceBase {
     public void dump(PrintWriter printWriter) {
         IndentingPrintWriter writer = new IndentingPrintWriter(printWriter);
         writer.println("*ExternalTime HAL*");
-        if (mAndroidTimeSupported) {
-            writer.increaseIndent();
-            writer.printf(
-                    "mLastAndroidTimeReported: %d millis",
-                    mLastAndroidTimeReported.toEpochMilli());
-            writer.decreaseIndent();
+        writer.increaseIndent();
+        synchronized (mLock) {
+            if (mAndroidTimeSupported) {
+                writer.printf("mLastAndroidTimeReported: %d millis\n",
+                        mLastAndroidTimeReported.toEpochMilli());
+            }
+            if (mExternalCarTimeSupported) {
+                writer.printf("mLastExternalTimeSuggestion: %s\n", mLastExternalTimeSuggestion);
+            }
         }
+        writer.decreaseIndent();
         writer.flush();
     }
 }
