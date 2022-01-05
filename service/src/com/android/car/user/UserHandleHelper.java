8d27f58c69d0ae074a263d68a802e6f80e67e130
Mayank Garg <gargmayank@google.com>
2022-01-04 23:39:14 -0800

    Replacing IsProfileUser with System api
    Bug: 196179969
    Test: manual verification
    Test: m
    
    Change-Id: I1cb891564aab4c6acf7b837d2f71511589937d04

--- service/src/com/android/car/user/UserHandleHelper.java
+++ service/src/com/android/car/user/UserHandleHelper.java
@@ -111,7 +111,7 @@ public final class UserHandleHelper {
      * Is user for a profile?
      */
     public boolean isProfileUser(UserHandle user) {
-        return UserManagerHelper.isProfileUser(mUserManager, user);
+        return getUserContextAwareUserManager(user.getIdentifier()).isProfile();
     }
 
     /**
@@ -127,7 +127,6 @@ public final class UserHandleHelper {
     public boolean isPreCreatedUser(UserHandle user) {
         return UserManagerHelper.isPreCreatedUser(mUserManager, user);
     }
-
 
     /**
      * Get profile group Id for the user
