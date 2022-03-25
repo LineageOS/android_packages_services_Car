8f9cb772e63b9a0f39cec4161428d604d781af3b
Changyeon Jo <changyeon@google.com>
2022-03-25 05:13:10 -0700

    Update EVS service configurations
    - evsmanagerd is added to the build only if ENABLE_EVS_SERVICE is true.
    - Start evsmanagerd from init.evs.rc only if
      CUSTOMIZE_EVS_SERVICE_PARAMETER is not set as true.
    - Start android.hardware.automotive.evs-default, evs_app, and
      cardisplayproxyd from their own rc files.
    - Remove persist.automotive.evs.mode property.
    - Allow evs_app to perform the binder IPC to evsmanagerd.
    
    Bug: 226874713
    Test: Build seahawk and cf_x86_64_auto targets and manually confirm that
          EVS service is functioning properly by running evs_app
    Change-Id: I19ff95474454921894e23ef5a4577082f1a4d1ab

--- cpp/evs/apps/sepolicy/evsapp.mk
+++ cpp/evs/apps/sepolicy/evsapp.mk
@@ -1,3 +1,2 @@
 # Selinux policies for the sample EVS application
-#PRODUCT_PUBLIC_SEPOLICY_DIRS += packages/services/Car/cpp/evs/apps/default/sepolicy/public
 PRODUCT_PRIVATE_SEPOLICY_DIRS += packages/services/Car/cpp/evs/apps/sepolicy/private
