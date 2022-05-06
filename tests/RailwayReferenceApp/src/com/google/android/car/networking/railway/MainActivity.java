b0a2f49d7ce3b1d6f5d44908e0e33774b4787544
Leslie Watkins <lesliewatkins@google.com>
2022-05-06 09:55:18 -0700

    Railway Reference App: Add configuration update logic and output for current ethernet networks list.
    Also add project OWNERS file.
    
    Bug: 225226520
    Ignore-AOSP-First: currently unavailable on AOSP
    Test: m RailwayReferenceApp
    Change-Id: I86f7a80bfe65903d1673287a5cc21f23d725853d

--- tests/RailwayReferenceApp/src/com/google/android/car/networking/railway/MainActivity.java
+++ tests/RailwayReferenceApp/src/com/google/android/car/networking/railway/MainActivity.java
@@ -15,13 +15,179 @@
  */
 package com.google.android.car.networking.railway;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.EthernetNetworkManagementException;
+import android.net.LinkProperties;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
+import android.os.OutcomeReceiver;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public final class MainActivity extends Activity {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public final class MainActivity extends FragmentActivity {
+    private static final String TAG = MainActivity.class.getName();
+    private ConfigurationUpdater mConfigurationUpdater;
+    private CurrentEthernetNetworksViewModel mCurrentEthernetNetworksViewModel;
+
+    private final List<Button> mButtons = new ArrayList<>();
+
+    private EditText mAllowedPackageNames;
+    private EditText mIpConfiguration;
+    private EditText mInterfaceName;
+    private EditText mNetworkCapabilities;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAllowedPackageNames = findViewById(R.id.allowedPackageNamesInput);
+        mIpConfiguration = findViewById(R.id.ipConfigurationInput);
+        mInterfaceName = findViewById(R.id.interfaceNameInput);
+        mNetworkCapabilities = findViewById(R.id.networkCapabilitiesInput);
+
+        Button updateButton = findViewById(R.id.updateButton);
+        updateButton.setOnClickListener(v -> onUpdateButtonClick());
+
+        mButtons.add(updateButton);
+        // TODO(b/225226520): add connectButton to list when its function has been defined
+        // TODO(b/225226520): add enable/disableButton to list when implemented
+
+        TextView currentEthernetNetworksOutput = findViewById(R.id.currentEthernetNetworksOutput);
+
+        mCurrentEthernetNetworksViewModel =
+                new ViewModelProvider(this,
+                        new CurrentEthernetNetworksViewModelFactory(
+                                (Application) getApplicationContext(),
+                                new NetworkCallback()))
+                        .get(CurrentEthernetNetworksViewModel.class);
+        mCurrentEthernetNetworksViewModel.getNetworksLiveData().observe(this,
+                networkNetworkInfoMap -> currentEthernetNetworksOutput
+                        .setText(mCurrentEthernetNetworksViewModel
+                                .getCurrentEthernetNetworksText(networkNetworkInfoMap)));
+
+        mConfigurationUpdater = new ConfigurationUpdater(getApplicationContext(),
+                new OutcomeReceiver<String, EthernetNetworkManagementException>() {
+                    @Override
+                    public void onResult(String result) {
+                        showDialogWithTitle(result);
+                    }
+
+                    @Override
+                    public void onError(EthernetNetworkManagementException error) {
+                        OutcomeReceiver.super.onError(error);
+                        showDialogWithTitle(error.getLocalizedMessage());
+                    }
+                });
     }
+
+    private void setButtonsEnabled(boolean enabled) {
+        for (Button button : mButtons) {
+            button.setEnabled(enabled);
+        }
+    }
+
+    private void showDialogWithTitle(String title) {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setOnDismissListener(d -> setButtonsEnabled(true))
+                .create();
+        dialog.show();
+    }
+
+    private void onUpdateButtonClick() {
+        setButtonsEnabled(false);
+        Log.d(TAG, "configuration update started");
+        try {
+            mConfigurationUpdater.updateNetworkConfiguration(
+                    mAllowedPackageNames.getText().toString(),
+                    mIpConfiguration.getText().toString(),
+                    mNetworkCapabilities.getText().toString(),
+                    mInterfaceName.getText().toString());
+        } catch (IllegalArgumentException | PackageManager.NameNotFoundException e) {
+            showDialogWithTitle(e.getLocalizedMessage());
+        }
+    }
+
+    private class NetworkCallback extends ConnectivityManager.NetworkCallback {
+        @Override
+        public void onAvailable(Network network) {
+            super.onAvailable(network);
+            Log.d(TAG, "Network " + network + " available");
+
+            runOnUiThread(() -> mCurrentEthernetNetworksViewModel.updateNetwork(
+                            network, /* available= */ true)
+            );
+        }
+
+        @Override
+        public void onLost(Network network) {
+            super.onLost(network);
+            Log.d(TAG, "Network " + network + " lost");
+
+            runOnUiThread(() -> mCurrentEthernetNetworksViewModel.updateNetwork(
+                            network, /* available= */ false)
+            );
+        }
+
+        @Override
+        public void onCapabilitiesChanged(Network network,
+                NetworkCapabilities networkCapabilities) {
+            super.onCapabilitiesChanged(network, networkCapabilities);
+            Log.d(TAG, "Network " + network + " capabilities changed to "
+                    + Arrays.toString(networkCapabilities.getCapabilities()));
+
+            runOnUiThread(() -> mCurrentEthernetNetworksViewModel.updateNetwork(
+                            network, networkCapabilities)
+            );
+        }
+
+        @Override
+        public void onLinkPropertiesChanged(Network network,
+                LinkProperties linkProperties) {
+            super.onLinkPropertiesChanged(network, linkProperties);
+            Log.d(TAG, "Network " + network + " link properties changed");
+
+            MainActivity.this.runOnUiThread(
+                    () -> mCurrentEthernetNetworksViewModel.updateNetwork(
+                            network, linkProperties)
+            );
+        }
+    }
+
+    private static class CurrentEthernetNetworksViewModelFactory extends
+            ViewModelProvider.AndroidViewModelFactory {
+        private final Application mApplication;
+        private final ConnectivityManager.NetworkCallback mNetworkCallback;
+
+        private CurrentEthernetNetworksViewModelFactory(
+                Application application, ConnectivityManager.NetworkCallback networkCallback) {
+            super(application);
+            mApplication = application;
+            mNetworkCallback = networkCallback;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(Class<T> modelClass) {
+            return modelClass.cast(
+                    new CurrentEthernetNetworksViewModel(mApplication, mNetworkCallback));
+        }
+    }
+
 }
