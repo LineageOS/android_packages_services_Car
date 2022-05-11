690bc432af5937f88142e1776137509069c99879
Alex Stetson <alexstetson@google.com>
2022-05-11 16:56:04 -0700

    Immersive Mode Support for RickyBobby
    When an application requests immersive mode, an affordance will be shown
    in the title bar that allows users to expand the foreground display area
    to take up the entire screen.
    
    Bug: 223807221
    Test: manual
    
    Change-Id: Ib355673498cbb0d7235d031174a21d6877312a9b

--- car_product/car_ui_portrait/apps/CarUiPortraitSystemUI/src/com/android/systemui/wmshell/CarUiPortraitWMShellModule.java
+++ car_product/car_ui_portrait/apps/CarUiPortraitSystemUI/src/com/android/systemui/wmshell/CarUiPortraitWMShellModule.java
@@ -16,15 +16,46 @@
 
 package com.android.systemui.wmshell;
 
+import android.content.Context;
+import android.os.Handler;
+import android.view.IWindowManager;
+
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.wm.CarUiPortraitDisplaySystemBarsController;
+import com.android.wm.shell.common.DisplayController;
+import com.android.wm.shell.common.DisplayImeController;
+import com.android.wm.shell.common.DisplayInsetsController;
+import com.android.wm.shell.common.TransactionPool;
+import com.android.wm.shell.dagger.DynamicOverride;
 import com.android.wm.shell.dagger.WMShellBaseModule;
+import com.android.wm.shell.dagger.WMSingleton;
 import com.android.wm.shell.pip.Pip;
 
 import dagger.BindsOptionalOf;
 import dagger.Module;
+import dagger.Provides;
 
 /** Provides dependencies from {@link com.android.wm.shell} for CarSystemUI. */
 @Module(includes = WMShellBaseModule.class)
 public abstract class CarUiPortraitWMShellModule {
+
+    @WMSingleton
+    @Provides
+    static CarUiPortraitDisplaySystemBarsController bindCarUiPortraitDisplaySystemBarsController(
+            Context context, IWindowManager wmService, DisplayController displayController,
+            DisplayInsetsController displayInsetsController,
+            @Main Handler mainHandler, TransactionPool transactionPool) {
+        return new CarUiPortraitDisplaySystemBarsController(context, wmService, displayController,
+                displayInsetsController, mainHandler, transactionPool);
+    }
+
+    @WMSingleton
+    @Provides
+    @DynamicOverride
+    static DisplayImeController provideDisplayImeController(
+            CarUiPortraitDisplaySystemBarsController carUiPortraitDisplaySystemBarsController) {
+        return carUiPortraitDisplaySystemBarsController;
+    }
 
     @BindsOptionalOf
     abstract Pip optionalPip();
