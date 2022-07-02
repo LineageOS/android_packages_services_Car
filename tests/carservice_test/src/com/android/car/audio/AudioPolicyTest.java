7ddcccd00238a5c51ba40e2e704f1dfed16b678b
Eric Jeong <ericjeong@google.com>
2022-07-01 23:57:34 -0700

    Move AudioPolicyTest to audio folder
    - AudioPolicyTest.java is located at a wrong folder.
    - This CL moves it to tests/carservice_test/src/com/android/car/audio.
    - Also, this CL removes CarAudioManagerTest which doesn't test any.
    
    Bug: 237797785
    Bug: 237625745
    Test: atest CarServiceTest
    Change-Id: Icf9c0482466ba37680586c40682266873ea4940f

--- tests/carservice_test/src/com/android/car/audio/AudioPolicyTest.java
+++ tests/carservice_test/src/com/android/car/audio/AudioPolicyTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package android.media.tests;
+package com.android.car.audio;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
