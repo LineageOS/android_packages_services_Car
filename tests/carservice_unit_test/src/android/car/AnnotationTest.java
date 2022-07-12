e7506db9e57a76c2c170ee3a972e5592d9d0276d
Mayank Garg <gargmayank@google.com>
2022-07-11 22:07:02 -0700

    Improved test for AddedInOrBefore annotation
    - Ths improvement makes sure that no new API uses AddedInOrBefore
    annotation as it should only be used for API major version 33
    
    - Also moved the common code to common library to remove code duplicacy
    
    Bug: 237565347
    Test: atest android.car.AnnotationTest com.android.server.wm.AnnotationTest
    Change-Id: Ide2497d5dc8a54d343ba93514a23d8dbea721300

--- tests/carservice_unit_test/src/android/car/AnnotationTest.java
+++ tests/carservice_unit_test/src/android/car/AnnotationTest.java
@@ -16,18 +16,12 @@
 
 package android.car;
 
-import static com.google.common.truth.Truth.assertWithMessage;
+import static android.car.test.util.AnnotationHelper.checkForAnnotation;
 
 import android.car.annotation.AddedIn;
 import android.car.annotation.AddedInOrBefore;
 
 import org.junit.Test;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class AnnotationTest {
     private static final String TAG = AnnotationTest.class.getSimpleName();
@@ -334,85 +328,5 @@ public final class AnnotationTest {
     @Test
     public void testCarBuiltInAPIAddedInAnnotation() throws Exception {
         checkForAnnotation(CAR_BUILT_IN_API_CLASSES, android.car.builtin.annotation.AddedIn.class);
-    }
-
-    private void checkForAnnotation(String[] classes, Class... annotationClasses) throws Exception {
-        List<String> errorsNoAnnotation = new ArrayList<>();
-        List<String> errorsExtraAnnotation = new ArrayList<>();
-
-        for (int i = 0; i < classes.length; i++) {
-            String className = classes[i];
-            Field[] fields = Class.forName(className).getDeclaredFields();
-            for (int j = 0; j < fields.length; j++) {
-                Field field = fields[j];
-                boolean isAnnotated = containsAddedInAnnotation(field, annotationClasses);
-                boolean isPrivate = Modifier.isPrivate(field.getModifiers());
-
-                if (isPrivate && isAnnotated) {
-                    errorsExtraAnnotation.add(className + " FIELD: " + field.getName());
-                }
-
-                if (!isPrivate && !isAnnotated) {
-                    errorsNoAnnotation.add(className + " FIELD: " + field.getName());
-                }
-            }
-
-            Method[] methods = Class.forName(className).getDeclaredMethods();
-            for (int j = 0; j < methods.length; j++) {
-                Method method = methods[j];
-
-                // These are some internal methods
-                if (method.getName().contains("$")) continue;
-
-                boolean isAnnotated = containsAddedInAnnotation(method, annotationClasses);
-                boolean isPrivate = Modifier.isPrivate(method.getModifiers());
-
-                if (isPrivate && isAnnotated) {
-                    errorsExtraAnnotation.add(className + " METHOD: " + method.getName());
-                }
-
-                if (!isPrivate && !isAnnotated) {
-                    errorsNoAnnotation.add(className + " METHOD: " + method.getName());
-                }
-            }
-        }
-
-        StringBuilder errorFlatten = new StringBuilder();
-        if (!errorsNoAnnotation.isEmpty()) {
-            errorFlatten.append("Errors:\nNo AddedIn annotation found for-\n");
-            for (int i = 0; i < errorsNoAnnotation.size(); i++) {
-                errorFlatten.append(errorsNoAnnotation.get(i) + "\n");
-            }
-        }
-
-        if (!errorsExtraAnnotation.isEmpty()) {
-            errorFlatten.append("\nErrors:\nExtra AddedIn annotation found for-\n");
-            for (int i = 0; i < errorsExtraAnnotation.size(); i++) {
-                errorFlatten.append(errorsExtraAnnotation.get(i) + "\n");
-            }
-        }
-
-        assertWithMessage(errorFlatten.toString())
-                .that(errorsExtraAnnotation.size() + errorsNoAnnotation.size()).isEqualTo(0);
-    }
-
-    @SuppressWarnings("unchecked")
-    private boolean containsAddedInAnnotation(Field field, Class... annotationClasses) {
-        for (int i = 0; i < annotationClasses.length; i++) {
-            if (field.getAnnotation(annotationClasses[i]) != null) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @SuppressWarnings("unchecked")
-    private boolean containsAddedInAnnotation(Method method, Class... annotationClasses) {
-        for (int i = 0; i < annotationClasses.length; i++) {
-            if (method.getAnnotation(annotationClasses[i]) != null) {
-                return true;
-            }
-        }
-        return false;
     }
 }
