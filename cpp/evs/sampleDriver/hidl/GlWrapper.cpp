afe0ffe3ac6e61d9866aa8da1512ea3311991682
Changyeon Jo <changyeon@google.com>
2022-07-26 21:14:23 -0700

    Modifies EVS shader programs
    This CL modifies EVS shader programs of the following EVS components, to
    make them be compatible with OpenGL ES 2.0.
    - A sample HIDL EVS HAL implementation
    - A sample AIDL EVS HAL implementation
    - And, CarEvsCameraPreviewApp
    
    Bug: 240306544
    Test: Run EVS applications on cuttlefish
    Change-Id: Ie862c3b6c004aa18b3a0b8b702328e4351b052ad

--- cpp/evs/sampleDriver/hidl/GlWrapper.cpp
+++ cpp/evs/sampleDriver/hidl/GlWrapper.cpp
@@ -29,27 +29,22 @@
 using android::GraphicBuffer;
 using android::sp;
 
-const char vertexShaderSource[] = ""
-                                  "#version 300 es                    \n"
-                                  "layout(location = 0) in vec4 pos;  \n"
-                                  "layout(location = 1) in vec2 tex;  \n"
-                                  "out vec2 uv;                       \n"
-                                  "void main()                        \n"
-                                  "{                                  \n"
-                                  "   gl_Position = pos;              \n"
-                                  "   uv = tex;                       \n"
-                                  "}                                  \n";
+const char vertexShaderSource[] = "attribute vec4 pos;                  \n"
+                                  "attribute vec2 tex;                  \n"
+                                  "varying vec2 uv;                     \n"
+                                  "void main()                          \n"
+                                  "{                                    \n"
+                                  "   gl_Position = pos;                \n"
+                                  "   uv = tex;                         \n"
+                                  "}                                    \n";
 
-const char pixelShaderSource[] = "#version 300 es                            \n"
-                                 "precision mediump float;                   \n"
-                                 "uniform sampler2D tex;                     \n"
-                                 "in vec2 uv;                                \n"
-                                 "out vec4 color;                            \n"
-                                 "void main()                                \n"
-                                 "{                                          \n"
-                                 "    vec4 texel = texture(tex, uv);         \n"
-                                 "    color = texel;                         \n"
-                                 "}                                          \n";
+const char pixelShaderSource[] = "precision mediump float;              \n"
+                                 "uniform sampler2D tex;                \n"
+                                 "varying vec2 uv;                      \n"
+                                 "void main()                           \n"
+                                 "{                                     \n"
+                                 "    gl_FragColor = texture2D(tex, uv);\n"
+                                 "}                                     \n";
 
 static const char* getEGLError(void) {
     switch (eglGetError()) {
@@ -148,6 +143,9 @@ static GLuint buildShaderProgram(const char* vtxSrc, const char* pxlSrc) {
     glAttachShader(program, vertexShader);
     glAttachShader(program, pixelShader);
 
+    glBindAttribLocation(program, 0, "pos");
+    glBindAttribLocation(program, 1, "tex");
+
     // Link the program
     glLinkProgram(program);
     GLint linked = 0;
@@ -223,7 +221,7 @@ bool GlWrapper::initialize(sp<IAutomotiveDisplayProxyService> pWindowProxy, uint
         return false;
     }
 
-    EGLint major = 3;
+    EGLint major = 2;
     EGLint minor = 0;
     if (!eglInitialize(mDisplay, &major, &minor)) {
         LOG(ERROR) << "Failed to initialize EGL: " << getEGLError();
