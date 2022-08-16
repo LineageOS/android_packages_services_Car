0ab146acb3eb49a11539ab22b6a79cd834d4f5bb
Mayank Garg <gargmayank@google.com>
2022-08-15 19:12:09 -0700

    Moved versionUtils to car-lib
    Bug: 242312199
    Test: atest VersionUtilsTest
    Change-Id: Iba39032f5d031a8b7db61f0113d7015059210a2a

--- tests/carservice_unit_test/src/com/android/car/internal/util/VersionUtilsTest.java
+++ tests/carservice_unit_test/src/com/android/car/internal/util/VersionUtilsTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.car.util;
+package com.android.car.internal.util;
 
 import static com.google.common.truth.Truth.assertThat;
 
