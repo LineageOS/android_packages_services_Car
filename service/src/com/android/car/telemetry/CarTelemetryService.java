ab9790aea7bf4d728611ca61e0257c992baab103
Rui Qiu <ruiqiu@google.com>
2022-08-19 11:05:36 -0700

    Block main thread in CarTelemetryService's release
    Fixes a race condition in CarServiceTest where the next test starts
    before the previous test's tearDown is properly finished.
    
    Bug: 238667486
    Test: atest com.android.car.telemetry
    Test: atest CarServiceTest:CarTelemetryManagerTest
    Test: atest CarServiceTest:CarTelemetryManagerSpyServiceTest
    Test: atest CtsCarTestCases:CarTelemetryManagerTest
    Test: atest CarTelemetryHostTest
    
    Change-Id: I87ca4a073bbbbc3ed1fa6da86f95583f9b9c1ca9

--- service/src/com/android/car/telemetry/CarTelemetryService.java
+++ service/src/com/android/car/telemetry/CarTelemetryService.java
@@ -271,7 +271,7 @@ public class CarTelemetryService extends ICarTelemetryService.Stub implements Ca
             mUidMapper.release();
             mTelemetryThreadTraceLog.traceEnd();
         });
-        mTelemetryThread.quitSafely();
+        CarServiceUtils.runOnLooperSync(mTelemetryThread.getLooper(), () -> {});
     }
 
     @Override
