612ebd8e349688ba9348c8cf4e03f3baf8d17ae7
Changyeon Jo <changyeon@google.com>
2022-09-29 15:18:29 -0700

    Add more EvsHalWrapperImpl test cases
    - EvsHalWrapperImplUnitTest initializes EvsHalWrapperImpl class to use a
      mock EVS HAL implementation.
    - testStartAndStopVideoStream() is added and verifies methods to control
      a video stream.
    - testDeathRecipient() is added and verifies a logic to handle an
      accidental service loss.
    
    Bug: 240620474
    Bug: 243749447
    Test: atest CarServiceUnitTest
    Change-Id: I7c728a9e398bcfccc656c905196a6e0e40f7c6fc

--- tests/carservice_unit_test/src/com/android/car/evs/EvsHalWrapperImplUnitTest.java
+++ tests/carservice_unit_test/src/com/android/car/evs/EvsHalWrapperImplUnitTest.java
@@ -16,13 +16,19 @@
 
 package com.android.car.evs;
 
+import static com.android.dx.mockito.inline.extended.ExtendedMockito.doAnswer;
+import static com.android.dx.mockito.inline.extended.ExtendedMockito.doNothing;
+import static com.android.dx.mockito.inline.extended.ExtendedMockito.verify;
+import static com.google.common.truth.Truth.assertThat;
+
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyObject;
 import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.spy;
-
-import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
 
 import com.android.car.internal.evs.EvsHalWrapper;
 
@@ -35,23 +41,41 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 @RunWith(MockitoJUnitRunner.class)
 public final class EvsHalWrapperImplUnitTest {
 
     private static final String TAG = EvsHalWrapperImplUnitTest.class.getSimpleName();
+    private static final String MOCK_EVS_CAMERA_NAME_BASE = "/dev/mockcamera";
+    private static final String MOCK_EVS_HAL_LIBRARY_NAME = "carservicejni_test";
+    private static final int NUMBER_OF_MOCK_CAMERAS = 3;
+    private static final int MAX_WAIT_TIME_MS = 3000;
+    private static final int NUMBER_EXPECTED_FRAMES = 5;
+    private static final int INVALID_HANDLE = 0;
 
-    private EvsHalWrapperImpl mEvsHalWrapper = null;
-    @Mock EvsHalWrapper.HalEventCallback mEventCallback;
+    @Mock private EvsHalWrapper.HalEventCallback mEventCallback;
+
+    private EvsHalWrapperImpl mEvsHalWrapper;
 
     @Before
     public void setUp() {
-        mEvsHalWrapper = new EvsHalWrapperImpl(mEventCallback);
+        mEvsHalWrapper = EvsHalWrapperImpl.create(mEventCallback, MOCK_EVS_HAL_LIBRARY_NAME);
         assertThat(mEvsHalWrapper.init()).isTrue();
+
+        long handle = mEvsHalWrapper.createServiceHandleForTest();
+        assertThat(handle).isNotEqualTo(INVALID_HANDLE);
+        assertThat(mEvsHalWrapper.setServiceHandle(handle)).isTrue();
+
+        // EvsHalWrapperImpl always succeeds with the mock EVS HAL implementation.
+        assertThat(mEvsHalWrapper.connectToHalServiceIfNecessary()).isTrue();
     }
 
     @After
     public void tearDown() throws Exception {
         if (mEvsHalWrapper != null) {
+            mEvsHalWrapper.disconnectFromHalService();
             mEvsHalWrapper.release();
         }
     }
@@ -62,10 +86,53 @@ public final class EvsHalWrapperImplUnitTest {
     }
 
     @Test
-    public void testConnectToHalServiceIfNecessary() {
-        // connectToHalServiceIfNecessary() returns false because this test is not running as either
-        // one of root, system, or automotive_evs.
-        assertThat(mEvsHalWrapper.connectToHalServiceIfNecessary()).isFalse();
-        mEvsHalWrapper.disconnectFromHalService();
+    public void testOpenAndCloseCamera() {
+        for (int i = 0; i < NUMBER_OF_MOCK_CAMERAS; i++) {
+            String cameraId = MOCK_EVS_CAMERA_NAME_BASE + i;
+            assertThat(mEvsHalWrapper.openCamera(cameraId)).isTrue();
+            mEvsHalWrapper.closeCamera();
+        }
+    }
+
+    @Test
+    public void testStartAndStopVideoStream() throws Exception {
+        for (int i = 0; i < NUMBER_OF_MOCK_CAMERAS; i++) {
+            CountDownLatch frameLatch = new CountDownLatch(NUMBER_EXPECTED_FRAMES);
+            CountDownLatch eventLatch = new CountDownLatch(1);
+            reset(mEventCallback);
+            doAnswer(args -> {
+                    eventLatch.countDown();
+                    return true;
+            }).when(mEventCallback).onHalEvent(anyInt());
+            doAnswer(args -> {
+                    mEvsHalWrapper.doneWithFrame(args.getArgument(0));
+                    frameLatch.countDown();
+                    return true;
+            }).when(mEventCallback).onFrameEvent(anyInt(), anyObject());
+
+            String cameraId = MOCK_EVS_CAMERA_NAME_BASE + i;
+            assertThat(mEvsHalWrapper.openCamera(cameraId)).isTrue();
+            assertThat(mEvsHalWrapper.requestToStartVideoStream()).isTrue();
+
+            assertThat(frameLatch.await(MAX_WAIT_TIME_MS, TimeUnit.MILLISECONDS)).isTrue();
+
+            mEvsHalWrapper.requestToStopVideoStream();
+            assertThat(eventLatch.await(1, TimeUnit.SECONDS)).isTrue();
+
+            mEvsHalWrapper.closeCamera();
+        }
+    }
+
+    @Test
+    public void testDeathRecipient() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        doAnswer(args -> {
+            latch.countDown();
+            return true;
+        }).when(mEventCallback).onHalDeath();
+
+        mEvsHalWrapper.triggerBinderDied();
+
+        assertThat(latch.await(MAX_WAIT_TIME_MS, TimeUnit.MILLISECONDS)).isTrue();
     }
 }
