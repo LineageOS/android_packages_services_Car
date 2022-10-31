2dec8f194d91bbc3f353a29ba9a6d079e8556533
Mayank Garg <gargmayank@google.com>
2022-10-31 01:35:30 -0700

    Exposed OEM service name as test API
    Bug: 226406223
    Test: adb shell dumpsys car_service --oem-service --name-only
    Change-Id: I79025008707e83893e61b8490d2f165810219441
    Merged-In: I79025008707e83893e61b8490d2f165810219441
    (cherry picked from commit 16390f793d0bb45bd0ce7a90f91b28abea478134)

--- service/src/com/android/car/ICarImpl.java
+++ service/src/com/android/car/ICarImpl.java
@@ -744,6 +744,7 @@ public class ICarImpl extends ICar.Stub {
                 dumpAllHals(writer);
                 return;
             }
+
             int length = args.length - 1;
             String[] halNames = new String[length];
             System.arraycopy(args, 1, halNames, 0, length);
@@ -756,7 +757,12 @@ public class ICarImpl extends ICar.Stub {
             dumpDataDir(writer);
             return;
         } else if ("--oem-service".equals(args[0])) {
-            dumpOemService(writer);
+            if (args.length > 1 && args[1].equalsIgnoreCase("--name-only")) {
+                writer.println(getOemServiceName());
+            } else {
+                dumpOemService(writer);
+            }
+            return;
         } else if ("--help".equals(args[0])) {
             showDumpHelp(writer);
         } else {
@@ -766,6 +772,10 @@ public class ICarImpl extends ICar.Stub {
 
     private void dumpOemService(IndentingPrintWriter writer) {
         mCarOemService.dump(writer);
+    }
+
+    public String getOemServiceName() {
+        return mCarOemService.getOemServiceName();
     }
 
     private void dumpAll(IndentingPrintWriter writer) {
