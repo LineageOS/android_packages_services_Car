51cfd67fb913d9c353b832bc355d81e3ff3f9062
Philip Chen <philipchen@google.com>
2022-11-02 17:56:00 -0700

    evs_app: Place config_override.json under /vendor
    To achieve a common system partition across devices, let's move the
    device-specific config_override.json to the vendor partition.
    
    Bug: 246656948
    Test: apply the change to gcar_emu_x86_64 and verify evs_app runs
    
    Change-Id: I034c073a64e66b7ee69a66fe17118b23ccdddffa

--- cpp/evs/apps/default/src/evs_app.cpp
+++ cpp/evs/apps/default/src/evs_app.cpp
@@ -47,7 +47,7 @@ using android::frameworks::automotive::vhal::ISubscriptionClient;
 using android::frameworks::automotive::vhal::IVhalClient;
 
 const char CONFIG_DEFAULT_PATH[] = "/system/etc/automotive/evs/config.json";
-const char CONFIG_OVERRIDE_PATH[] = "/system/etc/automotive/evs/config_override.json";
+const char CONFIG_OVERRIDE_PATH[] = "/vendor/etc/automotive/evs/config_override.json";
 
 std::shared_ptr<IEvsEnumerator> pEvsService;
 std::shared_ptr<IEvsDisplay> pDisplay;
