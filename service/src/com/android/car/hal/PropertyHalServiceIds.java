9914911fd35d43de99a8abe6b2dde78239ef445c
Aaqib Ismail <aaqibismail@google.com>
2022-11-09 11:25:06 -0800

    Add API changes for STEERING_WHEEL_HEIGHT_MOVE
    Bug: 257267721
    Test: atest VtsHalAutomotiveVehicle_TargetTest
          atest CtsCarTestCases:CarPropertyManagerTest
          atest CtsCarTestCases:VehiclePropertyIdsTest
    Change-Id: Iaff7478788580693c689855f7ccead75c39d9ca6

--- service/src/com/android/car/hal/PropertyHalServiceIds.java
+++ service/src/com/android/car/hal/PropertyHalServiceIds.java
@@ -303,6 +303,9 @@ public class PropertyHalServiceIds {
         mHalPropIdToPermissions.put(VehicleProperty.STEERING_WHEEL_HEIGHT_POS, new Pair<>(
                 Car.PERMISSION_CONTROL_STEERING_WHEEL,
                 Car.PERMISSION_CONTROL_STEERING_WHEEL));
+        mHalPropIdToPermissions.put(VehicleProperty.STEERING_WHEEL_HEIGHT_MOVE, new Pair<>(
+                Car.PERMISSION_CONTROL_STEERING_WHEEL,
+                Car.PERMISSION_CONTROL_STEERING_WHEEL));
 
         // HVAC properties
         mHalPropIdToPermissions.put(VehicleProperty.HVAC_FAN_SPEED, new Pair<>(
