dc90f6959c381c08d96ebd7df0aa4dd9de12d5a6
Aaqib Ismail <aaqibismail@google.com>
2022-11-09 14:51:25 -0800

    Add API changes for STEERING_WHEEL_LOCKED
    Bug: 257267840
    Test: atest VtsHalAutomotiveVehicle_TargetTest
          atest CtsCarTestCases:CarPropertyManagerTest
          atest CtsCarTestCases:VehiclePropertyIdsTest
    Change-Id: I4a089990d2a97310a2c17fc803b5ac4c9c0555b8

--- service/src/com/android/car/hal/PropertyHalServiceIds.java
+++ service/src/com/android/car/hal/PropertyHalServiceIds.java
@@ -309,6 +309,9 @@ public class PropertyHalServiceIds {
         mHalPropIdToPermissions.put(VehicleProperty.STEERING_WHEEL_THEFT_LOCK_ENABLED, new Pair<>(
                 Car.PERMISSION_CONTROL_STEERING_WHEEL,
                 Car.PERMISSION_CONTROL_STEERING_WHEEL));
+        mHalPropIdToPermissions.put(VehicleProperty.STEERING_WHEEL_LOCKED, new Pair<>(
+                Car.PERMISSION_CONTROL_STEERING_WHEEL,
+                Car.PERMISSION_CONTROL_STEERING_WHEEL));
 
         // HVAC properties
         mHalPropIdToPermissions.put(VehicleProperty.HVAC_FAN_SPEED, new Pair<>(
