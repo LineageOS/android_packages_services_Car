096ff7508e2d6305e93210dd41530bec283fdcc0
Pascal Mütschard <pmuetschard@google.com>
2022-11-10 13:11:27 +0000

    Revert "Input latency & reliability tracking - hide flow"
    Revert "Input latency & reliability tracking"
    
    Revert "Input latency & reliability tracking"
    
    Revert submission 20313761-IME tracking - hide
    
    Bug: b/258637227
    Reason for revert: Breaks CtsSystemApiSignatureTestCases android.signature.cts.api.system.SignatureTest#testRuntimeCompatibilityWithCurrentApi
    Reverted Changes:
    Icf9ab3fc0:Input latency & reliability tracking - hide flow
    I1096564d5:Input latency & reliability tracking - hide flow
    I14e009250:Input latency & reliability tracking - hide flow
    Ic32e36b2d:Input latency & reliability tracking
    I56f96dc6b:Input latency & reliability tracking
    I73dec2e9e:Input latency & reliability tracking
    
    Change-Id: Id0d8c92dc519dc0848de8e2cbadb8c6ce6d0871f

--- car_product/car_ui_portrait/apps/CarUiPortraitSystemUI/src/com/android/systemui/wm/CarUiPortraitDisplaySystemBarsController.java
+++ car_product/car_ui_portrait/apps/CarUiPortraitSystemUI/src/com/android/systemui/wm/CarUiPortraitDisplaySystemBarsController.java
@@ -211,7 +211,7 @@ public class CarUiPortraitDisplaySystemBarsController extends DisplaySystemBarsC
             mAppRequestedVisibleTypes = mRequestedVisibleTypes;
 
             showInsets(barVisibilities[0], /* fromIme= */ false, /* statsToken= */ null);
-            hideInsets(barVisibilities[1], /* fromIme= */ false, /* statsToken= */ null);
+            hideInsets(barVisibilities[1], /* fromIme= */ false);
 
             boolean immersiveState = mImmersiveOverride || mImmersiveForSUW || (
                     (barVisibilities[1] & (WindowInsets.Type.statusBars()
