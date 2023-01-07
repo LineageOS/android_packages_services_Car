ad315fcf19514e3dfc03796480ae6f9e1a913762
Ethan Lee <ethanalee@google.com>
2023-01-07 02:20:49 +0000

    Fix annotation issues with Codegen
    - Verified that AddedInOrBefore or ApiRequirement annotations are correct after codegen
    - Added appropriate annotations to permit correct codegen generation
    
    Test: m
    Test: Manually tested by generating files
    Fixes: 238929944
    Change-Id: Id5e14e5de896a32d06c3e283c935840d6cd60145

--- car-lib/src/android/car/input/CustomInputEvent.java
+++ car-lib/src/android/car/input/CustomInputEvent.java
@@ -79,6 +79,7 @@ public final class CustomInputEvent implements Parcelable {
     // To regenerate run:
     // $ codegen --to-string $ANDROID_BUILD_TOP/packages/services/Car/car-lib/src/android/car
     // /input/CustomInputEvent.java
+    // Added AddedInOrBefore or ApiRequirement Annotation manually
     //
     // To exclude the generated code from IntelliJ auto-formatting enable (one-time):
     //   Settings > Editor > Code Style > Formatter Control
