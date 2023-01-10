bbc8ea7a744d52a83c96859033cdb6fe095b492d
Paul Duffin <paulduffin@google.com>
2023-01-10 16:56:43 +0000

    Set a Soong config var when the car bcpf is needed
    Bug: 2378892
    Test: lunch aosp_cf_x86_64_auto-userdebug
          m nothing
    Change-Id: I02270b4354c14fb5ad37c7f1e25f28ddc7e8833a

--- car_product/build/car.mk
+++ car_product/build/car.mk
@@ -266,6 +266,8 @@ ifeq ($(USE_CAR_FRAMEWORK_APEX),true)
     PRODUCT_APEX_BOOT_JARS += com.android.car.framework:android.car-module
     PRODUCT_APEX_SYSTEM_SERVER_JARS += com.android.car.framework:car-frameworks-service-module
 
+    $(call soong_config_set,AUTO,car_bootclasspath_fragment,true)
+
     PRODUCT_HIDDENAPI_STUBS := android.car-module.stubs
     PRODUCT_HIDDENAPI_STUBS_SYSTEM := android.car-module.stubs.system
     PRODUCT_HIDDENAPI_STUBS_TEST := android.car-module.stubs.test
