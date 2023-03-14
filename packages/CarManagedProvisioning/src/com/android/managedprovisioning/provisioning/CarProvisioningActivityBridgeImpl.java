affbf2d1734016472cbae4119ac3b353e75c4087
Martynas Petuška <petuska@google.com>
2023-03-14 15:19:31 +0000

    Introduce LazyStringResource wrapper for parameterised strings to avoid eager resolution
    Test: Manual provisioning on device
    Bug: b/272249622
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:283446506caf2cb32e6a7a6ccba32991e4292b23)
    Merged-In: I6ebf529df189e72cd85bee134c4c1644f3674c4b
    Change-Id: I6ebf529df189e72cd85bee134c4c1644f3674c4b

--- packages/CarManagedProvisioning/src/com/android/managedprovisioning/provisioning/CarProvisioningActivityBridgeImpl.java
+++ packages/CarManagedProvisioning/src/com/android/managedprovisioning/provisioning/CarProvisioningActivityBridgeImpl.java
@@ -42,6 +42,7 @@ import com.android.server.utils.Slogf;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
 
 import java.util.function.Supplier;
 
@@ -177,7 +178,7 @@ abstract class CarProvisioningActivityBridgeImpl implements ProvisioningActivity
     private ProvisioningModeWrapper getProvisioningModeWrapperForFullyManaged(Context context) {
         int provisioningSummaryId;
         TransitionScreenWrapper.Builder secondScreenBuilder =
-                new TransitionScreenWrapper.Builder(context)
+                new TransitionScreenWrapper.Builder()
                         .setHeader(R.string.fully_managed_device_provisioning_step_2_header);
 
         if (!getParams().deviceOwnerPermissionGrantOptOut) {
@@ -203,11 +204,12 @@ abstract class CarProvisioningActivityBridgeImpl implements ProvisioningActivity
 
         TransitionScreenWrapper firstScreen = new TransitionScreenWrapper(
                 R.string.fully_managed_device_provisioning_step_1_header,
-                context.getString(R.string.fully_managed_device_provisioning_step_1_description),
-                /* drawable= */ 0, /* shouldLoop= */ false, context);
-        return new ProvisioningModeWrapper(new TransitionScreenWrapper[] {
-                firstScreen, secondScreenBuilder.build()},
-                context.getString(provisioningSummaryId));
+                R.string.fully_managed_device_provisioning_step_1_description,
+                /* drawable= */ 0,
+                /* shouldLoop= */ false);
+        return new ProvisioningModeWrapper(
+                ImmutableList.of(firstScreen, secondScreenBuilder.build()),
+                provisioningSummaryId);
     }
 
     @Override
