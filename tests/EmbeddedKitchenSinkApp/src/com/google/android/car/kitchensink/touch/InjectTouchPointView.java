ef938f120b5e47bc3af76e82f14208c06a72fc61
Antonio Kantek <kanant@google.com>
2023-03-28 14:06:40 -0700

    Remove dups in kitchensink touch point views
    Bug: 275614101
    Test: manual (kitchensink > touch test)
    Change-Id: If5ace1a4e08e586d8f0160728bc754280e2f761c

--- tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/touch/InjectTouchPointView.java
+++ tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/touch/InjectTouchPointView.java
@@ -17,105 +17,22 @@
 package com.google.android.car.kitchensink.touch;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Copied from TouchPointView
  * Show touch points as circles; allow multiple touch points
  * Set LOG_ONLY = false to test on physical device.
  */
-public final class InjectTouchPointView extends View {
-    private static final String TAG = InjectTouchPointView.class.getSimpleName();
+public final class InjectTouchPointView extends AbstractTouchPointView {
     private static final boolean LOG_ONLY = false;
-    private static final int[] COLORS = {
-        Color.RED,
-        Color.GREEN,
-        Color.BLUE,
-        Color.YELLOW,
-        Color.MAGENTA,
-        Color.BLACK,
-        Color.DKGRAY
-    };
-
-    private final List<Finger> mFingers = new ArrayList<Finger>();
-    private final Paint mPaint = new Paint();
 
     public InjectTouchPointView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
     public InjectTouchPointView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        mPaint.setStyle(Paint.Style.FILL);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (LOG_ONLY) {
-            logTouchEvents(event);
-            return true;
-        }
-        mFingers.clear();
-        if (event.getActionMasked() == MotionEvent.ACTION_UP) {
-            invalidate();
-            return true;
-        }
-        for (int i = 0; i < event.getPointerCount(); i++) {
-            int pointerId = event.getPointerId(i);
-            int pointerIndex = event.findPointerIndex(pointerId);
-            Finger finger = new Finger();
-            finger.point =  new Point((int) event.getX(pointerIndex),
-                    (int) event.getY(pointerIndex));
-            finger.pointerId = pointerId;
-
-            mFingers.add(finger);
-        }
-        invalidate();
-        return true;
-    }
-
-    private void logTouchEvents(MotionEvent event) {
-        if (event.getActionMasked() != MotionEvent.ACTION_UP) {
-            return;
-        }
-
-        for (int i = 0; i < event.getPointerCount(); i++) {
-            int pointerId = event.getPointerId(i);
-            int pointerIndex = event.findPointerIndex(pointerId);
-            long downTime = event.getDownTime();
-            long eventTime = event.getEventTime();
-            Log.d(TAG, "TouchUp [x=" + event.getX(pointerIndex) + ", y=" + event.getY(pointerIndex)
-                    + " , pointerId=" + pointerId + ", pointerIndex=" + pointerIndex + ", duration="
-                    + (eventTime - downTime) + "]");
-        }
-    }
-
-    @Override
-    public void onDraw(Canvas canvas) {
-        if (LOG_ONLY) {
-            return;
-        }
-        int radius = canvas.getWidth() / 100;
-        for (Finger finger: mFingers) {
-            Point point = finger.point;
-            int color = COLORS[finger.pointerId % COLORS.length];
-            mPaint.setColor(color);
-            canvas.drawCircle(point.x, point.y, radius, mPaint);
-        }
-    }
-
-    private static final class Finger {
-        public Point point;
-        public int pointerId;
+        super(context, attrs, defStyle, LOG_ONLY);
+        setRadius(getWidth() / 100);
     }
 }
