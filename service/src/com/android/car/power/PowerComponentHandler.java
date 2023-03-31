379938d58bc73253be943f0a2877eb9dc9ed1b17
Eric Jeong <ericjeong@google.com>
2023-03-31 15:54:52 -0700

    Add version guard around AppOpsManagerHelper.setTurnScreenOnAllowed
    Bug: 275100205
    Test: build okay
    Change-Id: Ifc2a6b809d9c205df98de2d4a9e0d49ab24c7c76

--- service/src/com/android/car/power/PowerComponentHandler.java
+++ service/src/com/android/car/power/PowerComponentHandler.java
@@ -27,6 +27,7 @@ import static android.car.hardware.power.PowerComponentUtil.powerComponentToStri
 import static android.car.hardware.power.PowerComponentUtil.toPowerComponent;
 
 import static com.android.car.internal.ExcludeFromCodeCoverageGeneratedReport.DUMP_INFO;
+import static com.android.car.internal.util.VersionUtils.isPlatformVersionAtLeastU;
 
 import android.annotation.Nullable;
 import android.bluetooth.BluetoothAdapter;
@@ -105,8 +106,12 @@ public final class PowerComponentHandler {
     }
 
     void init() {
-        AppOpsManagerHelper.setTurnScreenOnAllowed(mContext, Process.myUid(),
-                mContext.getOpPackageName(), /* isAllowed= */ true);
+        if (isPlatformVersionAtLeastU()) {
+            // Before Android U, this permission is not needed.
+            // And, AppOpsManagerHelper.setTurnScreenOnAllowed is added in UDC.
+            AppOpsManagerHelper.setTurnScreenOnAllowed(mContext, Process.myUid(),
+                    mContext.getOpPackageName(), /* isAllowed= */ true);
+        }
         PowerComponentMediatorFactory factory = new PowerComponentMediatorFactory();
         synchronized (mLock) {
             readUserOffComponentsLocked();
