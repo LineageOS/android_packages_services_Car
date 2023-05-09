c6db57f979146475590bcf27a9894bba468a825e
Abhijoy Saha <abhijoy@google.com>
2023-05-09 15:28:41 -0700

    Fix notification system bar button highlight
    Fixes: 281700630
    Test: manual
    Change-Id: Ie241a577ecd4762c24b8695d4137dfb72ba277be

--- car_product/car_ui_portrait/apps/CarUiPortraitLauncher/src/com/android/car/portraitlauncher/homeactivities/CarUiPortraitHomeScreen.java
+++ car_product/car_ui_portrait/apps/CarUiPortraitLauncher/src/com/android/car/portraitlauncher/homeactivities/CarUiPortraitHomeScreen.java
@@ -160,6 +160,7 @@ public final class CarUiPortraitHomeScreen extends FragmentActivity {
     private int mNavBarHeight;
     private boolean mIsSUWInProgress;
     private TaskCategoryManager mTaskCategoryManager;
+    private boolean mIsNotificationCenterOnTop;
     private boolean mIsRecentsOnTop;
     private TaskInfoCache mTaskInfoCache;
     private TaskViewPanel mAppGridTaskViewPanel;
@@ -260,6 +261,7 @@ public final class CarUiPortraitHomeScreen extends FragmentActivity {
                 return;
             }
 
+            mIsNotificationCenterOnTop = mTaskCategoryManager.isNotificationActivity(taskInfo);
             mIsRecentsOnTop = mTaskCategoryManager.isRecentsActivity(taskInfo);
             // Close the panel if the top application is a blank activity.
             // This is to prevent showing a blank panel to the user if an app crashes and reveals
@@ -396,11 +398,6 @@ public final class CarUiPortraitHomeScreen extends FragmentActivity {
         if (DBG) {
             Log.d(TAG, message);
         }
-    }
-
-    private boolean isNotificationCenterOnTop() {
-        if (mRootTaskViewPanel.getCurrentTask() == null) return false;
-        return mTaskCategoryManager.isNotificationActivity(mRootTaskViewPanel.getCurrentTask());
     }
 
     private final View.OnLayoutChangeListener mControlBarOnLayoutChangeListener =
@@ -547,7 +544,7 @@ public final class CarUiPortraitHomeScreen extends FragmentActivity {
     }
 
     private void collapseNotificationPanel() {
-        if (isNotificationCenterOnTop()) {
+        if (mIsNotificationCenterOnTop) {
             mRootTaskViewPanel.closePanel();
         }
     }
@@ -879,7 +876,7 @@ public final class CarUiPortraitHomeScreen extends FragmentActivity {
                 }
 
                 // Update the notification button's selection state.
-                if (isNotificationCenterOnTop() && isVisible) {
+                if (mIsNotificationCenterOnTop && isVisible) {
                     notifySystemUI(MSG_NOTIFICATIONS_VISIBILITY_CHANGE, boolToInt(true));
                 } else {
                     notifySystemUI(MSG_NOTIFICATIONS_VISIBILITY_CHANGE, boolToInt(false));
