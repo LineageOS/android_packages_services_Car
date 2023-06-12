67204f3b3032684612a9de2c848fc29d15c08250
Ethan Lee <ethanalee@google.com>
2023-06-12 19:13:07 +0000

    Add default switch statement to CarWatchdog related classes
    Added in CarWatchdogService, OveruseConfigurationCache, WatchdogPerfHandler, CarWatchdogTestFragment.
    
    Test: atest
    Fixes: 261106236
    Change-Id: I4e6487ad39f1060f0fef298a310a7da19f90d3f8

--- service/src/com/android/car/watchdog/CarWatchdogService.java
+++ service/src/com/android/car/watchdog/CarWatchdogService.java
@@ -203,6 +203,8 @@ public final class CarWatchdogService extends ICarWatchdogService.Stub implement
                     mWatchdogPerfHandler.processPackageChangedIntent(intent);
                     break;
                 }
+                default:
+                    Slogf.i(TAG, "Ignoring unknown intent %s", intent);
             }
         }
     };
@@ -820,6 +822,8 @@ public final class CarWatchdogService extends ICarWatchdogService.Stub implement
             // ON covers resume.
             case CarPowerManager.STATE_ON:
                 return PowerCycle.POWER_CYCLE_RESUME;
+            default:
+                Slogf.e(TAG, "Invalid power state: %d", powerState);
         }
         return -1;
     }
@@ -830,6 +834,8 @@ public final class CarWatchdogService extends ICarWatchdogService.Stub implement
                 return "GARAGE_MODE_OFF";
             case GarageMode.GARAGE_MODE_ON:
                 return "GARAGE_MODE_ON";
+            default:
+                Slogf.e(TAG, "Invalid garage mode: %d", garageMode);
         }
         return "INVALID";
     }
