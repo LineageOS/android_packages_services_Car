a98bce4e537c3addb25d23cdec7afb4052972ad7
Abhijoy Saha <abhijoy@google.com>
2023-06-12 13:43:42 -0700

    Remove port qualifier from car portrait apps common launcher rro
    Bug: 285943253
    Test: manual
    Change-Id: Ia1c730d1f78eda3c9c991a48ddd1a49aa5c7ae96

--- car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk
+++ car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk
@@ -52,7 +52,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     car.ui.config=rickybobby
 
 PORTRAIT_RRO_PACKAGES := com.android.car.calendar.googlecaruiportrait.rro; \
-    com.android.car.carlauncher.apps.caruiportrait.rro; \
     com.android.car.carlauncher.googlecaruiportrait.rro; \
     com.android.car.carlauncher.media.caruiportrait.rro; \
     com.android.car.caruiportrait.rro; \
