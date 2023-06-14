6a3fc73d19ecf20ee064fe9e6ab02de3de15debb
Yu Shan <shanyu@google.com>
2023-06-13 17:58:31 -0700

    Deprecate VEHICLE_MAP_SERVICE.
    It is not supposed to be exposed through CarPropMgr.
    
    Test: android.car-stubs-docs-update-current-api \
    android.car-module.stubs.source-update-current-api \
    android.car-test-stubs-docs-update-current-api \
    android.car-system-stubs-docs-update-current-api \
    android.car-module.stubs.source.system-update-current-api
    Bug: 286303637
    
    Change-Id: Ib7e5bfafc5bae124c8dbc534c0b9f12cbc1309a8
    Merged-In: Ib7e5bfafc5bae124c8dbc534c0b9f12cbc1309a8
    (cherry picked from commit 96d0839f9ea496c4caadf9103311ba7daf78aa54)

--- car-lib/src/android/car/VehiclePropertyIds.java
+++ car-lib/src/android/car/VehiclePropertyIds.java
@@ -4251,20 +4251,11 @@ public final class VehiclePropertyIds {
     /**
      * Vehicle Maps Service (VMS) message.
      *
-     * <p>Property Config:
-     * <ul>
-     *  <li>{@link android.car.hardware.CarPropertyConfig#VEHICLE_PROPERTY_ACCESS_READ_WRITE}
-     *  <li>{@link VehicleAreaType#VEHICLE_AREA_TYPE_GLOBAL}
-     *  <li>{@link android.car.hardware.CarPropertyConfig#VEHICLE_PROPERTY_CHANGE_MODE_ONCHANGE}
-     *  <li>{@code Object[]} property type
-     * </ul>
+     * <p>Not exposed through {@link android.car.hardware.property.CarPropertyManager}.
      *
-     * <p>Required Permission:
-     * <ul>
-     *  <li>Signature|Privileged permission "android.car.permission.VMS_PUBLISHER" or
-     *  Signature|Privileged permission "android.car.permission.VMS_SUBSCRIBER" to read and write
-     *  property.
-     * </ul>
+     * <p>Trying to get/set this property will cause {@link SecurityException}.
+     *
+     * Deprecated, use {@link android.car.vms.VmsClientManager} instead.
      */
     @RequiresPermission(anyOf = {Car.PERMISSION_VMS_PUBLISHER, Car.PERMISSION_VMS_SUBSCRIBER})
     @AddedInOrBefore(majorVersion = 33)
