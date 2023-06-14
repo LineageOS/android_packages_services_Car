3c06ebddcc88dd874f55c4e154a233539284574f
Yu Shan <shanyu@google.com>
2023-06-13 17:46:16 -0700

    Deprecate HW_KEY_INPUT from CarPropMgr.
    It is not supposed to be expoesed from CarPropMgr.
    
    Test: m android.car-stubs-docs-update-current-api \
    android.car-module.stubs.source-update-current-api \
    android.car-test-stubs-docs-update-current-api \
    android.car-system-stubs-docs-update-current-api \
    android.car-module.stubs.source.system-update-current-api
    Bug: 286303921
    
    Change-Id: I13d4ed1c3e5c154fa9d3b55f944474ab9fa81f11

--- car-lib/src/android/car/VehiclePropertyIds.java
+++ car-lib/src/android/car/VehiclePropertyIds.java
@@ -2117,14 +2117,13 @@ public final class VehiclePropertyIds {
     /**
      * Property to feed H/W input events to android.
      *
-     * <p>Property Config:
-     * <ul>
-     *  <li>{@link android.car.hardware.CarPropertyConfig#VEHICLE_PROPERTY_ACCESS_READ}
-     *  <li>{@link VehicleAreaType#VEHICLE_AREA_TYPE_GLOBAL}
-     *  <li>{@link android.car.hardware.CarPropertyConfig#VEHICLE_PROPERTY_CHANGE_MODE_ONCHANGE}
-     *  <li>{@code Integer[]} property type
-     * </ul>
+     * <p>Not exposed through {@link android.car.hardware.property.CarPropertyManager}.
+     *
+     * <p>Trying to get/set this property will cause {@link SecurityException}.
+     *
+     * @deprecated use {@link android.car.input.CarInputManager} instead.
      */
+    @Deprecated
     @AddedInOrBefore(majorVersion = 33)
     public static final int HW_KEY_INPUT = 289475088;
     /**
