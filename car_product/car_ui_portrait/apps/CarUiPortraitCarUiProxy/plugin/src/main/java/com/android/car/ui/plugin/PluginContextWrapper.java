678384caa5f02d1d5494ff50478455e537efef7a
Mounika Nekkalapudi <snekkalapudi@google.com>
2023-06-15 14:13:14 -0700

    Proxy plugin changes
      * Return Activity WindowManager from plugin context
      * Return layout inflater with plugin context from wrapper
      * Return application package name from plugin context
    
    Test: manual
    Bug: 287474459
    Change-Id: I3607b965391a03ab47d08f4729e3c86951893471

--- car_product/car_ui_portrait/apps/CarUiPortraitCarUiProxy/plugin/src/main/java/com/android/car/ui/plugin/PluginContextWrapper.java
+++ car_product/car_ui_portrait/apps/CarUiPortraitCarUiProxy/plugin/src/main/java/com/android/car/ui/plugin/PluginContextWrapper.java
@@ -18,18 +18,73 @@ package com.android.car.ui.plugin;
 
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 /**
  * A wrapper class around the plugin context so it can provide {@link android.app.Application}
  * context.
  */
 public class PluginContextWrapper extends ContextWrapper {
-    public PluginContextWrapper(Context base) {
-        super(base);
+
+    @NonNull
+    private final String mApplicationPackageName;
+    @Nullable
+    private WindowManager mWindowManager;
+    @Nullable
+    private LayoutInflater mLayoutInflater = null;
+
+    public PluginContextWrapper(@NonNull Context pluginContext,
+            @NonNull String applicationPackageName) {
+        super(pluginContext);
+        mApplicationPackageName = applicationPackageName;
     }
 
+    /**
+     * Return this plugin context as the application context so that it doesn't return null when
+     * called in the static implementation, for example, {@code MenuItem}
+     */
     @Override
     public Context getApplicationContext() {
         return this;
+    }
+
+    /**
+     * Return the application package name instead of the plugin package name because
+     * {@code SearchResultsProvider} in static implementation needs application id for authority
+     */
+    @Override
+    public String getPackageName() {
+        return mApplicationPackageName;
+    }
+
+    /**
+     * Sets the {@link WindowManager} instance for the plugin {@link Context}.
+     * This was specifically needed for launching {@code Dialog}s.
+     * @param windowManager needs to be attached to the main Activity
+     */
+    public void setWindowManager(@Nullable WindowManager windowManager) {
+        mWindowManager = windowManager;
+    }
+
+    @Override
+    public Object getSystemService(@NonNull String name) {
+        if (WINDOW_SERVICE.equals(name) && mWindowManager != null) {
+            return mWindowManager;
+        }
+        // Return a layout inflater cloned with this context so that views inflated with a
+        // LayoutInflater.from call will return a PluginContextWrapper which is needed to override
+        // other methods in Context, e.g., getPackageName
+        if (LAYOUT_INFLATER_SERVICE.equals(name)) {
+            if (mLayoutInflater == null) {
+                mLayoutInflater = ((LayoutInflater) super.getSystemService(LAYOUT_INFLATER_SERVICE))
+                        .cloneInContext(this);
+            }
+            return mLayoutInflater;
+        }
+        return super.getSystemService(name);
     }
 }
