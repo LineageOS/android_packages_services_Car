c9810295e6e885aaaf2c2ab82905c19e6e3b0c39
Philip Chen <philipchen@google.com>
2023-06-27 13:56:48 -0700

    Clean up the conditions for installing evs_app
    Bug: 288970525
    Test: build
    Change-Id: If2d07d939171c811736758e7a0bb9ffa475a1c7f
    Merged-In: If2d07d939171c811736758e7a0bb9ffa475a1c7f

--- car_product/build/car_generic_system.mk
+++ car_product/build/car_generic_system.mk
@@ -78,15 +78,6 @@ PRODUCT_COPY_FILES += \
     packages/services/Car/cpp/evs/manager/aidl/init.evs.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.evs.rc
 endif
 
-ifeq ($(ENABLE_EVS_SAMPLE), true)
-# ENABLE_EVS_SAMPLE should set be true or their vendor specific equivalents should be included in
-# the device.mk with the corresponding selinux policies
-PRODUCT_PACKAGES += evs_app \
-                    android.hardware.automotive.evs-default \
-                    cardisplayproxyd
-include packages/services/Car/cpp/evs/apps/sepolicy/evsapp.mk
-endif  # ENABLE_EVS_SAMPLE
-
 ifeq ($(ENABLE_CAREVSSERVICE_SAMPLE), true)
 PRODUCT_PACKAGES += CarEvsCameraPreviewApp
 endif
@@ -103,16 +94,16 @@ ifeq ($(ENABLE_CARTELEMETRY_SERVICE), true)
 PRODUCT_PACKAGES += android.automotive.telemetryd@1.0
 endif
 
-ifeq ($(ENABLE_EVS_SAMPLE), true)
-# ENABLE_EVS_SAMPLE should set be true or their vendor specific equivalents should be included in
+# ENABLE_EVS_SAMPLE should be set true or their vendor specific equivalents should be included in
 # the device.mk with the corresponding selinux policies
+ifeq ($(ENABLE_EVS_SAMPLE), true)
 PRODUCT_PACKAGES += evs_app
-include packages/services/Car/cpp/evs/apps/sepolicy/evsapp.mk
-
 # A reference EVS HAL implementation will be added in car_vendor.mk and require AIDL version of
 # the automotive display service implementation.
 USE_AIDL_DISPLAY_SERVICE := true
-endif  # ENABLE_EVS_SAMPLE
+else ifeq ($(ENABLE_SAMPLE_EVS_APP), true)
+PRODUCT_PACKAGES += evs_app
+endif
 
 ifeq ($(USE_AIDL_DISPLAY_SERVICE), true)
 PRODUCT_PACKAGES += cardisplayproxyd
