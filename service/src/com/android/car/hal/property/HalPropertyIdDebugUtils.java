41166b5301828272861a19279a768217e0e403c1
Tyler Trephan <tylertrephan@google.com>
2023-10-12 04:24:59 +0000

    Move HalPropertyIdDebugUtils in property directory
    Test: None
    Bug: 290972334
    Change-Id: I16d822e53a4d0188ecef7b7d25ef0a70ab4a0671

--- service/src/com/android/car/hal/property/HalPropertyIdDebugUtils.java
+++ service/src/com/android/car/hal/property/HalPropertyIdDebugUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.car.hal;
+package com.android.car.hal.property;
 
 import android.annotation.Nullable;
 import android.hardware.automotive.vehicle.VehicleProperty;
