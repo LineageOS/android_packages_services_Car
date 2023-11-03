2106797ec6a3c40afaf676de6f129b43973bc343
Calvin Huang <calhuang@google.com>
2023-11-02 18:29:16 -0700

    Add RROs to update systemUI theme
    Define color_tonal_palettes with orange and pink as seed colors.
    Create dynamic rros based on new color_tonal_palettes.
    
    Dynamic rros are located in
    1. androidRRO for device defaults
    2. car-ui-customizations to change theme color used in car-ui-lib
    3. AppsCommon for apps that use color palettes directly
    
    Fix: 305745266
    Test: manually enable rros
    Change-Id: Ic2ab1bfd61c62d941941e34df307f623618985eb

---
+++ car_product/rro/ThemeSamples/ThemeOrange/product.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2023 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Include generated RROs with theme orange.
+PRODUCT_PACKAGES += \
+    googlecarui.theme.orange-com-android-car-ui-paintbooth \
+    googlecarui.theme.orange-com-android-car-rotaryplayground \
+    googlecarui.theme.orange-com-android-car-themeplayground \
+    googlecarui.theme.orange-com-android-car-carlauncher \
+    googlecarui.theme.orange-com-android-car-home \
+    googlecarui.theme.orange-com-android-car-media \
+    googlecarui.theme.orange-com.android.car.notification \
+    googlecarui.theme.orange-com-android-car-radio \
+    googlecarui.theme.orange-com-android-car-calendar \
+    googlecarui.theme.orange-com-android-car-messenger \
+    googlecarui.theme.orange-com-android-car-systemupdater \
+    googlecarui.theme.orange-com-android-car-dialer \
+    googlecarui.theme.orange-com-android-car-linkviewer \
+    googlecarui.theme.orange-com-android-car-settings \
+    googlecarui.theme.orange-com-android-car-developeroptions \
+    googlecarui.theme.orange-com-android-managedprovisioning \
+    googlecarui.theme.orange-com-android-settings-intelligence \
+    googlecarui.theme.orange-com-android-htmlviewer \
+    googlecarui.theme.orange-com-android-systemui \
+    googlecarui.theme.orange-android \
+
