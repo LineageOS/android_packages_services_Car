c2923b8dd468ae8a4fe7892d781f7980da2cab37
Bruno Martins <bgcngm@gmail.com>
2024-01-20 01:51:34 +0000

    car_product: Only include test packages on eng builds
    Change-Id: I440b939d0ff30f06ea27d9ec23d73b2646326258

--- car_product/build/car.mk
+++ car_product/build/car.mk
@@ -38,7 +38,7 @@ PRODUCT_PACKAGES += \
 
 ifneq ($(PRODUCT_IS_AUTOMOTIVE_SDK),true)
 # This is for testing
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifeq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_PACKAGES += \
     DefaultStorageMonitoringCompanionApp \
     EmbeddedKitchenSinkApp \
