9ca7bd924246e24f605aa3336154b9da46b4d674
Ashok Mutyala <quic_amutyala@quicinc.com>
2024-03-26 17:22:40 +0000

    Correct Shutdown Behaviour in Suspend sequence
    Shutdown time which set by 3 minutes in AOSP when suspend keeps failing not working due to not consider roundtripping
    
    Bug: 331518416
    Test: Do not detach USB && adb shell cmd car_service suspend && shutdown in 3 minutes
    
    Change-Id: I1e89512adf63907009262062dc1351cac8980e10

--- service/src/com/android/car/power/CarPowerManagementService.java
+++ service/src/com/android/car/power/CarPowerManagementService.java
@@ -2772,6 +2772,7 @@ public class CarPowerManagementService extends ICarPower.Stub implements
         long retryIntervalMs = INITIAL_SUSPEND_RETRY_INTERVAL_MS;
         long totalWaitDurationMs = 0;
         while (true) {
+            long suspendStartTime = SystemClock.elapsedRealtime();
             Slogf.i(TAG, "Entering %s", suspendTarget);
             if (isSuspendToDisk) {
                 freeMemory();
@@ -2797,7 +2798,7 @@ public class CarPowerManagementService extends ICarPower.Stub implements
                         return false;
                     }
                 }
-
+                long suspendStopTime = SystemClock.elapsedRealtime();
                 Slogf.w(TAG, "Failed to Suspend; will retry after %dms", retryIntervalMs);
                 try {
                     mLock.wait(retryIntervalMs);
@@ -2805,6 +2806,7 @@ public class CarPowerManagementService extends ICarPower.Stub implements
                     Thread.currentThread().interrupt();
                 }
                 totalWaitDurationMs += retryIntervalMs;
+                totalWaitDurationMs += (suspendStopTime - suspendStartTime);
                 retryIntervalMs = Math.min(retryIntervalMs * 2, MAX_RETRY_INTERVAL_MS);
             }
         }
