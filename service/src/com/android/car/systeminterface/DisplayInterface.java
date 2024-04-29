437f798888620fc411c5aa9dfcd0c52b8da514ca
Eric Jeong <ericjeong@google.com>
2024-04-29 11:18:38 -0700

    Print informational log around brightness change
    Flag: EXEMPT Log modification only
    Bug: 337068708
    Test: build okay
    Change-Id: I0fff88b4054bdd150b72c66e9e96440fc4a8de46

--- service/src/com/android/car/systeminterface/DisplayInterface.java
+++ service/src/com/android/car/systeminterface/DisplayInterface.java
@@ -173,6 +173,7 @@ public interface DisplayInterface {
         private final DisplayManager.DisplayListener mDisplayListener = new DisplayListener() {
             @Override
             public void onDisplayAdded(int displayId) {
+                Slogf.i(TAG, "onDisplayAdded: displayId=%d", displayId);
                 synchronized (mLock) {
                     mDisplayStateSet.put(displayId, isDisplayOn(displayId));
                     mDisplayBrightnessSet.put(displayId, INVALID_DISPLAY_BRIGHTNESS);
@@ -181,6 +182,7 @@ public interface DisplayInterface {
 
             @Override
             public void onDisplayRemoved(int displayId) {
+                Slogf.i(TAG, "onDisplayRemoved: displayId=%d", displayId);
                 synchronized (mLock) {
                     mDisplayStateSet.delete(displayId);
                     mDisplayBrightnessSet.delete(displayId);
@@ -189,6 +191,7 @@ public interface DisplayInterface {
 
             @Override
             public void onDisplayChanged(int displayId) {
+                Slogf.i(TAG, "onDisplayChanged: displayId=%d", displayId);
                 handleDisplayChanged(displayId);
             }
         };
@@ -333,6 +336,7 @@ public interface DisplayInterface {
 
         @Override
         public void startDisplayStateMonitoring() {
+            Slogf.i(TAG, "Starting to monitor display state change");
             CarPowerManagementService carPowerManagementService;
             CarUserService carUserService;
             synchronized (mLock) {
