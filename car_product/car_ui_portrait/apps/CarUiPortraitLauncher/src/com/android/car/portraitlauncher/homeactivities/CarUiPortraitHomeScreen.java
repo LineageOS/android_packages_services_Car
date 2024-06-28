f4f59af967fee9f985161f7744bc88d46c200dc1
Calvin Huang <calhuang@google.com>
2024-06-28 22:19:34 +0000

    Rest TaskId tracking for AppGrid on task removal
    Test: manual
    Bug: 349669988
    Flag: EXEMPT bugfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:7d0d1739f78e695535f7d836bd8d0c6b61c2c4b7)
    Merged-In: I4e34d1f8fe4bc8442c574ecdcec5b901bb98df12
    Change-Id: I4e34d1f8fe4bc8442c574ecdcec5b901bb98df12

--- car_product/car_ui_portrait/apps/CarUiPortraitLauncher/src/com/android/car/portraitlauncher/homeactivities/CarUiPortraitHomeScreen.java
+++ car_product/car_ui_portrait/apps/CarUiPortraitLauncher/src/com/android/car/portraitlauncher/homeactivities/CarUiPortraitHomeScreen.java
@@ -160,6 +160,7 @@ public final class CarUiPortraitHomeScreen extends FragmentActivity {
             "SAVED_BACKGROUND_APP_COMPONENT_NAME";
     private static final IActivityTaskManager sActivityTaskManager =
             ActivityTaskManager.getService();
+    private static final int INVALID_TASK_ID = -1;
     private final UserEventReceiver mUserEventReceiver = new UserEventReceiver();
     private final Configuration mConfiguration = new Configuration();
 
@@ -183,7 +184,7 @@ public final class CarUiPortraitHomeScreen extends FragmentActivity {
     private TaskInfoCache mTaskInfoCache;
     private TaskViewPanel mRootTaskViewPanel;
     private boolean mSkipAppGridOnRestartAttempt;
-    private int mAppGridTaskId;
+    private int mAppGridTaskId = INVALID_TASK_ID;
     private final IntentHandler mMediaIntentHandler = new IntentHandler() {
         @Override
         public void handleIntent(Intent intent) {
@@ -280,6 +281,7 @@ public final class CarUiPortraitHomeScreen extends FragmentActivity {
             logIfDebuggable("onTaskRemoved taskId=" + taskId);
             if (mAppGridTaskId == taskId) {
                 Log.e(TAG, "onTaskRemoved, App Grid task is removed.");
+                mAppGridTaskId = INVALID_TASK_ID;
             }
         }
 
