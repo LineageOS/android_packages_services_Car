3efb7e5c66ee9d949fe9ef8481e44b967b9e3cfd
Eva Chen <evaschen@google.com>
2024-12-10 06:13:58 +0000

    Revert^2 "Add PERMISSION_CAR_INFO for VEHICLE_CURB_WEIGHT."
    This reverts commit b9cb9e16113b287e1258ffb2bca9749ba301adbe.
    
    Reason for revert: This API change is needed. Additional changes have been added to address failures.
    
    Flag: android.car.feature.vehicle_property_25q2_3p_permissions
    Bug: 357910697
    Test: atest CarPropertyManagerTest
    Change-Id: Ieeec2dabe424806bda4ae7bbddfb3dbbb94e39c8

--- car-lib/src/android/car/VehiclePropertyIds.java
+++ car-lib/src/android/car/VehiclePropertyIds.java
@@ -6206,12 +6206,14 @@ public final class VehiclePropertyIds {
      *
      * <p>Required Permission:
      * <ul>
-     *  <li>Signature|Privileged permission {@link Car#PERMISSION_PRIVILEGED_CAR_INFO} to read
-     *  property.
+     *  <li>Normal permission {@link Car#PERMISSION_CAR_INFO} or Signature|Privileged permission
+     *  {@link Car#PERMISSION_PRIVILEGED_CAR_INFO} to read property.
      *  <li>Property is not writable.
      * </ul>
      */
-    @RequiresPermission.Read(@RequiresPermission(Car.PERMISSION_PRIVILEGED_CAR_INFO))
+    @FlaggedApi(FLAG_VEHICLE_PROPERTY_25Q2_3P_PERMISSIONS)
+    @RequiresPermission.Read(@RequiresPermission(anyOf = {Car.PERMISSION_CAR_INFO,
+            Car.PERMISSION_PRIVILEGED_CAR_INFO}))
     public static final int VEHICLE_CURB_WEIGHT = 289410886;
 
      /**
