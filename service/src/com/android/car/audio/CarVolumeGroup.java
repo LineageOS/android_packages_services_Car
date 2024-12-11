42869e248b4d58902f0c21df33965234cef64b25
Weilin Xu <xuweilin@google.com>
2024-12-11 13:01:08 -0800

    Add activation volume event log in car volume group
    Bug: 379571821
    Flag: EXEMPT log only update
    Test: adb shell dumpsys car_service --services CarAudioService
    Change-Id: I1425f5d4d18d62a6440e7e4cf30063ad26aed849

--- service/src/com/android/car/audio/CarVolumeGroup.java
+++ service/src/com/android/car/audio/CarVolumeGroup.java
@@ -66,6 +66,7 @@ import com.android.car.audio.hal.HalAudioDeviceInfo;
 import com.android.car.internal.ExcludeFromCodeCoverageGeneratedReport;
 import com.android.car.internal.util.DebugUtils;
 import com.android.car.internal.util.IndentingPrintWriter;
+import com.android.car.internal.util.LocalLog;
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.Preconditions;
@@ -96,6 +97,8 @@ import java.util.Set;
  */
 /* package */ abstract class CarVolumeGroup {
     public static final int UNINITIALIZED = -1;
+    private static final int EVENT_LOGGER_QUEUE_SIZE = 50;
+    private static final String TAG = CarLog.tagFor(CarVolumeGroup.class);
 
     private final boolean mUseCarVolumeGroupMute;
     private final boolean mHasCriticalAudioContexts;
@@ -171,6 +174,11 @@ import java.util.Set;
      * com.android.car.audio.hal.HalAudioGainCallback#onAudioDeviceGainsChanged}.
      */
     protected List<Integer> mReasons = new ArrayList<>();
+
+    /**
+     * Event logger for volume group changes such as activation volume invocation
+     */
+    protected final LocalLog mEventLogger = new LocalLog(EVENT_LOGGER_QUEUE_SIZE);
 
     protected CarVolumeGroup(CarAudioContext carAudioContext, CarAudioSettings settingsManager,
             SparseArray<CarAudioDeviceInfo> contextToDevices, int zoneId, int configId,
@@ -577,8 +585,19 @@ import java.util.Set;
             }
             mCurrentGainIndex = activationVolume;
             setCurrentGainIndexLocked(mCurrentGainIndex);
+            if (invokeVolumeGainIndexChanged) {
+                String activationVolumeMsg = "Change gain index " + curGainIndex + " to "
+                        + mCurrentGainIndex + " due to min/max activation type "
+                        + activationVolumeInvocationType;
+                logEvent(activationVolumeMsg);
+            }
         }
         return invokeVolumeGainIndexChanged;
+    }
+
+    protected void logEvent(String message) {
+        mEventLogger.log(message);
+        Slogf.d(TAG, message);
     }
 
     boolean hasCriticalAudioContexts() {
@@ -649,6 +668,10 @@ import java.util.Set;
                     isAttenuatedLocked(),
                     (isAttenuatedLocked() ? " (at: " + mAttenuatedGainIndex + ")" : ""));
             writer.printf("Muted by HAL: %b\n", isHalMutedLocked());
+            writer.decreaseIndent();
+            writer.println("Events:");
+            writer.increaseIndent();
+            mEventLogger.dump(writer);
             writer.decreaseIndent();
             // Empty line for comfortable reading
             writer.println();
