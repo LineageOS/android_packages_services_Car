1ac1940cf17069a482a28ec335dba3e7a9e62aba
Maxim Pleshivenkov <mpleshivenkov@google.com>
2024-12-13 10:55:07 -0800

    Fix the shutdown race condition in the CarPowerManager service.
    This change fixes a race condition in the PowerManager service
    where a second shutdown (with immediate flag) will not result in canceling
    ongoing garage mode.
    
    This commit fixes this issue by ensuring that a second shutdown
    request will cancel listeners (if applicable).
    
    Flag: EXEMPT minor bugfix
    Bug: 339665663
    Change-Id: Ia15df4dc1d741d15df996a14a3aaffa8ec7d70b2
    Test: atest com.android.car.power.CarPowerManagementServiceUnitTest

--- tests/carservice_unit_test/src/com/android/car/power/CarPowerManagementServiceUnitTest.java
+++ tests/carservice_unit_test/src/com/android/car/power/CarPowerManagementServiceUnitTest.java
@@ -2653,6 +2653,56 @@ public final class CarPowerManagementServiceUnitTest extends AbstractExtendedMoc
         assertPowerPolicyNotApplied(POWER_POLICY_AUDIO_INVERT, listenerToWait);
     }
 
+    @Test
+    public void testImmediateShutdownPrepareAfterNonImmediateShutdownPrepare() throws Exception {
+        grantAdjustShutdownProcessPermission();
+        mPowerSignalListener.addEventListener(PowerHalService.SET_ON);
+        mPowerSignalListener.addEventListener(PowerHalService.SET_SHUTDOWN_START);
+        mPowerSignalListener.addEventListener(PowerHalService.SET_SHUTDOWN_PREPARE);
+
+        CountDownLatch shutdownPrepareReceived = new CountDownLatch(1);
+        ICarPowerStateListener listener = new ICarPowerStateListener.Stub() {
+            @Override
+            public void onStateChanged(int state, long expirationTimeMs) {
+                if (state == CarPowerManager.STATE_SHUTDOWN_PREPARE) {
+                    // Start new thread that will signal to test that event is received
+                    // This listener never completes intentionally
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            super.run();
+                            // signal to the test that second SHUTDOWN_PREPARE can be sent
+                            shutdownPrepareReceived.countDown();
+                        }
+                    }.start();
+                } else if (CarPowerManagementService.isCompletionAllowed(state)) {
+                    mService.completeHandlingPowerStateChange(state, this);
+                }
+            }
+        };
+
+        mService.registerInternalListener(listener);
+        // Transition to ON state
+        mPowerHal.setCurrentPowerState(new PowerState(VehicleApPowerStateReq.ON, 0));
+        mPowerSignalListener.waitFor(PowerHalService.SET_ON, WAIT_TIMEOUT_MS);
+        // Trigger first SHUTDOWN_PREPARE
+        mPowerHal.setCurrentPowerState(
+                new PowerState(
+                        VehicleApPowerStateReq.SHUTDOWN_PREPARE,
+                        VehicleApPowerStateShutdownParam.SHUTDOWN_ONLY));
+        // Wait for signal from listener
+        shutdownPrepareReceived.await(WAIT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        // Trigger second SHUTDOWN_PREPARE
+        mPowerHal.setCurrentPowerState(
+                new PowerState(
+                        VehicleApPowerStateReq.SHUTDOWN_PREPARE,
+                        VehicleApPowerStateShutdownParam.SHUTDOWN_IMMEDIATELY));
+        mPowerSignalListener.waitFor(PowerHalService.SET_SHUTDOWN_START, WAIT_TIMEOUT_MS);
+        // Send the finished signal
+        mPowerHal.setCurrentPowerState(new PowerState(VehicleApPowerStateReq.FINISHED, 0));
+        mSystemStateInterface.waitForShutdown(WAIT_TIMEOUT_MS);
+    }
+
     private void setCarPowerPolicyRefactoringFeatureFlag(boolean flagValue) {
         mFeatureFlags.setFlag(Flags.FLAG_CAR_POWER_POLICY_REFACTORING, flagValue);
     }
