9a7155c7c2d1931dfa7e12eba9092f855ac7f7d0
Gaurav Bhola <gauravbhola@google.com>
2025-01-07 10:51:36 -0800

    Add dependency from CarUiPortraitSystemUI to AutoShellModule.
    - This is required to satisfy build dependencies because CarUiPortraitSystemUI doesn't use the CarWmShellModule and instead creates a custom one.
    
    Change-Id: I07447e417299ee8a0c60b2e3e38f7f0e2a4ca3ae
    Flag: com.android.systemui.car.auto_task_stack_windowing
    Test: m
    Bug: 384082238

--- car_product/car_ui_portrait/apps/CarUiPortraitSystemUI/src/com/android/systemui/wmshell/CarUiPortraitWMShellModule.java
+++ car_product/car_ui_portrait/apps/CarUiPortraitSystemUI/src/com/android/systemui/wmshell/CarUiPortraitWMShellModule.java
@@ -29,6 +29,7 @@ import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.wm.CarUiPortraitDisplaySystemBarsController;
 import com.android.systemui.wm.DisplaySystemBarsController;
 import com.android.wm.shell.ShellTaskOrganizer;
+import com.android.wm.shell.automotive.AutoShellModule;
 import com.android.wm.shell.common.DisplayController;
 import com.android.wm.shell.common.DisplayInsetsController;
 import com.android.wm.shell.common.ShellExecutor;
@@ -58,7 +59,7 @@ import kotlinx.coroutines.CoroutineScope;
 import java.util.Optional;
 
 /** Provides dependencies from {@link com.android.wm.shell} for CarSystemUI. */
-@Module(includes = WMShellBaseModule.class)
+@Module(includes = {WMShellBaseModule.class, AutoShellModule.class})
 public abstract class CarUiPortraitWMShellModule {
 
     @WMSingleton
