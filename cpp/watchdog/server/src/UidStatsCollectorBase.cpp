0d36be166da1661adc29ef66172a703019ed8f3f
Samuel Johnson <samueldjohnson@google.com>
2025-03-06 15:07:49 -0800

    Extract I/O collector logic from UidStatsCollector to UidStatsCollectorBase
    Test: atest libpilotwatchdog_test
    Flag: EXEMPT refactor
    Fixes: 398042440
    Change-Id: I6adbae744e263ebd376c7c98f9d7d9761681861e

---
+++ cpp/watchdog/server/src/UidStatsCollectorBase.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2025, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "carwatchdogd"
+
+#include "UidStatsCollector.h"
+
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace android {
+namespace automotive {
+namespace watchdog {
+
+using ::android::base::Error;
+using ::android::base::Result;
+
+uid_t UidBaseStats::uid() const {
+    return static_cast<uid_t>(packageInfo.packageIdentifier.uid);
+}
+
+bool UidBaseStats::hasPackageInfo() const {
+    return !packageInfo.packageIdentifier.name.empty();
+}
+
+std::string UidBaseStats::genericPackageName() const {
+    if (hasPackageInfo()) {
+        return packageInfo.packageIdentifier.name;
+    }
+    return std::to_string(packageInfo.packageIdentifier.uid);
+}
+
+Result<void> UidStatsCollectorBase::collect() {
+    Mutex::Autolock lock(mMutex);
+    if (mUidIoStatsCollector->enabled()) {
+        if (const auto& result = mUidIoStatsCollector->collect(); !result.ok()) {
+            return Error() << "Failed to collect per-uid I/O stats: " << result.error();
+        }
+    }
+
+    mLatestBaseStats = process(mUidIoStatsCollector->latestStats());
+    mDeltaBaseStats = process(mUidIoStatsCollector->deltaStats());
+    return {};
+}
+
+std::vector<UidBaseStats> UidStatsCollectorBase::process(
+        const std::unordered_map<uid_t, UidIoStats>& uidIoStatsByUid) const {
+    if (uidIoStatsByUid.empty()) {
+        return std::vector<UidBaseStats>();
+    }
+    std::unordered_set<uid_t> uidSet;
+    for (const auto& [uid, _] : uidIoStatsByUid) {
+        uidSet.insert(uid);
+    }
+    std::vector<uid_t> uids;
+    for (const auto& uid : uidSet) {
+        uids.push_back(uid);
+    }
+    const auto packageInfoByUid = mPackageInfoResolver->getPackageInfosForUids(uids);
+    std::vector<UidBaseStats> uidBaseStats;
+    for (const auto& uid : uids) {
+        UidBaseStats curUidStats;
+        if (const auto it = packageInfoByUid.find(uid); it != packageInfoByUid.end()) {
+            curUidStats.packageInfo = it->second;
+        } else {
+            curUidStats.packageInfo.packageIdentifier.uid = uid;
+        }
+        if (const auto it = uidIoStatsByUid.find(uid); it != uidIoStatsByUid.end()) {
+            curUidStats.ioStats = it->second;
+        }
+        uidBaseStats.emplace_back(std::move(curUidStats));
+    }
+    return uidBaseStats;
+}
+
+}  // namespace watchdog
+}  // namespace automotive
+}  // namespace android
---
+++ cpp/watchdog/server/src/UidStatsCollectorBase.h
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2025, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "PackageInfoResolver.h"
+#include "UidIoStatsCollector.h"
+
+#include <aidl/android/automotive/watchdog/internal/PackageInfo.h>
+#include <android-base/result.h>
+#include <utils/Mutex.h>
+#include <utils/RefBase.h>
+#include <utils/StrongPointer.h>
+
+#include <string>
+#include <vector>
+
+namespace android {
+namespace automotive {
+namespace watchdog {
+
+// Forward declaration for testing use only.
+namespace internal {
+
+class UidStatsCollectorBasePeer;
+
+}  // namespace internal
+
+struct UidBaseStats {
+    aidl::android::automotive::watchdog::internal::PackageInfo packageInfo;
+    UidIoStats ioStats = {};
+    // Returns package name if the |packageInfo| is available. Otherwise, returns the |uid|.
+    std::string genericPackageName() const;
+    // Returns true when package info is available.
+    bool hasPackageInfo() const;
+    // Returns the uid for the stats;
+    uid_t uid() const;
+};
+
+// Collector/Aggregator for per-UID I/O stats.
+class UidStatsCollectorBaseInterface : virtual public RefBase {
+public:
+    // Initializes the collector.
+    virtual void init() = 0;
+    // Collects the per-UID I/O stats.
+    virtual android::base::Result<void> collect() = 0;
+    // Returns the latest per-uid I/O stats.
+    virtual const std::vector<UidBaseStats> latestBaseStats() const = 0;
+    // Returns the delta of per-uid I/O stats since the last before collection.
+    virtual const std::vector<UidBaseStats> deltaBaseStats() const = 0;
+    // Returns true only when the per-UID I/O stats files are accessible.
+    virtual bool enabled() const = 0;
+};
+
+class UidStatsCollectorBase : public UidStatsCollectorBaseInterface {
+public:
+    UidStatsCollectorBase() :
+          mPackageInfoResolver(PackageInfoResolver::getInstance()),
+          mUidIoStatsCollector(android::sp<UidIoStatsCollector>::make()) {}
+
+    void init() override {
+        Mutex::Autolock lock(mMutex);
+        mUidIoStatsCollector->init();
+    }
+
+    android::base::Result<void> collect() override;
+
+    const std::vector<UidBaseStats> latestBaseStats() const override {
+        Mutex::Autolock lock(mMutex);
+        return mLatestBaseStats;
+    }
+
+    const std::vector<UidBaseStats> deltaBaseStats() const override {
+        Mutex::Autolock lock(mMutex);
+        return mDeltaBaseStats;
+    }
+
+    bool enabled() const override { return mUidIoStatsCollector->enabled(); }
+
+protected:
+    // Local PackageInfoResolverInterface instance. Useful to mock in tests.
+    std::shared_ptr<PackageInfoResolverInterface> mPackageInfoResolver;
+
+    mutable Mutex mMutex;
+
+    android::sp<UidIoStatsCollectorInterface> mUidIoStatsCollector GUARDED_BY(mMutex);
+
+    std::vector<UidBaseStats> mLatestBaseStats GUARDED_BY(mMutex);
+
+    std::vector<UidBaseStats> mDeltaBaseStats GUARDED_BY(mMutex);
+
+private:
+    std::vector<UidBaseStats> process(
+            const std::unordered_map<uid_t, UidIoStats>& uidIoStatsByUid) const;
+
+    // For unit tests.
+    friend class internal::UidStatsCollectorBasePeer;
+};
+
+}  // namespace watchdog
+}  // namespace automotive
+}  // namespace android
