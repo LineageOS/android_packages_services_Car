aef506f9305e0d4d9be9318c128b14057835303a
Mayank Garg <gargmayank@google.com>
2025-03-14 13:30:17 -0700

    Migrate task listener to car wm shell
    Flag: EXEMPT refactor
    
    Bug: 401349206
    Test: atest CarWMShellUnitTests
    Change-Id: I7c83955bb913a8909ae1b6f9e5ca04ef7d370f9b

--- libs/car-wm-shell-lib/src/com/android/wm/shell/automotive/AutoShellModule.java
+++ libs/car-wm-shell-lib/src/com/android/wm/shell/automotive/AutoShellModule.java
@@ -16,10 +16,14 @@
 
 package com.android.wm.shell.automotive;
 
+import com.android.wm.shell.common.SyncTransactionQueue;
+import com.android.wm.shell.dagger.DynamicOverride;
 import com.android.wm.shell.dagger.WMSingleton;
+import com.android.wm.shell.fullscreen.FullscreenTaskListener;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
 
 
 @Module
@@ -27,4 +31,12 @@ public abstract class AutoShellModule {
     @WMSingleton
     @Binds
     abstract AutoTaskStackController provideTaskStackController(AutoTaskStackControllerImpl impl);
+
+    @WMSingleton
+    @Provides
+    @DynamicOverride
+    static FullscreenTaskListener provideFullScreenTaskListener(SyncTransactionQueue syncQueue,
+            AutoTaskRepository autoTaskRepository) {
+        return new AutoTaskRepository.AutoFullscreenTaskListener(syncQueue, autoTaskRepository);
+    }
 }
