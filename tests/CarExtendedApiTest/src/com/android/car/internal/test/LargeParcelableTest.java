5a8464e3c6d878795e9f70b79cf420520eb7982f
Yu Shan <shanyu@google.com>
2025-03-25 19:02:26 -0700

    Revert^2 "Using cleaner to replace finalize."
    This reverts commit 7dc9150b840ea0c319fdf15e56438e5dbaf3f24d.
    
    Reason for revert: Reland with fix for CarServiceHostUnitTest
    
    Test: atest LargeParcelableTest
    Flag: EXEMPT internal refactor
    Bug: 402291632
    
    Change-Id: Ica4fa78e1ca349c4b819207e6d73417e23259c11

--- tests/CarExtendedApiTest/src/com/android/car/internal/test/LargeParcelableTest.java
+++ tests/CarExtendedApiTest/src/com/android/car/internal/test/LargeParcelableTest.java
@@ -34,7 +34,6 @@ import androidx.test.filters.LargeTest;
 import androidx.test.filters.SmallTest;
 
 import com.android.car.internal.LargeParcelable;
-import com.android.compatibility.common.util.NonApiTest;
 
 import org.junit.After;
 import org.junit.Before;
@@ -104,95 +103,75 @@ public final class LargeParcelableTest extends CarLessApiTestBase {
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testLocalSerializationDeserializationSmallPayload() throws Exception {
         doTestLocalSerializationDeserialization(ARRAY_LENGTH_SMALL);
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testLocalSerializationDeserializationBigPayload() throws Exception {
         doTestLocalSerializationDeserialization(ARRAY_LENGTH_BIG);
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testLocalSerializationDeserializationNullPayload() throws Exception {
-        TestLargeParcelable origParcelable = new TestLargeParcelable();
-        Parcel dest = Parcel.obtain();
-        origParcelable.writeToParcel(dest, 0);
-        dest.setDataPosition(0);
+        try (TestLargeParcelable origParcelable = new TestLargeParcelable()) {
+            Parcel dest = Parcel.obtain();
+            origParcelable.writeToParcel(dest, 0);
+            dest.setDataPosition(0);
 
-        TestLargeParcelable newParcelable = new TestLargeParcelable(dest);
-
-        assertThat(newParcelable.byteData).isNull();
+            try (TestLargeParcelable newParcelable = new TestLargeParcelable(dest)) {
+                assertThat(newParcelable.byteData).isNull();
+            }
+        }
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testRemoteNullPayload() throws Exception {
-        TestLargeParcelable origParcelable = new TestLargeParcelable();
-
-        TestLargeParcelable r = mBinder.echoTestLargeParcelable(origParcelable);
-
-        assertThat(r).isNotNull();
-        assertThat(r.byteData).isNull();
+        try (TestLargeParcelable origParcelable = new TestLargeParcelable()) {
+            try (TestLargeParcelable r = mBinder.echoTestLargeParcelable(origParcelable)) {
+                assertThat(r).isNotNull();
+                assertThat(r.byteData).isNull();
+            }
+        }
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testTestParcelableSmallPayload() throws Exception {
         doTestLargeParcelable(ARRAY_LENGTH_SMALL);
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testTestParcelableBigPayload() throws Exception {
         doTestLargeParcelable(ARRAY_LENGTH_BIG);
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testLargeParcelableSmallPayload() throws Exception {
         doTestTestLargeParcelable(ARRAY_LENGTH_SMALL);
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testLargeParcelableBigPayload() throws Exception {
         doTestTestLargeParcelable(ARRAY_LENGTH_BIG);
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testMultiArgsWithNullPayload() throws Exception {
-        TestLargeParcelable origParcelable = new TestLargeParcelable();
         long argValue = 0x12345678;
 
-        long r = mBinder.echoLongWithTestLargeParcelable(origParcelable, argValue);
+        try (TestLargeParcelable origParcelable = new TestLargeParcelable()) {
+            long r = mBinder.echoLongWithTestLargeParcelable(origParcelable, argValue);
 
-        assertThat(r).isEqualTo(argValue);
+            assertThat(r).isEqualTo(argValue);
+        }
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testMultiArgsSmallPayload() throws Exception {
         doTestMultipleArgs(ARRAY_LENGTH_SMALL);
     }
 
     @Test
-    @NonApiTest(exemptionReasons = {}, justification = "Testing large parcelable, which is a "
-            + "hidden API")
     public void testMultiArgsBigPayload() throws Exception {
         doTestMultipleArgs(ARRAY_LENGTH_BIG);
     }
@@ -214,7 +193,9 @@ public final class LargeParcelableTest extends CarLessApiTestBase {
         }
 
         for (int i = 0; i < loopCount; i++) {
-            mBinder.echoTestLargeParcelable(parcelables[i]);
+            try (TestLargeParcelable echoed = mBinder.echoTestLargeParcelable(parcelables[i])) {
+                // Do nothing.
+            }
             // After close, the shared memory allocated should be closed, so we will not keep
             // increasing memory size.
             parcelables[i].close();
@@ -223,50 +204,58 @@ public final class LargeParcelableTest extends CarLessApiTestBase {
 
     private void doTestLargeParcelable(int payloadSize) throws Exception {
         byte[] origArray = createByteArray(payloadSize);
+
         TestParcelable origParcelable = new TestParcelable(origArray);
+        try (var sendParcelable = new LargeParcelable(origParcelable)) {
+            try (LargeParcelable r = mBinder.echoLargeParcelable(sendParcelable)) {
+                assertThat(r).isNotNull();
 
-        LargeParcelable r = mBinder.echoLargeParcelable(new LargeParcelable(origParcelable));
-        assertThat(r).isNotNull();
+                TestParcelable receivedParcelable = (TestParcelable) r.getParcelable();
 
-        TestParcelable receivedParcelable = (TestParcelable) r.getParcelable();
-
-        assertThat(receivedParcelable).isNotNull();
-        assertThat(receivedParcelable.byteData).isEqualTo(origArray);
+                assertThat(receivedParcelable).isNotNull();
+                assertThat(receivedParcelable.byteData).isEqualTo(origArray);
+            }
+        }
     }
 
     private void doTestTestLargeParcelable(int payloadSize) throws Exception {
         byte[] origArray = createByteArray(payloadSize);
-        TestLargeParcelable origParcelable = new TestLargeParcelable(origArray);
 
-        TestLargeParcelable r = mBinder.echoTestLargeParcelable(origParcelable);
-
-        assertThat(r).isNotNull();
-        assertThat(r.byteData).isNotNull();
-        assertThat(r.byteData).isEqualTo(origArray);
+        try (var origParcelable = new TestLargeParcelable(origArray)) {
+            try (TestLargeParcelable r = mBinder.echoTestLargeParcelable(origParcelable)) {
+                assertThat(r).isNotNull();
+                assertThat(r.byteData).isNotNull();
+                assertThat(r.byteData).isEqualTo(origArray);
+            }
+        }
     }
 
     private void doTestLocalSerializationDeserialization(int payloadSize) throws Exception {
         byte[] origArray = createByteArray(payloadSize);
-        TestLargeParcelable origParcelable = new TestLargeParcelable(origArray);
-        Parcel dest = Parcel.obtain();
+        try (TestLargeParcelable origParcelable = new TestLargeParcelable(origArray)) {
+            Parcel dest = Parcel.obtain();
 
-        origParcelable.writeToParcel(dest, 0);
-        dest.setDataPosition(0);
-        TestLargeParcelable newParcelable = new TestLargeParcelable(dest);
+            origParcelable.writeToParcel(dest, 0);
+            dest.setDataPosition(0);
 
-        assertThat(newParcelable.byteData).isNotNull();
-        assertThat(newParcelable.byteData).isEqualTo(origArray);
+            try (TestLargeParcelable newParcelable = new TestLargeParcelable(dest)) {
+                assertThat(newParcelable.byteData).isNotNull();
+                assertThat(newParcelable.byteData).isEqualTo(origArray);
+            }
+        }
     }
 
     private void doTestMultipleArgs(int payloadSize) throws Exception {
         byte[] origArray = createByteArray(payloadSize);
-        TestLargeParcelable origParcelable = new TestLargeParcelable(origArray);
-        long argValue = 0x12345678;
-        long expectedRet = argValue + LargeParcelableTestService.calcByteSum(origParcelable);
 
-        long r = mBinder.echoLongWithTestLargeParcelable(origParcelable, argValue);
+        try (TestLargeParcelable origParcelable = new TestLargeParcelable(origArray)) {
+            long argValue = 0x12345678;
+            long expectedRet = argValue + LargeParcelableTestService.calcByteSum(origParcelable);
 
-        assertThat(r).isEqualTo(expectedRet);
+            long r = mBinder.echoLongWithTestLargeParcelable(origParcelable, argValue);
+
+            assertThat(r).isEqualTo(expectedRet);
+        }
     }
 
     /**
