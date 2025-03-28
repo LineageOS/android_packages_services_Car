6711d50a8f1e64cbacc1e643d385d6ca43bd531c
Tyler Trephan <tylertrephan@google.com>
2025-03-28 22:32:57 +0000

    Update ContCarPropertyEventTracker debug logging to verbose
    Test: None
    Fix: 406066852
    Flag: EXEMPT log only update
    Change-Id: I92e7d67765e2c1ebfcee0dfdd481b53f03d24d47

--- car-lib/src/com/android/car/internal/property/ContCarPropertyEventTracker.java
+++ car-lib/src/com/android/car/internal/property/ContCarPropertyEventTracker.java
@@ -126,7 +126,7 @@ public final class ContCarPropertyEventTracker implements CarPropertyEventTracke
     public boolean hasUpdate(CarPropertyValue<?> carPropertyValue) {
         if (carPropertyValue.getTimestamp() < mNextUpdateTimeNanos) {
             if (DBG) {
-                Slogf.d(TAG,
+                Slogf.v(TAG,
                         "hasUpdate: Dropping carPropertyValue: %s, "
                         + "because getTimestamp()=%d < nextUpdateTimeNanos=%d",
                         carPropertyValue, carPropertyValue.getTimestamp(), mNextUpdateTimeNanos);
@@ -141,7 +141,7 @@ public final class ContCarPropertyEventTracker implements CarPropertyEventTracke
         if (mEnableVur && status == mCurrentStatus && mCurrentCarPropertyValue != null
                     && Objects.deepEquals(value, mCurrentCarPropertyValue.getValue())) {
             if (DBG) {
-                Slogf.d(TAG,
+                Slogf.v(TAG,
                                 "hasUpdate: Dropping carPropertyValue: %s, "
                                 + "because VUR is enabled and value is the same",
                         sanitizedCarPropertyValue);
