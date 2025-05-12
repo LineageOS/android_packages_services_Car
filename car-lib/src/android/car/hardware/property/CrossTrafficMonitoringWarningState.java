fc9efe4d94c06a39055378374d706bec3bc6e0ea
shrikar <shrikar@google.com>
2025-05-12 19:51:45 +0000

    Removed @hide and @SystemApi tags from non-system enums.
    In 25Q2, some properties had their @SystemApi tags removed. The enums
    for each of these properties however, did not have their @hide and
    @SystemApi tags removed. This CL removes them.
    
    Bug: 414429013
    Test: atest CtsCarTestCases
    Flag: android.car.feature.vehicle_property_enums_remove_system_api_tags
    API-Coverage-Bug: 419559212
    Change-Id: Iae8c6b8a6dfb1fda36f93a4d84d488f959d4295c

--- car-lib/src/android/car/hardware/property/CrossTrafficMonitoringWarningState.java
+++ car-lib/src/android/car/hardware/property/CrossTrafficMonitoringWarningState.java
@@ -16,12 +16,11 @@
 
 package android.car.hardware.property;
 
-import static android.car.feature.Flags.FLAG_ANDROID_VIC_VEHICLE_PROPERTIES;
+import static android.car.feature.Flags.FLAG_VEHICLE_PROPERTY_ENUMS_REMOVE_SYSTEM_API_TAGS;
 
 import android.annotation.FlaggedApi;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
-import android.annotation.SystemApi;
 
 import com.android.car.internal.util.ConstantDebugUtils;
 
@@ -30,11 +29,8 @@ import java.lang.annotation.RetentionPolicy;
 
 /**
  * Used to enumerate the state of Cross Traffic Monitoring Warning system.
- *
- * @hide
  */
-@FlaggedApi(FLAG_ANDROID_VIC_VEHICLE_PROPERTIES)
-@SystemApi
+@FlaggedApi(FLAG_VEHICLE_PROPERTY_ENUMS_REMOVE_SYSTEM_API_TAGS)
 public final class CrossTrafficMonitoringWarningState {
 
     /**
