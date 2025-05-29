867cba3850b974f33883af437b1d041bd387b39e
Mayank Garg <gargmayank@google.com>
2025-05-29 12:43:56 -0700

    Add shell command to set focused panel
    Flag: com.android.wm.shell.enable_auto_task_stack_controller
    
    Bug: 415124173
    Test: Manual using the shell commands
       step 0: adb shell dumpsys activity | grep -i "focuseda\|focusedw"
        <this will give the current focused window and app>
       step 1: adb shell wm shell car-wm-shell get-root-tasks
        <get the root task id to set focus, use it in step 2>
       step 2: adb shell wm shell car-wm-shell set-focus-root-task <taskId choose from step1>
       Step 3: adb shell dumpsys activity | grep -i "focuseda\|focusedw"
        <Focus app should update to the panel given in step 2>
    Change-Id: I8f469141c8540773eaccf10220216e3ca9b379cb

--- libs/car-wm-shell-lib/src/com/android/wm/shell/automotive/AutoTaskStackControllerImpl.kt
+++ libs/car-wm-shell-lib/src/com/android/wm/shell/automotive/AutoTaskStackControllerImpl.kt
@@ -798,4 +798,8 @@ class AutoTaskStackControllerImpl @Inject constructor(
     ) {
         var isClaimed: IBinder? = null
     }
+
+    fun getRootTasks(): List<AutoTaskStack> {
+        return taskStackMap.values.toList()
+    }
 }
