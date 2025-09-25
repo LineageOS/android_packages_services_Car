486c6fa08cae5f6cd83d3d65921f5c8c58f4e746
Aaqib Ismail <aaqibismail@google.com>
2025-09-24 17:01:12 -0700

    Add E2E test for property vendor status
    This test uses the reference VHAL to inject events and validates that
    the client receives the expected vendor statuses.
    
    Bug: 405477436
    Bug: 417572359
    Test: atest CarExtendedApiTest:CarPropertyManagerTest
    Flag: android.car.feature.car_property_status_detailed_not_available
    Change-Id: I266a5b9c4a3aac765bbaf52b0571e6287a362af0

--- tests/CarExtendedApiTest/src/android/car/extendedapitest/CarPropertyManagerTest.java
+++ tests/CarExtendedApiTest/src/android/car/extendedapitest/CarPropertyManagerTest.java
@@ -17,6 +17,7 @@
 package android.car.extendedapitest;
 
 import static android.hardware.automotive.vehicle.TestVendorProperty.VENDOR_PROPERTY_FOR_ERROR_CODE_TESTING;
+import static android.hardware.automotive.vehicle.TestVendorProperty.VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -25,17 +26,28 @@ import static org.junit.Assume.assumeTrue;
 
 import android.car.Car;
 import android.car.extendedapitest.testbase.CarApiTestBase;
+import android.car.feature.Flags;
 import android.car.hardware.CarPropertyConfig;
+import android.car.hardware.CarPropertyValue;
 import android.car.hardware.property.CarInternalErrorException;
 import android.car.hardware.property.CarPropertyManager;
+import android.car.hardware.property.CarPropertyManager.CarPropertyEventCallback;
 import android.car.test.CarTestManager;
+import android.car.test.PermissionsCheckerRule;
+import android.car.test.PermissionsCheckerRule.EnsureHasPermission;
 import android.hardware.automotive.vehicle.VehicleArea;
 import android.hardware.automotive.vehicle.VehiclePropertyGroup;
+import android.hardware.automotive.vehicle.VehiclePropertyStatus;
 import android.hardware.automotive.vehicle.VehiclePropertyType;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerExecutor;
 import android.os.HandlerThread;
+import android.os.SystemClock;
+import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.platform.test.flag.junit.CheckFlagsRule;
+import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 import android.util.ArraySet;
 import android.util.Log;
 
@@ -44,12 +56,15 @@ import com.android.compatibility.common.util.ApiTest;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
 public final class CarPropertyManagerTest extends CarApiTestBase {
     private static final int EXPECTED_VENDOR_ERROR_CODE = 0x00ab;
@@ -60,8 +75,18 @@ public final class CarPropertyManagerTest extends CarApiTestBase {
     private static final int END_TEST_CODES = 0x3000 + NUMBER_OF_TEST_CODES
             | VehiclePropertyGroup.VENDOR | VehicleArea.GLOBAL | VehiclePropertyType.INT32;
     private static final String TAG = CarPropertyManagerTest.class.getSimpleName();
+    private static final long VHAL_DUMP_TIMEOUT_MS = 1000;
+    // Number of bits to shift left to set the property vendor status in VHAL VehiclePropValue
+    // status field.
+    private static final int VENDOR_STATUS_SHIFT = 16;
 
     private final HandlerThread mHandlerThread = new HandlerThread(getClass().getSimpleName());
+
+    @Rule
+    public final PermissionsCheckerRule mPermissionsCheckerRule = new PermissionsCheckerRule();
+
+    @Rule
+    public final CheckFlagsRule mCheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
 
     private CarPropertyManager mCarPropertyManager;
     private CarTestManager mTestManager;
@@ -70,6 +95,7 @@ public final class CarPropertyManagerTest extends CarApiTestBase {
 
     @Before
     public void setUp() throws Exception {
+        getContext().getApplicationInfo().targetSdkVersion = Build.VERSION_CODES.CUR_DEVELOPMENT;
         mHandlerThread.start();
         mHandler = new Handler(mHandlerThread.getLooper());
         mCarPropertyManager = (CarPropertyManager) getCar().getCarManager(Car.PROPERTY_SERVICE);
@@ -109,6 +135,75 @@ public final class CarPropertyManagerTest extends CarApiTestBase {
                         VENDOR_PROPERTY_FOR_ERROR_CODE_TESTING, /* areaId = */ 0, /* val= */ 0));
 
         assertThat(thrown.getVendorErrorCode()).isEqualTo(EXPECTED_VENDOR_ERROR_CODE);
+    }
+
+
+    @RequiresFlagsEnabled(Flags.FLAG_CAR_PROPERTY_STATUS_DETAILED_NOT_AVAILABLE)
+    @EnsureHasPermission(Car.PERMISSION_READ_PROPERTY_VENDOR_STATUS)
+    @ApiTest(
+            apis = {
+                "android.car.hardware.property.CarPropertyManager#subscribePropertyEvents",
+                "android.car.hardware.CarPropertyValue#getPropertyVendorStatus"
+            })
+    @Test
+    public void testSubscribePropertyEvents_getPropertyVendorStatus() throws Exception {
+        List<CarPropertyConfig> carPropertyConfigs =
+                mCarPropertyManager.getPropertyList(
+                        new ArraySet(List.of(VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING)));
+
+        assumeTrue(
+                "VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING is not supported",
+                !carPropertyConfigs.isEmpty());
+
+        int areaId = 0;
+        int numEvents = 3;
+        TestCallback callback =
+                new TestCallback(VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING, areaId, numEvents);
+
+        assertThat(
+                        mCarPropertyManager.subscribePropertyEvents(
+                                VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING, callback))
+                .isTrue();
+
+        CarPropertyValue initialValue = callback.waitAndGetInitialEvent();
+
+        expectThat(initialValue.getPropertyId())
+                .isEqualTo(VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING);
+        expectThat(initialValue.getAreaId()).isEqualTo(areaId);
+        expectThat(initialValue.getValue()).isEqualTo(0);
+        // The initial event is created via getProperty call in CarPropertyService.
+        // The vendor error codes are not mapped to the vendor status codes.
+        expectThat(initialValue.getPropertyVendorStatus()).isEqualTo(0);
+
+        int value = 0;
+        int vendorStatus = 0xdead;
+        int status = vendorStatus << VENDOR_STATUS_SHIFT | VehiclePropertyStatus.ERROR;
+        injectEventFromVehicleSide(
+                VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING,
+                areaId,
+                value,
+                status,
+                SystemClock.elapsedRealtimeNanos());
+        injectEventFromVehicleSide(
+                VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING,
+                areaId,
+                value,
+                initialValue.getPropertyVendorStatus(),
+                SystemClock.elapsedRealtimeNanos());
+
+        List<CarPropertyValue> carPropertyValues = callback.waitAndGetChangeEvents();
+        mCarPropertyManager.unsubscribePropertyEvents(callback);
+
+        assertThat(carPropertyValues).hasSize(2);
+        for (CarPropertyValue carPropertyValue : carPropertyValues) {
+            expectThat(carPropertyValue.getPropertyId())
+                    .isEqualTo(VENDOR_PROPERTY_FOR_PROPERTY_STATUS_TESTING);
+            expectThat(carPropertyValue.getAreaId()).isEqualTo(areaId);
+            expectThat(carPropertyValue.getValue()).isEqualTo(value);
+        }
+        expectThat(carPropertyValues.get(0).getPropertyVendorStatus()).isEqualTo(vendorStatus);
+        expectThat(carPropertyValues.get(1).getPropertyVendorStatus())
+                .isEqualTo(initialValue.getPropertyVendorStatus());
     }
 
     @ApiTest(apis = {"android.car.hardware.property.CarPropertyManager#getPropertyList()"})
@@ -250,6 +345,90 @@ public final class CarPropertyManagerTest extends CarApiTestBase {
             Log.d(TAG, "Starting car service for test");
         } finally {
             mTestManager.startCarService(restartCarService);
+        }
+    }
+
+    /**
+     * Inject events from vehicle side. It change the value of property even the property is a
+     * read_only property such as GEAR_SELECTION. It only works with reference VHAL.
+     */
+    private void injectEventFromVehicleSide(
+            int propertyId, int areaId, int value, int status, long timestampNs) {
+        String propIdStr = Integer.toString(propertyId);
+        String areaIdStr = Integer.toString(areaId);
+        String valueStr = Integer.toString(value);
+        String statusStr = Integer.toString(status);
+        String timestampStr = Long.toString(timestampNs);
+
+        String output =
+                mTestManager.dumpVhal(
+                        List.of(
+                                "--inject-event",
+                                propIdStr,
+                                "-p",
+                                statusStr,
+                                "-a",
+                                areaIdStr,
+                                "-i",
+                                valueStr,
+                                "-t",
+                                timestampStr),
+                        VHAL_DUMP_TIMEOUT_MS);
+
+        if (!output.contains("injected")) {
+            throw new IllegalArgumentException(
+                    "Failed to set the property via VHAL dump, output: " + output);
+        }
+    }
+
+    private static final class TestCallback implements CarPropertyEventCallback {
+        private static final String TAG = TestCallback.class.getSimpleName();
+        private static final int CALLBACK_TIMEOUT_MS = 5000;
+        private final Object mLock = new Object();
+        private final List<CarPropertyValue> mCarPropertyValues = new ArrayList<>();
+        private final CountDownLatch mInitialEventLatch = new CountDownLatch(1);
+        private final CountDownLatch mChangeEventsLatch;
+        private final int mPropertyId;
+        private final int mAreaId;
+
+        TestCallback(int propertyId, int areaId, int numEvents) {
+            mChangeEventsLatch = new CountDownLatch(numEvents);
+            mPropertyId = propertyId;
+            mAreaId = areaId;
+        }
+
+        @Override
+        public void onChangeEvent(CarPropertyValue carPropertyValue) {
+            if (carPropertyValue.getPropertyId() != mPropertyId
+                    || carPropertyValue.getAreaId() != mAreaId) {
+                return;
+            }
+            synchronized (mLock) {
+                mCarPropertyValues.add(carPropertyValue);
+                mInitialEventLatch.countDown();
+                mChangeEventsLatch.countDown();
+            }
+        }
+
+        @Override
+        public void onErrorEvent(int propertyId, int areaId) {
+            Log.w(TAG, "Error event for propertyId: " + propertyId + ", areaId: " + areaId);
+        }
+
+        CarPropertyValue waitAndGetInitialEvent() throws InterruptedException {
+            assertThat(mInitialEventLatch.await(CALLBACK_TIMEOUT_MS, TimeUnit.MILLISECONDS))
+                    .isTrue();
+            synchronized (mLock) {
+                return mCarPropertyValues.get(0);
+            }
+        }
+
+        List<CarPropertyValue> waitAndGetChangeEvents() throws InterruptedException {
+            assertThat(mChangeEventsLatch.await(CALLBACK_TIMEOUT_MS, TimeUnit.MILLISECONDS))
+                    .isTrue();
+            synchronized (mLock) {
+                return List.copyOf(mCarPropertyValues.subList(1, mCarPropertyValues.size()));
+            }
         }
     }
 }
