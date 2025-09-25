7bc0f7a25ed7823e5606ccae3cf992b404d68b39
Ram Parameswaran <rampara@google.com>
2025-09-25 21:27:29 +0000

    Remove CarUiPortraitMessengerRRO
    Bug: 443324266
    Test: N/A
    Flag: EXEMPT PURE_REFACTOR
    
    Change-Id: I0028dc489a5f9f1feccb6aab2ae30f8dcada2e2e

--- car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk
+++ car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk
@@ -34,7 +34,6 @@ PRODUCT_PACKAGES += \
     CarUiPortraitLauncherReferenceRRO \
     CarUiPortraitMediaCommonRRO \
     CarUiPortraitMediaRRO \
-    CarUiPortraitMessengerRRO \
     CarUiPortraitNotificationRRO \
     CarUiPortraitRadioRRO \
     CarUiPortraitSettingsRRO \
