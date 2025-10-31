e9f33d00ec0ed78c01bb5deb03ac68a4c3759588
Lakshman Annadorai <lakshmana@google.com>
2025-10-31 13:00:30 -0700

    KitchenSink: Implement Perfetto trace list activity for system user
    Adds a new activity, PerfettoTraceListActivity, to display saved
    Perfetto traces. This activity is designed to run as the system user
    (User 0) to ensure it can access trace files stored in the system
    user's context.
    
    Key changes include:
    - Creation of PerfettoTraceListActivity to list Perfetto trace files.
    - Modification of PerfettoFragment to launch PerfettoTraceListActivity.
    - Update to PerfettoReportService to correctly launch
      PerfettoTraceListActivity via a notification, ensuring it runs in
      the system user's context.
    - Registration of PerfettoTraceListActivity in AndroidManifest.xml.
    - Addition of a 'Back' button in PerfettoTraceListActivity to return to
      the PerfettoFragment for the current non-system user.
    - Refactoring of PerfettoTraceListActivity to use a dedicated directory
      for storing trace files and to display a title.
    - Update PerfettoFragment layout to split the buttons across 2 rows.
    
    Fixes: 445244218
    Fixes: 456288302
    Test: `m EmbeddedKitchenSinkApp` and Verified manually
    Flag: EXEMPT TEST_ONLY
    Change-Id: I09d5fc0cd59e8c23827aee84509a3486b3c4127a

--- tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/perfetto/PerfettoReportService.java
+++ tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/perfetto/PerfettoReportService.java
@@ -16,16 +16,40 @@
 
 package com.google.android.car.kitchensink.perfetto;
 
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.ParcelFileDescriptor.AutoCloseInputStream;
+import android.os.UserHandle;
 import android.service.tracing.TraceReportService;
 import android.util.Log;
+
+import com.google.android.car.kitchensink.R;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * A service that receives Perfetto traces and saves them to disk.
+ *
+ * <p>This service is always started in user 0 / system user's context by the TraceReportService
+ * API. Thus, the traces are saved under user 0's directory.
+ */
 public class PerfettoReportService extends TraceReportService {
-    public static final String TAG = "PerfettoReportService";
+    private static final String TAG = PerfettoReportService.class.getSimpleName();
+    private static final String NOTIFICATION_CHANNEL_ID = "KITCHENSINK_PERFETTO_REPORT_SERVICE";
+    private static final int NOTIFICATION_ID = 1;
+    public static final String TRACE_FILES_ROOT_DIR = "perfetto_traces";
+
+    private NotificationManager mNotificationManager;
+    private NotificationChannel mNotificationChannel;
+    private String mNotificationTitle;
+    private String mNotificationContent;
 
     @Override
     public void onCreate() {
@@ -37,6 +61,13 @@ public class PerfettoReportService extends TraceReportService {
             throw new AssertionError("Trace reporting service should not be enabled pre-T.");
         }
         super.onCreate();
+        mNotificationManager = getSystemService(NotificationManager.class);
+        mNotificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                getString(R.string.perfetto_notification_channel_label),
+                NotificationManager.IMPORTANCE_HIGH);
+        mNotificationTitle = getString(R.string.perfetto_issue_detected_notification_title);
+        mNotificationContent = getString(R.string.perfetto_issue_detected_notification_content);
+        mNotificationManager.createNotificationChannel(mNotificationChannel);
     }
 
     @Override
@@ -45,7 +76,11 @@ public class PerfettoReportService extends TraceReportService {
         String fileName = "perfetto_" + args.getUuid().toString() + ".trace";
 
         try {
-            File f = new File(getFilesDir(), fileName);
+            File traceDir = new File(getFilesDir(), TRACE_FILES_ROOT_DIR);
+            if (!traceDir.exists()) {
+                traceDir.mkdirs();
+            }
+            File f = new File(traceDir, fileName);
             boolean created = f.createNewFile();
             if (!created) {
                 throw new IllegalStateException("Failed to create file");
@@ -56,8 +91,42 @@ public class PerfettoReportService extends TraceReportService {
                 }
             }
             Log.d(TAG, "Wrote the trace to " + f.getAbsolutePath());
+            sendNotification();
         } catch (IOException e) {
             throw new IllegalStateException("IO Exception", e);
         }
+    }
+
+    private void sendNotification() {
+        PendingIntent listTracesIntent = getPendingIntent(this);
+        Notification notification = new Notification.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setContentTitle(mNotificationTitle)
+                .setContentText(mNotificationContent)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setCategory(Notification.CATEGORY_CAR_WARNING)
+                .setLocalOnly(true)
+                .setContentIntent(listTracesIntent)
+                .setAutoCancel(true)
+                .build();
+
+        // The notification is triggered from User 0's context. So, send the notification for the
+        // current visible foreground user. Don't cache the user ID because the user profiles may
+        // switch anytime.
+        mNotificationManager.notifyAsUser(TAG, NOTIFICATION_ID, notification,
+                UserHandle.of(ActivityManager.getCurrentUser()));
+        Log.d(TAG, "Sent user notification with id " + NOTIFICATION_ID);
+    }
+
+    /**
+     * Returns a {@link PendingIntent} to launch the {@link PerfettoTraceListActivity}.
+     *
+     * {@link PerfettoTraceListActivity} must be launched in user 0 context to show the traces
+     * saved under user 0's directory.
+     */
+    static PendingIntent getPendingIntent(Context context) {
+        Intent intent = new Intent(context, PerfettoTraceListActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return PendingIntent.getActivity(context, NOTIFICATION_ID, intent,
+                PendingIntent.FLAG_IMMUTABLE, /* options= */ null);
     }
 }
---
+++ tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/perfetto/PerfettoTraceListActivity.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.car.kitchensink.perfetto;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.android.car.kitchensink.KitchenSinkActivity;
+import com.google.android.car.kitchensink.R;
+
+import java.io.File;
+
+/**
+ * An activity that shows a list of saved Perfetto traces.
+ *
+ * <p>This activity is always launched as the system user to be able to read the trace files
+ * saved in the system user's context.
+ */
+public class PerfettoTraceListActivity extends Activity {
+    private static final String TAG = "PerfettoTraceList";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int myUserId = UserHandle.myUserId();
+        Log.i(TAG, "onCreate userid " + myUserId);
+        if (myUserId != UserHandle.USER_SYSTEM) {
+            Log.i(TAG, "onCreate re-starting self as user 0");
+            Intent selfIntent = new Intent(this, PerfettoTraceListActivity.class);
+            startActivityAsUser(selfIntent, UserHandle.SYSTEM);
+            finish();
+            return;
+        }
+
+        setContentView(R.layout.perfetto_trace_list_activity);
+        Button backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, KitchenSinkActivity.class);
+            intent.putExtra("select", "perfetto");
+            startActivityAsUser(intent, UserHandle.of(ActivityManager.getCurrentUser()));
+            finish();
+        });
+
+        showTraceList();
+    }
+
+    private void showTraceList() {
+        ListView listView = findViewById(R.id.trace_list);
+        File traceDir = new File(getFilesDir(), PerfettoReportService.TRACE_FILES_ROOT_DIR);
+        File[] traceFiles = traceDir.listFiles((dir, name) -> name.startsWith("perfetto_")
+                && name.endsWith(".trace"));
+
+        if (traceFiles == null || traceFiles.length == 0) {
+            String[] noFiles = {getString(R.string.perfetto_trace_list_no_files)};
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_list_item_1, noFiles);
+            listView.setAdapter(adapter);
+            return;
+        }
+
+        String[] traceFileNames = new String[traceFiles.length];
+        for (int i = 0; i < traceFiles.length; i++) {
+            traceFileNames[i] = traceFiles[i].getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, traceFileNames);
+        listView.setAdapter(adapter);
+    }
+}
