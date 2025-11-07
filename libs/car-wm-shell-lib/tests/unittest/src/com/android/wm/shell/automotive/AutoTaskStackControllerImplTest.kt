86a8aca69119cbb7f327a92611f8677829aa58cd
Gaurav Bhola <gauravbhola@google.com>
2025-11-07 10:44:22 -0800

    Improve the reconciliation, merge logic and logging
    - Task panel can become invisible if home is launched in the same
      transition because of timing.
    - Before this change such a panel (even when not requested to be
      invisible) will go invisible and clients won't know about it.
    - After this change, this panel is counted in the changedTaskStacks
      correctly with the correct visibility so that client (scalable-ui)
      knows about the correct state.
    
    - Reconcile the task stack states even after merge has happened as even
      this represents that the wm change for the transition has been applied
    
    - Reformat the logs and show when the build is debuggable
    
    FLAG: EXEMPT BUGFIX
    Bug: 450270107
    Test: atest AutoTaskStackControllerImplTest
    Test: Test using script a shell script to start activities belonging to
          different panels + home activity repeatedly, stopping the
          execution randomly and then observing that the panels are in an
          expected state
    
    Change-Id: I52c95b711ad94896f4d5f68fa532123a276237f9

--- libs/car-wm-shell-lib/tests/unittest/src/com/android/wm/shell/automotive/AutoTaskStackControllerImplTest.kt
+++ libs/car-wm-shell-lib/tests/unittest/src/com/android/wm/shell/automotive/AutoTaskStackControllerImplTest.kt
@@ -30,6 +30,7 @@ import android.os.Looper
 import android.testing.AndroidTestingRunner
 import android.view.SurfaceControl
 import android.view.WindowManager.TRANSIT_OPEN
+import android.view.WindowManager.TRANSIT_TO_BACK
 import android.window.TaskOrganizer
 import android.window.TransitionInfo
 import android.window.TransitionRequestInfo
@@ -1019,6 +1020,7 @@ class AutoTaskStackControllerImplTest : CarWmShellTestCase() {
             })
             .addChange(TransitionInfo.Change(rootTaskInfo2.token, taskLeash).apply {
                 taskInfo = rootTaskInfo2
+                mode = TRANSIT_TO_BACK
             })
             // Send an additional change for the rootTask3 child
             .addChange(TransitionInfo.Change(rootTask3Child.token, taskLeash).apply {
@@ -1039,6 +1041,10 @@ class AutoTaskStackControllerImplTest : CarWmShellTestCase() {
         // Assert
         assertThat(result).isTrue()
         assertThat(delegate.lastTaskStackStates).containsKey(rootTaskInfo3.taskId)
+        assertThat(delegate.lastTaskStackStates).containsEntry(
+            rootTaskInfo2.taskId,
+            AutoTaskStackState(Rect(10, 10, 40, 300), false, 0)
+        )
         assertThat(delegate.lastTaskStackStates).containsEntry(
             rootTaskInfo3.taskId,
             AutoTaskStackState(Rect(10, 10, 40, 300), true, 900)
