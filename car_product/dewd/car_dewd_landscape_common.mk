a1d31456ff3776af08e969847458140fc012adf5
Nolen Johnson <johnsonnolen@gmail.com>
2026-01-14 21:15:54 -0500

    dewd: Fix broken makefile
    * Seriously, Google?
    * EPPE Google vendor app and wrong comment.
    
    Change-Id: I04bb71d349de3ac7af5278b0e600ca5a8c1e518f

--- car_product/dewd/car_dewd_landscape_common.mk
+++ car_product/dewd/car_dewd_landscape_common.mk
@@ -15,9 +15,8 @@
 
 PRODUCT_PACKAGES += \
     CarFrameworkDewdRRO \
-    CarLauncherEnableDewdRRO \
     CarUpdatableDewdRRO \
-    StubCarLauncher // TODO(b/432217693) remove once dedicated visibility barrier is present
+    StubCarLauncher # TODO(b/432217693) remove once dedicated visibility barrier is present
 
 PRODUCT_COPY_FILES += \
     packages/services/Car/car_product/dewd/android.software.car.splitscreen_multitasking.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.car.splitscreen_multitasking.xml
