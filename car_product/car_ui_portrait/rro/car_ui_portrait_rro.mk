c874b5519c1bb6a1a8e8f2bdae49204702b9536f
Ram Parameswaran <rampara@google.com>
2026-02-10 20:57:48 +0000

    Delete CarUiPortraitDataSubscriptionReferenceRRO
    Bug: 443324266
    Test: Manual
    Flag: EXEMPT PURE_REFACTOR
    
    Change-Id: Ibc2bb6afc5482c429f85b40b481db1a212b322d6

--- car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk
+++ car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk
@@ -25,7 +25,6 @@ PRODUCT_PACKAGES += \
     CarEvsCameraPreviewAppRRO \
     CarUiPortraitCarServiceRRO \
     CarUiPortraitCommon \
-    CarUiPortraitDataSubscriptionReferenceRRO \
     CarUiPortraitDialerRRO \
     CarUiPortraitFrameworkResRRO \
     CarUiPortraitLauncherAppsRRO \
