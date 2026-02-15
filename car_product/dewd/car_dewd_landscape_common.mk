b67fd41cd79a3a9a3cffe94c1e9b343c2d78d84b
Akash Suresh <akashsrocks1234@gmail.com>
2026-02-15 18:37:56 +0530

    car_dewd_common: Fix EPPE violations
    - Fix comment style to reduce EPPE violations in gsi_car.
    - Clean-up CarUiPortraitCommon and PaintBooth as they aren't in the source tree.
    
    Change-Id: I3ad41053c634434c41699cc4448a9bf4339e7d7f

--- car_product/dewd/car_dewd_landscape_common.mk
+++ car_product/dewd/car_dewd_landscape_common.mk
@@ -13,10 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO(b/432217693) remove StubCarLauncher once dedicated visibility barrier is present
 PRODUCT_PACKAGES += \
     CarFrameworkDewdRRO \
     CarUpdatableDewdRRO \
-    StubCarLauncher # TODO(b/432217693) remove once dedicated visibility barrier is present
+    StubCarLauncher
 
 PRODUCT_PACKAGES_DEBUG += \
     HunBottomRRO
