707a38734e90226f95b10309abbc72c31e77d6c1
Adam Bookatz <bookatz@google.com>
2026-03-16 18:49:24 -0700

    HSUM car sets PRODUCT_USE_HSUM
    HSUM devices are expected to set PRODUCT_USE_HSUM, to ensure that they
    inherit any other HSUM product variants in their build.
    
    Currently, the only HSUM variant is for TelephonyProvider. This probably
    doesn't usually apply to Auto, so it hasn't really mattered, but Cf Auto
    actually does use it, so it should use the correct variant. (And going
    forward, other places may also care about PRODUCT_USE_HSUM.)
    
    That is, under certain circumstances, apparently including cf, HSUM auto
    can include Telephony, so it needs to use the correct HSUM variant when
    it does.
    
    device/google/cuttlefish/vsoc_x86_64_only/auto/aosp_cf.mk inherits both
    * car_generic_system: inherits car_system_ext.mk, which (surpringly) inherits handheld_system.mk, which inherits Telephony, conditional on the HSUM variant
    * car_system_ext.mk: responsible for setting `ro.fw.mu.headless_system_user?=true`
    
    Although no known problems are from car.mk, it also sets HSUM, so it
    should also set the PRODUCT_USE_HSUM too.
    
    Test: TelephonyProviderTests:com.android.providers.telephony.TelephonyProviderHsumTest
    Bug: 481405881
    Flag: EXEMPT BUGFIX
    Change-Id: I529142efa8aa28673091fbadd5754b15adb3f04b

--- car_product/build/car.mk
+++ car_product/build/car.mk
@@ -95,6 +95,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.fw.mu.headless_system_user?=true
 
+# Variable for elsewhere choosing the appropriate products based on HSUM status.
+PRODUCT_USE_HSUM?=true
+
 # Enable User HAL integration
 # NOTE: when set to true, VHAL must also implement the user-related properties,
 # otherwise CarService will ignore it
